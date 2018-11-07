e85d340123d19e11965d281b77103f72e33567d4
Vladimir Klimenko <klimbox@gmail.com>
2018-11-07 13:19:47 +0200

    basic styles for attached objects

--- src/client/components/EditorObject/EditorObject.js
+++ src/client/components/EditorObject/EditorObject.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import './EditorObject.less';
 
 const EditorObject = ({ wObj }) => (
   <div className="editor-object">
-    <img className="editor-object__avatar" src={wObj.avatar} alt={wObj.tag} />
-    <span className="editor-object__names">
-      <span className="editor-object__names main">{`${wObj.tag} `}</span>
-      {Boolean(wObj.names.length) && (
-        <span className="editor-object__names other">
-          ({wObj.names.reduce((acc, curr) => `${acc}${curr.value}, `, '').slice(0, -2)})
-        </span>
-      )}
-    </span>
+    <div className="editor-object__content">
+      <img className="editor-object__avatar" src={wObj.avatar} alt={wObj.tag} />
+      <span className="editor-object__names">
+        <span className="editor-object__names main">{`${wObj.tag} `}</span>
+        {Boolean(wObj.names.length) && (
+          <span className="editor-object__names other">
+            ({wObj.names.reduce((acc, curr) => `${acc}${curr.value}, `, '').slice(0, -2)})
+          </span>
+        )}
+      </span>
+    </div>
+    <div className="editor-object__controls">
+      <div className="editor-object__controls delete">
+        <i className="iconfont icon-trash editor-object__controls delete-icon" />
+        <FormattedMessage id="remove" defaultMessage="Remove" />
+      </div>
+    </div>
   </div>
 );
 
