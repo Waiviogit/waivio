ac2fe3eecff9df69dc600df82a49783ba45bbdc7
Oleh T <olezhek_85@list.ru>
2018-11-19 10:46:27 +0200

    Fixed object name

--- src/client/components/EditorObject/EditorObject.js
+++ src/client/components/EditorObject/EditorObject.js
@@ -1,77 +1,38 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
-import { InputNumber, Slider } from 'antd';
 import './EditorObject.less';
 
-class EditorObject extends React.Component {
-  static propTypes = {
-    wObject: PropTypes.shape().isRequired,
-    handleRemoveObject: PropTypes.func.isRequired,
-    handleChangeInfluence: PropTypes.func.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-    this.handleChangeInfluence = this.handleChangeInfluence.bind(this);
-  }
-
-  handleChangeInfluence(influence) {
-    const { wObject, handleChangeInfluence } = this.props;
-    handleChangeInfluence(wObject, influence);
-  }
-
-  render() {
-    const { wObject, handleRemoveObject } = this.props;
-    return (
-      <div className="editor-object">
-        <div
-          onClick={() => console.log(wObject.tag)}
-          className="editor-object__content"
-          role={'button'}
-          tabIndex={0}
-        >
-          <div className="editor-object__content info">
-            <img className="editor-object__avatar" src={wObject.avatar} alt={wObject.tag} />
-            <span className="editor-object__names">
-              <span className="editor-object__names main">{wObject.tag}</span>
-              {Boolean(wObject.name) && (
-                <span className="editor-object__names other">{` (${wObject.name})`}</span>
-              )}
-            </span>
-          </div>
-          <div className="editor-object__content influence-slider">
-            <InputNumber
-              min={1}
-              max={wObject.influence.max}
-              formatter={value => `${value}%`}
-              parser={value => value.replace('%', '')}
-              size="small"
-              value={wObject.influence.value}
-              onChange={this.handleChangeInfluence}
-            />
-            <Slider
-              min={1}
-              max={wObject.influence.max}
-              onChange={this.handleChangeInfluence}
-              value={wObject.influence.value}
-            />
-          </div>
-        </div>
-        <div className="editor-object__controls">
-          <div
-            role="button"
-            tabIndex={0}
-            className="editor-object__controls delete"
-            onClick={() => handleRemoveObject(wObject.tag)}
-          >
-            <i className="iconfont icon-trash editor-object__controls delete-icon" />
-            <FormattedMessage id="remove" defaultMessage="Remove" />
-          </div>
-        </div>
+const EditorObject = ({ wObj, handleRemoveObject }) => (
+  <div className="editor-object">
+    <div className="editor-object__content">
+      <img className="editor-object__avatar" src={wObj.avatar} alt={wObj.tag} />
+      <span className="editor-object__names">
+        <span className="editor-object__names main">{`${wObj.tag} `}</span>
+        {Boolean(wObj.names.length) && (
+          <span className="editor-object__names other">
+            ({wObj.names.reduce((acc, curr) => `${acc}${curr.value}, `, '').slice(0, -2)})
+          </span>
+        )}
+      </span>
+    </div>
+    <div className="editor-object__controls">
+      <div
+        role="button"
+        tabIndex={0}
+        className="editor-object__controls delete"
+        onClick={() => handleRemoveObject(wObj.id)}
+      >
+        <i className="iconfont icon-trash editor-object__controls delete-icon" />
+        <FormattedMessage id="remove" defaultMessage="Remove" />
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
+
+EditorObject.propTypes = {
+  wObj: PropTypes.shape().isRequired,
+  handleRemoveObject: PropTypes.func.isRequired,
+};
 
 export default EditorObject;
