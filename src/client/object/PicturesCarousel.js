98984844559418e75b6cdf8b6088bb407ce15ca2
otvetve <oleg@123software.ru>
2019-01-16 16:10:11 +0200

    Added arrows to carousel

--- src/client/object/PicturesCarousel.js
+++ src/client/object/PicturesCarousel.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Carousel } from 'antd';
+import { Carousel, Icon } from 'antd';
 import './PicturesCarousel.less';
 
-const PicturesCarousel = ({ pics }) =>
-  pics.length && (
-    <Carousel autoplay lazyLoad>
-      {pics.map(pic => (
-        <img key={pic} src={pic} alt="pic" />
-      ))}
-    </Carousel>
+const Arrow = ({ className, style, onClick, icon }) => (
+  <Icon
+    className={className}
+    style={{ ...style, fontSize: '20px' }}
+    type={icon}
+    onClick={onClick}
+  />
+);
+
+Arrow.propTypes = {
+  className: PropTypes.string.isRequired,
+  style: PropTypes.shape().isRequired,
+  onClick: PropTypes.func.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+const PicturesCarousel = ({ pics }) => {
+  const settings = {
+    arrows: true,
+    lazyLoad: true,
+    nextArrow: <Arrow icon="right" />,
+    prevArrow: <Arrow icon="left" />,
+  };
+
+  return (
+    pics.length && (
+      <Carousel {...settings}>
+        {pics.map(pic => (
+          <img key={pic} src={pic} alt="pic" />
+        ))}
+      </Carousel>
+    )
   );
+};
 
 PicturesCarousel.propTypes = {
   pics: PropTypes.arrayOf(PropTypes.string),
