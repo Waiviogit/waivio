b552496c4b7ecb5e5e71aa57acb3ba1e8144d076
Roman <Waiviogit@gmail.com>
2019-01-30 11:39:42 +0200

    links in post

--- src/investarena/components/PostQuotation/PostQuotation.js
+++ src/investarena/components/PostQuotation/PostQuotation.js
@@ -1,4 +1,4 @@
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -11,8 +11,9 @@ import { currencyFormat } from '../../platform/numberFormat';
 import './PostQuotation.less';
 
 const propTypes = {
-    quoteSettings: PropTypes.object,
-    quote: PropTypes.object,
+    quoteSettings: PropTypes.shape(),
+    quote: PropTypes.shape(),
+    intl: PropTypes.shape().isRequired,
     amount: PropTypes.string,
     margin: PropTypes.string,
     toggleConfirmationModal: PropTypes.func,
@@ -38,83 +39,86 @@ const defaultProps = {
   handleKeyPressInput: ()=>{},
 };
 
-const PostQuotation = (props) => {
-    const quote = props.quote;
-    const quoteSettings = props.quoteSettings;
-    const dailyChange = `${quote.dailyChange.toFixed(2)}%`;
+const PostQuotation = ({quote, quoteSettings, margin, amount, toggleConfirmationModal, handleClickOpenDeal, handleClickLess, handleClickMore, handleBlurInput, handleChangeInput, handleKeyPressInput, intl}) => {
+
+  const wobj = quoteSettings.wobjData ? quoteSettings.wobjData : {};
+  const dailyChange = `${quote.dailyChange.toFixed(2)}%`;
     const classOfDailyChange = quote.dailyChange > 0 ? 'st-quote-text-up' : 'st-quote-text-down';
     const classOfIndicator = (quoteSettings.isSession || quote.isSession) ? 'st-post-quotation-icon-point-green' : 'st-post-quotation-icon-point-red';
-    const handleClickOpenDeal = (direction) => {
-        props.handleClickOpenDeal(direction);
-        if (props.toggleConfirmationModal) props.toggleConfirmationModal();
+    const handleOpenDeal = (direction) => {
+        handleClickOpenDeal(direction);
+        if (toggleConfirmationModal) toggleConfirmationModal();
     };
     return (
         <div className="st-post-quotation-wrap">
             <div className="st-post-quotation-block margins">
-                <header className="st-post-quotation-header d-flex justify-content-between align-items-center" >
+                <div className="st-post-quotation-header d-flex justify-content-between align-items-center" >
                     <div className="d-flex">
                         <Favorite quoteSecurity = {quote.security}/>
                     </div>
                     <div className='st-quote-name-container'>
                       <Link
-                        to={`/quote/${quote.security}`}
+                        to={`/object/@${wobj.author_permlink}`}
                         className="st-post-quotation-quote"
                         title={ quoteSettings.name }>
                         { quoteSettings.name }
-                        </Link>
-                      </div>
+                      </Link>
+                    </div>
                     <div
-                      title={props.intl.formatMessage({ id: 'tips.sessionStatus', defaultMessage: 'Trading session status' })}
+                      title={intl.formatMessage({ id: 'tips.sessionStatus', defaultMessage: 'Trading session status' })}
                       className={classOfIndicator}
                     />
-                </header>
+                </div>
                 <div className="st-post-quotation-container">
-                    <div title={props.intl.formatMessage({ id: 'tips.dailyChange', defaultMessage: 'Daily change' })} className={`st-daily-change ${classOfDailyChange}`}>
+                    <div title={intl.formatMessage({ id: 'tips.dailyChange', defaultMessage: 'Daily change' })} className={`st-daily-change ${classOfDailyChange}`}>
                         {dailyChange }
                     </div>
                 </div>
                 <div className="d-flex justify-content-between st-margin-bottom-small">
                     <span>
-                        {props.intl.formatMessage({ id: 'postQuotation.margin', defaultMessage: 'Margin'})}
+                        {intl.formatMessage({ id: 'postQuotation.margin', defaultMessage: 'Margin'})}
                     </span>
-                    {currencyFormat(props.margin)}
+                    {currencyFormat(margin)}
                 </div>
             </div>
             <div className="st-post-quotation-block amount">
                 <div className="st-post-amount">
-                    <button className="st-post-more-deal" onClick={props.handleClickLess}>&ndash;</button>
+                    <button className="st-post-more-deal" onClick={handleClickLess}>&ndash;</button>
                     <input
                         type="text"
-                        onBlur={props.handleBlurInput}
-                        onChange={ props.handleChangeInput }
-                        onKeyPress={props.handleKeyPressInput}
-                        value = {props.amount}
+                        onBlur={handleBlurInput}
+                        onChange={ handleChangeInput }
+                        onKeyPress={handleKeyPressInput}
+                        value = {amount}
                     />
-                    <button className="st-post-less-deal" onClick={props.handleClickMore}>+</button>
+                    <button className="st-post-less-deal" onClick={handleClickMore}>+</button>
                 </div>
-                <footer className="st-post-quotation-footer">
+                <div className="st-post-quotation-footer">
                     <div
-                      className={ 'st-post-action-block st-margin-right-small' + ` st-quote-${quote.state ? quote.state : 'not-update'}`}
-                      onClick={handleClickOpenDeal.bind(this, 'Sell')}
+                      role="presentation"
+                      className={`st-post-action-block st-margin-right-small st-quote-${quote.state ? quote.state : 'not-update'}`}
+                      onClick={handleOpenDeal.bind(this, 'Sell')}
                     >
                         <span className="st-post-action-span">
-                          <FormattedMessage id="postQuotation.button.sell" defaultMessage="Sell" />
+                          {intl.formatMessage({ id: 'postQuotation.button.sell', defaultMessage: 'Sell'})}
                         </span>
                         <div>
                             { quoteFormat(quote.bidPrice, quoteSettings)}
                         </div>
                     </div>
-                    <div className={ 'st-post-action-block' + ` st-quote-${quote.state ? quote.state : 'not-update'}`}
-                         onClick={handleClickOpenDeal.bind(this, 'Buy')}
+                    <div
+                      role="presentation"
+                      className={`st-post-action-block st-quote-${quote.state ? quote.state : 'not-update'}`}
+                      onClick={handleOpenDeal.bind(this, 'Buy')}
                     >
                         <span className="st-post-action-span">
-                          <FormattedMessage id="postQuotation.button.buy" defaultMessage="Buy" />
+                         {intl.formatMessage({ id: 'postQuotation.button.buy', defaultMessage: 'Buy'})}
                         </span>
                         <div>
                             {quoteFormat(quote.askPrice, quoteSettings)}
                         </div>
                     </div>
-                </footer>
+                </div>
             </div>
         </div>
     );
--- src/investarena/components/PostSellBuy/PostSellBuy.js
+++ src/investarena/components/PostSellBuy/PostSellBuy.js
@@ -1,6 +1,7 @@
 import { injectIntl, FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import React from 'react';
+import {Link} from "react-router-dom";
 import Favorite from '../Favorite';
 import InstrumentAvatar from '../InstrumentAvatar/InstrumentAvatar';
 import PostCurrentPrice from './PostCurrentPrice';
@@ -11,22 +12,26 @@ import quoteSettingsData from '../../default/quoteSettingsData';
 import './PostSellBuy.less';
 
 const propTypes = {
-    quoteSettings: PropTypes.object,
-    quote: PropTypes.object,
+    quoteSettings: PropTypes.shape(),
+    quote: PropTypes.shape(),
     profitability: PropTypes.number,
-    finalQuote: PropTypes.object,
+    finalQuote: PropTypes.shape(),
+    intl: PropTypes.shape().isRequired,
     forecast: PropTypes.string.isRequired,
     isExpired: PropTypes.bool.isRequired,
     postPrice: PropTypes.string.isRequired,
     recommend: PropTypes.string.isRequired,
-    // isSignIn: PropTypes.bool.isRequired,
-    // goQuotePage: PropTypes.func.isRequired
+};
+const defaultProps = {
+    quoteSettings: quoteSettingsData,
+    quote: quoteData,
+    profitability: 0,
+    finalQuote: {}
 };
 
-const PostSellBuy = ({intl, quote, quoteSettings, recommend, postPrice, goQuotePage, profitability, isExpired, forecast, finalQuote}) => {
+const PostSellBuy = ({intl, quote, quoteSettings, recommend, postPrice, profitability, isExpired, forecast, finalQuote}) => {
     let profitabilityIcon;
-    const quotePost = quote || quoteData;
-    const quoteSettingsPost = quoteSettings || quoteSettingsData;
+    const wobj = quoteSettings.wobjData ? quoteSettings.wobjData : {};
     const recommendPost = recommend.toLowerCase();
     if (isExpired && profitability !== undefined) {
         let data = {img: '/static/images/icons/ic_trending_flat_black_24px.svg', title: 'Neutral'};
@@ -37,49 +42,57 @@ const PostSellBuy = ({intl, quote, quoteSettings, recommend, postPrice, goQuoteP
         }
         profitabilityIcon =
             <div title={intl.formatMessage({ id: `profit.is${data.title}`})} className="st-profitability-icon">
-                <img src={data.img}/>
+                <img alt="profitability" src={data.img}/>
             </div>;
     }
     return (
         <div className='st-post-sell-buy-wrap'>
             <div className='d-flex align-items-center'>
-                <Favorite quoteSecurity = {quotePost.security}/>
-                <InstrumentAvatar quoteSecurity={quotePost.security} market={quoteSettingsPost.market}/>
+                <Favorite quoteSecurity = {quote.security}/>
+                <InstrumentAvatar
+                  avatarlink={wobj.avatarlink}
+                  market={quoteSettings.market}
+                  permlink={wobj.author_permlink}
+                />
                 <div className='st-margin-left-small'>
-                    <a
-                      // onClick={goQuotePage.bind(this, quotePost.security)}
-                      className="st-post-sell-buy-quote">{quoteSettingsPost.name}
-                    </a>
+                  <Link to={`/object/@${wobj.author_permlink}`} className="st-post-sell-buy-quote">
+                    {quoteSettings.name}
+                  </Link>
                 </div>
                 <div className={`st-post-sell-buy-block st-post-recommend-${recommendPost}`}>
                     <div title={intl.formatMessage({ id: 'tips.recommendationType', defaultMessage: 'Forecast type' })} className={`st-post-sell-buy-icon-${recommendPost}`}>
                         <FormattedMessage id={`postQuotation.recommend.${recommendPost}`} defaultMessage={recommendPost.toUpperCase()}/>
                     </div>
                     <div title={intl.formatMessage({ id: 'tips.recommendationPrice', defaultMessage: 'Price at the beginning of the forecast' })}>
-                        {quoteFormat(postPrice, quoteSettingsPost)}
+                        {quoteFormat(postPrice, quoteSettings)}
                     </div>
                 </div>
             </div>
             <div className="d-flex align-items-center">
-                <div className=''>
-                    <PostCurrentPrice quoteSettings = {quoteSettingsPost}
-                        quote = {quotePost}
-                        finalQuote = {finalQuote}
-                        isExpired = {isExpired}
-                        recommend = {recommendPost}
-                        forecast = {forecast}/>
-                </div>
+                <PostCurrentPrice
+                  quoteSettings = {quoteSettings}
+                  quote = {quote}
+                  finalQuote = {finalQuote}
+                  isExpired = {isExpired}
+                  recommend = {recommendPost}
+                  forecast = {forecast}
+                />
                 <div className='d-flex st-margin-left-large'>
-                    <span className='st-post-sell-buy-profitability' title={intl.formatMessage({ id: 'tips.profitabilityTitle', defaultMessage: 'The difference between the price at the time of publication of the post and the current price in pips'})}>
+                    <span
+                      className='st-post-sell-buy-profitability'
+                      title={intl.formatMessage({ id: 'tips.profitabilityTitle', defaultMessage: 'The difference between the price at the time of publication of the post and the current price in pips'})}
+                    >
                       <FormattedMessage id='postSellBuy.profitability' defaultMessage='Profitability'/>
                     </span>
-                    <PostDifference quoteSettings = {quoteSettingsPost}
-                        isExpired = {isExpired}
-                        profitability = {profitability}
-                        quote = {quotePost}
-                        forecast = {forecast}
-                        postPrice = {postPrice}
-                        recommend = {recommend} />
+                    <PostDifference
+                      quoteSettings = {quoteSettings}
+                      isExpired = {isExpired}
+                      profitability = {profitability}
+                      quote = {quote}
+                      forecast = {forecast}
+                      postPrice = {postPrice}
+                      recommend = {recommend}
+                    />
                 </div>
                 {profitabilityIcon}
             </div>
@@ -88,5 +101,6 @@ const PostSellBuy = ({intl, quote, quoteSettings, recommend, postPrice, goQuoteP
 };
 
 PostSellBuy.propTypes = propTypes;
+PostSellBuy.defaultProps = defaultProps;
 
 export default injectIntl(PostSellBuy);
