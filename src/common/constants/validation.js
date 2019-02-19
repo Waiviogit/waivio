c4f95105fd8d6a96612a5d2baab6df8eaa11130b
Oleh T <olezhek85@list.ru>
2019-02-19 13:43:31 +0200

    Added phone field

--- src/common/constants/validation.js
+++ src/common/constants/validation.js
@@ -7,6 +7,8 @@ export const websiteTitleRegExp = /^[a-zA-Z0-9!@#$%^&*)(+=._-]{0,100}$/;
 
 export const objectURLValidationRegExp = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
+export const phoneNameValidationRegExp = /^[^!@#$%^&*,.?":{}|<>]{0,100}$/;
+
 export const ALLOWED_IMG_FORMATS = ['jpg', 'jpeg', 'png', 'gif'];
 export const MAX_IMG_SIZE = {
   [objectFields.avatar]: 2097152,
