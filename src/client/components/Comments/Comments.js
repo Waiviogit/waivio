ea4c56b483dbc02c3cbc403850f3a7004c562c24
Vladimir Klimenko <klimbox@gmail.com>
2019-02-25 11:13:10 +0200

    comments - pending votes prop types

--- src/client/components/Comments/Comments.js
+++ src/client/components/Comments/Comments.js
@@ -31,7 +31,7 @@ class Comments extends React.Component {
     commentsChildren: PropTypes.shape(),
     pendingVotes: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.number,
+        id: PropTypes.string,
         percent: PropTypes.number,
       }),
     ),
