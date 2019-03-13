524df5fa0de2511ed5ecf8f187eb27e237522819
Roman <Waiviogit@gmail.com>
2019-03-13 14:39:26 +0200

    - commenta

--- src/client/post/Write/Write.js
+++ src/client/post/Write/Write.js
@@ -92,7 +92,6 @@ class Write extends React.Component {
       showModalDelete: false,
     };
   }
-  // NOTE: To be replaced with getDerivedStateFromProps or refactored entirely after React 16.3
   componentDidMount() {
     this.props.newPost();
     const { draftPosts, draftId, objPermlink } = this.props;
