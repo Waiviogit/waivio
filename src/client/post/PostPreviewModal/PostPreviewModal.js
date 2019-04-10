7ea15101a8a0d609e2e8f33a9ce4cb7b7cac732f
Vladimir Klimenko <klimbox@gmail.com>
2019-04-10 17:49:08 +0300

    waiv-342 post preview modal - tags selector component

--- src/client/post/PostPreviewModal/PostPreviewModal.js
+++ src/client/post/PostPreviewModal/PostPreviewModal.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { injectIntl } from 'react-intl';
 import { Button, Modal } from 'antd';
-import { FormattedMessage } from 'react-intl';
 import BodyContainer from '../../containers/Story/BodyContainer';
+import TagsSelector from '../../components/TagsSelector/TagsSelector';
 
+@injectIntl
 class PostPreviewModal extends Component {
   static propTypes = {
+    intl: PropTypes.shape().isRequired,
     content: PropTypes.string.isRequired,
   };
 
@@ -14,6 +17,7 @@ class PostPreviewModal extends Component {
 
     this.state = {
       isModalOpen: false,
+      topics: [],
     };
   }
 
@@ -21,14 +25,16 @@ class PostPreviewModal extends Component {
 
   hideModal = () => this.setState({ isModalOpen: false });
 
+  handleTopicsChange = topics => this.setState({ topics });
+
   render() {
     const { isModalOpen } = this.state;
-    const { content } = this.props;
+    const { intl, content } = this.props;
     return (
       <React.Fragment>
         {isModalOpen && (
           <Modal
-            title="post preview"
+            title={intl.formatMessage({ id: 'preview', defaultMessage: 'Preview' })}
             visible={isModalOpen}
             centered={false}
             closable
@@ -40,6 +46,17 @@ class PostPreviewModal extends Component {
             onOk={() => console.log('You are my hero!')}
           >
             <BodyContainer full body={content} />
+            <TagsSelector
+              label={intl.formatMessage({
+                id: 'topics',
+                defaultMessage: 'Topics',
+              })}
+              placeholder={intl.formatMessage({
+                id: 'topics_placeholder',
+                defaultMessage: 'Add story topics here',
+              })}
+              onChange={this.handleTopicsChange}
+            />
           </Modal>
         )}
         {content && content.length > 0 ? (
@@ -48,7 +65,7 @@ class PostPreviewModal extends Component {
             onClick={this.showModal}
             className="edit-post__publish-ready-btn"
           >
-            <FormattedMessage id="ready_to_publish" defaultMessage="Ready to publish" />
+            {intl.formatMessage({ id: 'ready_to_publish', defaultMessage: 'Ready to publish' })}
           </Button>
         ) : null}
       </React.Fragment>
