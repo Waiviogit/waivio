e7e66b1a184786d81ff9bb761d88bde0ace545bc
Vladimir Klimenko <klimbox@gmail.com>
2019-05-09 14:36:03 +0300

    editor - hide forecast clear btn

--- src/investarena/components/CreatePostForecast/CreatePostForecast.js
+++ src/investarena/components/CreatePostForecast/CreatePostForecast.js
@@ -278,175 +278,168 @@ class CreatePostForecast extends Component {
             }
           >
             <div className="st-create-post-show-options">
-              <div className="st-margin-top-middle">
-                <div className="st-create-post-dropdowns">
-                  <div className="st-create-post-dropdowns-row">
-                    <div className="st-create-post-select-wrap" data-test="select-instrument">
-                      <p className="m-0">
-                        <FormattedMessage
-                          id="createPost.selectTitle.instrument"
-                          defaultMessage="Instrument"
-                        />
-                      </p>
-                      <Select
-                        name="selected-quote"
-                        placeholder={intl.formatMessage({
-                          id: 'createPost.selectLabel.default',
-                          defaultMessage: 'Select',
-                        })}
-                        className={classNames('st-create-post-select__quote', {
-                          'st-create-post-danger': isPosted && !isValid && !selectQuote,
-                        })}
-                        disabled={isUpdating}
-                        onChange={this.updateValueQuote}
-                        value={selectQuote}
-                        showSearch
-                      >
-                        {optionsQuote.map(option => (
-                          <Select.Option key={option.value} value={option.value}>
-                            {option.label}
-                          </Select.Option>
-                        ))}
-                      </Select>
-                    </div>
-                    <div className="st-create-post-select-wrap" data-test="select-recommend">
-                      <p className="m-0">
-                        <FormattedMessage
-                          id="createPost.selectTitle.recommend"
-                          defaultMessage="I recommend"
-                        />
-                      </p>
-                      <Select
-                        name="selected-action"
-                        placeholder={intl.formatMessage({
-                          id: 'createPost.selectLabel.default',
-                          defaultMessage: 'Select',
-                        })}
-                        className={classNames('st-create-post-select__action', {
-                          'st-create-post-danger': isPosted && !isValid && !selectRecommend,
-                        })}
-                        disabled={isUpdating}
-                        value={selectRecommend}
-                        onChange={this.updateValueRecommend}
-                      >
-                        {optionsAction.map(option => (
-                          <Select.Option key={option.value} value={option.value}>
-                            {option.label}
-                          </Select.Option>
-                        ))}
-                      </Select>
-                    </div>
-                    <div className="st-create-post-select-wrap">
-                      <p className="m-0">
-                        <FormattedMessage
-                          id="createPost.selectTitle.price"
-                          defaultMessage="Price"
-                        />
-                      </p>
-                      <Input
-                        className="st-create-post-quotation"
-                        type="text"
-                        value={this.state.quotePrice}
-                        disabled
+              <div className="st-create-post-dropdowns">
+                <div className="st-create-post-dropdowns-row">
+                  <div className="st-create-post-select-wrap" data-test="select-instrument">
+                    <p className="m-0">
+                      <FormattedMessage
+                        id="createPost.selectTitle.instrument"
+                        defaultMessage="Instrument"
                       />
-                    </div>
+                    </p>
+                    <Select
+                      name="selected-quote"
+                      placeholder={intl.formatMessage({
+                        id: 'createPost.selectLabel.default',
+                        defaultMessage: 'Select',
+                      })}
+                      className={classNames('st-create-post-select__quote', {
+                        'st-create-post-danger': isPosted && !isValid && !selectQuote,
+                      })}
+                      disabled={isUpdating}
+                      onChange={this.updateValueQuote}
+                      value={selectQuote}
+                      showSearch
+                    >
+                      {optionsQuote.map(option => (
+                        <Select.Option key={option.value} value={option.value}>
+                          {option.label}
+                        </Select.Option>
+                      ))}
+                    </Select>
                   </div>
-                  <div className="st-create-post-dropdowns-row">
-                    <div className="st-create-post-select-wrap">
-                      <p className="m-0">
-                        <FormattedMessage
-                          id="modalTakeProfit.header.title"
-                          defaultMessage="Take profit"
-                        />
-                      </p>
-                      <Input
-                        type="text"
-                        className={`st-create-post-quotation${
-                          this.state.takeProfitValueIncorrect ? ' st-create-post-danger' : ''
-                        }`}
-                        value={takeProfitValue}
-                        disabled={!selectRecommend || !selectQuote || isUpdating}
-                        onChange={e =>
-                          this.handleChangeTakeProfitStopLostInputs(e, 'takeProfitValue')
-                        }
-                        onFocus={() => this.handleFocusTakeProfitStopLostInputs('takeProfitValue')}
+                  <div className="st-create-post-select-wrap" data-test="select-recommend">
+                    <p className="m-0">
+                      <FormattedMessage
+                        id="createPost.selectTitle.recommend"
+                        defaultMessage="I recommend"
                       />
-                    </div>
-                    <div className="st-create-post-select-wrap">
-                      <p className="m-0">
-                        <FormattedMessage
-                          id="modalStopLoss.header.title"
-                          defaultMessage="Stop loss"
-                        />
-                      </p>
-                      <Input
-                        type="text"
-                        className={`st-create-post-quotation${
-                          this.state.stopLossValueIncorrect ? ' st-create-post-danger' : ''
-                        }`}
-                        value={stopLossValue}
-                        disabled={!selectRecommend || !selectQuote || isUpdating}
-                        onChange={e =>
-                          this.handleChangeTakeProfitStopLostInputs(e, 'stopLossValue')
-                        }
-                        onFocus={() => this.handleFocusTakeProfitStopLostInputs('stopLossValue')}
-                      />
-                    </div>
-                    <div className="st-create-post-select-wrap" data-test="select-forecast">
-                      <p className="m-0">
-                        <FormattedMessage
-                          id="createPost.selectTitle.forecast"
-                          defaultMessage="Forecast time"
-                        />
-                      </p>
-                      {isUpdating || this.state.selectForecast === 'Custom' ? (
-                        <DatePicker
-                          disabled={isUpdating}
-                          allowClear={false}
-                          showTime={{ format: 'HH:mm', minuteStep: 5 }}
-                          style={{ width: '100%' }}
-                          locale={intl.formatMessage({ id: 'locale', defaultMessage: 'en' })}
-                          value={dateTimeValue}
-                          onChange={this.handleChangeDatetime}
-                          format="YYYY-MM-DD HH:mm"
-                          disabledDate={date =>
-                            date < moment().local() ||
-                            date >
-                              moment()
-                                .local()
-                                .add(maxForecastDay, 'days')
-                          }
-                        />
-                      ) : (
-                        <Select
-                          name="selected-forecast"
-                          placeholder={intl.formatMessage({
-                            id: 'createPost.selectLabel.default',
-                            defaultMessage: 'Select',
-                          })}
-                          className={classNames('st-create-post-select__forecast', {
-                            'st-create-post-danger': isPosted && !isValid && !selectForecast,
-                          })}
-                          value={selectForecast}
-                          onChange={this.updateValueForecast}
-                        >
-                          {optionsForecast.map(option => (
-                            <Select.Option key={option.value} value={option.value}>
-                              {option.label}
-                            </Select.Option>
-                          ))}
-                        </Select>
-                      )}
-                    </div>
+                    </p>
+                    <Select
+                      name="selected-action"
+                      placeholder={intl.formatMessage({
+                        id: 'createPost.selectLabel.default',
+                        defaultMessage: 'Select',
+                      })}
+                      className={classNames('st-create-post-select__action', {
+                        'st-create-post-danger': isPosted && !isValid && !selectRecommend,
+                      })}
+                      disabled={isUpdating}
+                      value={selectRecommend}
+                      onChange={this.updateValueRecommend}
+                    >
+                      {optionsAction.map(option => (
+                        <Select.Option key={option.value} value={option.value}>
+                          {option.label}
+                        </Select.Option>
+                      ))}
+                    </Select>
                   </div>
-                  <div
-                    className="st-create-post-dropdowns-row clear"
-                    role="presentation"
-                    onClick={this.resetForm}
-                  >
-                    {intl.formatMessage({ id: 'deals.clear', defaultMessage: 'Clear' })}
+                  <div className="st-create-post-select-wrap">
+                    <p className="m-0">
+                      <FormattedMessage id="createPost.selectTitle.price" defaultMessage="Price" />
+                    </p>
+                    <Input
+                      className="st-create-post-quotation"
+                      type="text"
+                      value={this.state.quotePrice}
+                      disabled
+                    />
                   </div>
                 </div>
+                <div className="st-create-post-dropdowns-row">
+                  <div className="st-create-post-select-wrap">
+                    <p className="m-0">
+                      <FormattedMessage
+                        id="modalTakeProfit.header.title"
+                        defaultMessage="Take profit"
+                      />
+                    </p>
+                    <Input
+                      type="text"
+                      className={`st-create-post-quotation${
+                        this.state.takeProfitValueIncorrect ? ' st-create-post-danger' : ''
+                      }`}
+                      value={takeProfitValue}
+                      disabled={!selectRecommend || !selectQuote || isUpdating}
+                      onChange={e =>
+                        this.handleChangeTakeProfitStopLostInputs(e, 'takeProfitValue')
+                      }
+                      onFocus={() => this.handleFocusTakeProfitStopLostInputs('takeProfitValue')}
+                    />
+                  </div>
+                  <div className="st-create-post-select-wrap">
+                    <p className="m-0">
+                      <FormattedMessage
+                        id="modalStopLoss.header.title"
+                        defaultMessage="Stop loss"
+                      />
+                    </p>
+                    <Input
+                      type="text"
+                      className={`st-create-post-quotation${
+                        this.state.stopLossValueIncorrect ? ' st-create-post-danger' : ''
+                      }`}
+                      value={stopLossValue}
+                      disabled={!selectRecommend || !selectQuote || isUpdating}
+                      onChange={e => this.handleChangeTakeProfitStopLostInputs(e, 'stopLossValue')}
+                      onFocus={() => this.handleFocusTakeProfitStopLostInputs('stopLossValue')}
+                    />
+                  </div>
+                  <div className="st-create-post-select-wrap" data-test="select-forecast">
+                    <p className="m-0">
+                      <FormattedMessage
+                        id="createPost.selectTitle.forecast"
+                        defaultMessage="Forecast time"
+                      />
+                    </p>
+                    {isUpdating || this.state.selectForecast === 'Custom' ? (
+                      <DatePicker
+                        disabled={isUpdating}
+                        allowClear={false}
+                        showTime={{ format: 'HH:mm', minuteStep: 5 }}
+                        style={{ width: '100%' }}
+                        locale={intl.formatMessage({ id: 'locale', defaultMessage: 'en' })}
+                        value={dateTimeValue}
+                        onChange={this.handleChangeDatetime}
+                        format="YYYY-MM-DD HH:mm"
+                        disabledDate={date =>
+                          date < moment().local() ||
+                          date >
+                            moment()
+                              .local()
+                              .add(maxForecastDay, 'days')
+                        }
+                      />
+                    ) : (
+                      <Select
+                        name="selected-forecast"
+                        placeholder={intl.formatMessage({
+                          id: 'createPost.selectLabel.default',
+                          defaultMessage: 'Select',
+                        })}
+                        className={classNames('st-create-post-select__forecast', {
+                          'st-create-post-danger': isPosted && !isValid && !selectForecast,
+                        })}
+                        value={selectForecast}
+                        onChange={this.updateValueForecast}
+                      >
+                        {optionsForecast.map(option => (
+                          <Select.Option key={option.value} value={option.value}>
+                            {option.label}
+                          </Select.Option>
+                        ))}
+                      </Select>
+                    )}
+                  </div>
+                </div>
+                {!isUpdating && (
+                  <div className="st-create-post-dropdowns-row clear">
+                    <span className="clear-btn" role="presentation" onClick={this.resetForm}>
+                      {intl.formatMessage({ id: 'deals.clear', defaultMessage: 'Clear' })}
+                    </span>
+                  </div>
+                )}
               </div>
             </div>
           </Collapse.Panel>
