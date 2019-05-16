d28190459741665f07e7a548ee7b0c4211fbdd04
Vladimir Klimenko <klimbox@gmail.com>
2019-05-16 16:41:49 +0300

    fix forecast validation helper

--- src/client/helpers/forecastHelper.js
+++ src/client/helpers/forecastHelper.js
@@ -1,15 +1,19 @@
+import moment from 'moment';
+
 export const isValidForecast = forecast => {
   let isValid = true;
   ['quoteSecurity', 'postPrice', 'recommend', 'expiredAt', 'createdAt'].forEach(field => {
     if (!forecast || forecast[field] === undefined || forecast[field] === null) isValid = false;
   });
+  if (!isValid) return false;
+  if (moment.utc(moment(forecast.expiredAt).diff(moment(forecast.createdAt))) < 0) return false;
   if (
     forecast &&
-    forecast.recommend !== 'Buy' &&
-    forecast.recommend !== 'Sell' &&
+    (forecast.recommend !== 'Buy' || forecast.recommend !== 'Sell') &&
     typeof forecast.postPrice !== 'number'
   )
     isValid = false;
+
   return isValid;
 };
 
