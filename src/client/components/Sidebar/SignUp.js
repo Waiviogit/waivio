ace01ac7aad522415c28b7992e3cdd148080b176
Roman <Waiviogit@gmail.com>
2019-06-05 20:09:58 +0300

    New registration

--- src/client/components/Sidebar/SignUp.js
+++ src/client/components/Sidebar/SignUp.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import './SidebarBlock.less';
+import ModalSignUp from '../Navigation/ModalSignUp/ModalSignUp';
 
 const SignUp = () => (
   <div className="SidebarBlock">
     <h3 className="SidebarBlock__title">
       <FormattedMessage id="new_to_busy" defaultMessage="New to Waivio?" />
     </h3>
-    <a target="_blank" rel="noopener noreferrer" href={process.env.SIGNUP_URL}>
-      <button className="SidebarBlock__button">
-        <FormattedMessage id="signup" defaultMessage="Sign up" />
-      </button>
-    </a>
+    <ModalSignUp isButton />
   </div>
 );
 
