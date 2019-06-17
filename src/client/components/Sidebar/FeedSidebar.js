370b574e6d34f2be30b2327a6ab4a8cecb03f5a9
Vladimir Klimenko <klimbox@gmail.com>
2019-06-17 09:16:56 +0300

    top experts refactor

--- src/client/components/Sidebar/FeedSidebar.js
+++ src/client/components/Sidebar/FeedSidebar.js
@@ -2,54 +2,32 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getIsAuthenticated, getRecommendations, getRecommendedObjects } from '../../reducers';
+import { getIsAuthenticated } from '../../reducers';
 import { getCryptoDetails } from '../../helpers/cryptosHelper';
-import { updateRecommendations } from '../../user/userActions';
 import InterestingObjects from './InterestingObjects';
 import CryptoTrendingCharts from './CryptoTrendingCharts';
 import InterestingPeople from './InterestingPeople';
 
-@connect(
-  state => ({
-    authenticated: getIsAuthenticated(state),
-    recommendations: getRecommendations(state),
-    recommendedObjects: getRecommendedObjects(state),
-  }),
-  { updateRecommendations },
-)
+@connect(state => ({
+  authenticated: getIsAuthenticated(state),
+}))
 class FeedSidebar extends React.Component {
   static propTypes = {
     authenticated: PropTypes.bool.isRequired,
-    recommendations: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string })).isRequired,
-    recommendedObjects: PropTypes.arrayOf(PropTypes.shape({ tag: PropTypes.string })).isRequired,
-    updateRecommendations: PropTypes.func.isRequired,
   };
 
-  constructor(props) {
-    super(props);
-    this.handleInterestingPeopleRefresh = this.handleInterestingPeopleRefresh.bind(this);
-  }
-
-  handleInterestingPeopleRefresh() {
-    this.props.updateRecommendations();
-  }
-
   render() {
-    const { authenticated, recommendations, recommendedObjects } = this.props;
-    const isAuthenticated = authenticated && recommendations.length > 0;
+    const { authenticated } = this.props;
     const currentTag = _.get(this.props, 'match.params.tag', '');
     const currentCrypto = getCryptoDetails(currentTag);
 
     return (
       <div>
         {!_.isEmpty(currentCrypto) && <CryptoTrendingCharts cryptos={[currentTag]} />}
-        {isAuthenticated && (
+        {authenticated && (
           <React.Fragment>
-            <InterestingObjects objects={recommendedObjects} />
-            <InterestingPeople
-              users={this.props.recommendations}
-              onRefresh={this.handleInterestingPeopleRefresh}
-            />
+            <InterestingObjects />
+            <InterestingPeople />
           </React.Fragment>
         )}
       </div>
