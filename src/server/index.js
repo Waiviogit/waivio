50693c87334b895977327f16d6b8031b8b0afe46
Roman <Waiviogit@gmail.com>
2019-06-27 18:49:02 +0300

    steemconnect fix

--- src/server/index.js
+++ src/server/index.js
@@ -6,20 +6,7 @@ const server = http.createServer(app);
 let currentApp = app;
 
 server.listen(process.env.PORT || 3000, () => console.log('SSR started'));
-console.log('START');
-console.log('process.env.STEEMCONNECT_CLIENT_ID');
-console.log(process.env.STEEMCONNECT_CLIENT_ID);
-console.log('process.env.STEEMCONNECT_HOST');
-console.log(process.env.STEEMCONNECT_HOST);
-console.log('process.env.STEEMCONNECT_REDIRECT_URL');
-console.log(process.env.STEEMCONNECT_REDIRECT_URL);
-console.log('process.env');
-console.log(process.env);
-console.log('process.env.NODE_ENV');
-console.log(process.env.NODE_ENV);
-console.log('process.env.production');
-console.log(process.env.production);
-console.log('END');
+
 if (module.hot) {
   console.log('✅  Server-side HMR Enabled!');
 
