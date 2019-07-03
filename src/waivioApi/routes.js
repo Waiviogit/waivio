db892066f448b7a4243ce513d15e01c4ef6314bf
Vladimir Klimenko <klimbox@gmail.com>
2019-07-03 17:33:55 +0300

    redesign route config

--- src/waivioApi/routes.js
+++ src/waivioApi/routes.js
@@ -1,3 +1,10 @@
 import config from './config.json';
 
-export default config[process.env.NODE_ENV];
+const baseUrl = config[process.env.NODE_ENV].protocol + config[process.env.NODE_ENV].host;
+const routes = {
+  ...config,
+  apiPrefix: baseUrl + config.apiPrefix,
+  objectsBotApiPrefix: baseUrl + config.objectsBotApiPrefix,
+};
+
+export default routes;
