fe2ebc8279232dfcee6bdb71f9e3dc3233e135bc
Vladimir Klimenko <klimbox@gmail.com>
2019-08-20 14:23:32 +0300

    discover page sidenav - add 'show more' btn

--- src/client/discoverObjects/SidenavDiscoverObjects.js
+++ src/client/discoverObjects/SidenavDiscoverObjects.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
-import _ from 'lodash';
+import { size } from 'lodash';
 import { getCurrentLocation, getObjectTypesList, getObjectTypesLoading } from '../reducers';
 import SkeletonCustom from '../components/Skeleton/SkeletonCustom';
 
+const typesLimit = 9;
 const SidenavDiscoverObjects = () => {
+  // redux-store
   const { pathname } = useSelector(getCurrentLocation);
   const isLoading = useSelector(getObjectTypesLoading);
   const objectTypes = useSelector(getObjectTypesList, shallowEqual);
+  // state
+  const [displayedTypesCount, setTypesCount] = useState(typesLimit);
   return (
     <ul className="sidenav-discover-objects Sidenav">
       <div className="Sidenav__section-title">
@@ -24,18 +28,31 @@ const SidenavDiscoverObjects = () => {
           width={170}
         />
       ) : (
-        _.map(objectTypes, type => (
-          <li key={`${type.author}/${type.permlink}`} className="ttc">
-            <NavLink
-              to={`/discover-objects/${type.name}`}
-              isActive={() => pathname.includes(type.name)}
-              className="sidenav-discover-objects__item"
-              activeClassName="Sidenav__item--active"
+        <React.Fragment>
+          {Object.values(objectTypes)
+            .slice(0, displayedTypesCount)
+            .map(type => (
+              <li key={`${type.author}/${type.permlink}`} className="ttc">
+                <NavLink
+                  to={`/discover-objects/${type.name}`}
+                  isActive={() => pathname.includes(type.name)}
+                  className="sidenav-discover-objects__item"
+                  activeClassName="Sidenav__item--active"
+                >
+                  {type.name}
+                </NavLink>
+              </li>
+            ))}
+          {displayedTypesCount < size(objectTypes) ? (
+            <div
+              className="sidenav-discover-objects__show-more"
+              role="presentation"
+              onClick={() => setTypesCount(displayedTypesCount + typesLimit)}
             >
-              {type.name}
-            </NavLink>
-          </li>
-        ))
+              <FormattedMessage id="show_more" defaultMessage="show more" />
+            </div>
+          ) : null}
+        </React.Fragment>
       )}
     </ul>
   );
