c74257f8a20c4314979df71717c6fcc24d361911
Vladimir Klimenko <klimbox@gmail.com>
2019-09-05 09:45:16 +0300

    discover navsidebar - render 'All' option without redirect

--- src/client/discoverObjects/DiscoverObjects.js
+++ src/client/discoverObjects/DiscoverObjects.js
@@ -7,31 +7,41 @@ import LeftSidebar from '../app/Sidebar/LeftSidebar';
 import DiscoverFiltersSidebar from './DiscoverFiltersSidebar/DiscoverFiltersSidebar';
 import DiscoverObjectsContent from './DiscoverObjectsContent';
 import './DiscoverObjects.less';
+import ObjectsContainer from '../objects/ObjectsContainer';
 
-const DiscoverObjects = ({ intl, match }) => (
-  <div className="shifted">
-    <Helmet>
-      <title>
-        {intl.formatMessage({ id: 'objects_title', defaultMessage: 'Discover objects' })} - Waivio
-      </title>
-    </Helmet>
-    <div className="feed-layout container">
-      <Affix className="leftContainer" stickPosition={77}>
-        <div className="left">
-          <LeftSidebar />
+const DiscoverObjects = ({ intl, match }) => {
+  const isTypeChosen = Boolean(match.params.typeName);
+  return (
+    <div className="shifted">
+      <Helmet>
+        <title>
+          {intl.formatMessage({ id: 'objects_title', defaultMessage: 'Discover objects' })} - Waivio
+        </title>
+      </Helmet>
+      <div className="feed-layout container">
+        <Affix className="leftContainer" stickPosition={77}>
+          <div className="left">
+            <LeftSidebar />
+          </div>
+        </Affix>
+        {isTypeChosen && (
+          <Affix className="rightContainer" stickPosition={77}>
+            <div className="right">
+              <DiscoverFiltersSidebar />
+            </div>
+          </Affix>
+        )}
+        <div className={`discover-objects${isTypeChosen ? ' center' : ''}`}>
+          {match.params.typeName ? (
+            <DiscoverObjectsContent typeName={match.params.typeName} key={match.url} intl={intl} />
+          ) : (
+            <ObjectsContainer />
+          )}
         </div>
-      </Affix>
-      <Affix className="rightContainer" stickPosition={77}>
-        <div className="right">
-          <DiscoverFiltersSidebar />
-        </div>
-      </Affix>
-      <div className="center discover-objects">
-        <DiscoverObjectsContent typeName={match.params.typeName} key={match.url} intl={intl} />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 DiscoverObjects.propTypes = {
   intl: PropTypes.shape().isRequired,
--- src/client/objects/Objects.js
+++ src/client/objects/Objects.js
@@ -1,78 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { Tabs } from 'antd';
-import { withRouter } from 'react-router-dom';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import LeftSidebar from '../app/Sidebar/LeftSidebar';
-import ObjectContent from './ObjectContent';
+import ObjectsContainer from './ObjectsContainer';
 import Affix from '../components/Utils/Affix';
-import SearchObjectsAutocomplete from '../components/EditorObject/SearchObjectsAutocomplete';
-import './Objects.less';
 
-const TabPane = Tabs.TabPane;
-
-const Objects = ({ intl, history }) => {
-  const handleSelectObject = selected => {
-    if (selected) {
-      history.push(`/object/${selected.id}`);
-    }
-  };
-  return (
-    <div className="shifted">
-      <Helmet>
-        <title>
-          {intl.formatMessage({ id: 'objects_title', defaultMessage: 'objects_title' })} - Waivio
-        </title>
-      </Helmet>
-      <div className="feed-layout container">
-        <Affix className="leftContainer" stickPosition={77}>
-          <div className="left">
-            <LeftSidebar />
-          </div>
-        </Affix>
-        <div className="Objects">
-          <div className="Objects__title">
-            <div className="Objects__message">
-              <FormattedMessage id="objects_title" defaultMessage="Discover more objects" />
-            </div>
-            <SearchObjectsAutocomplete handleSelect={handleSelectObject} />
-          </div>
-          <Tabs defaultActiveKey="1">
-            <TabPane
-              tab={
-                <span className="UserExpertise__item">
-                  <FormattedMessage id="hashtag_value_placeholder" defaultMessage="Hashtags" />
-                </span>
-              }
-              key="1"
-            >
-              <div className="Objects__content">
-                <ObjectContent isOnlyHashtags />
-              </div>
-            </TabPane>
-            <TabPane
-              tab={
-                <span className="UserExpertise__item">
-                  <FormattedMessage id="objects" defaultMessage="Objects" />
-                </span>
-              }
-              key="2"
-            >
-              <div className="Objects__content">
-                <ObjectContent />
-              </div>
-            </TabPane>
-          </Tabs>
+const Objects = ({ intl }) => (
+  <div className="shifted">
+    <Helmet>
+      <title>
+        {intl.formatMessage({ id: 'objects_title', defaultMessage: 'objects_title' })} - Waivio
+      </title>
+    </Helmet>
+    <div className="feed-layout container">
+      <Affix className="leftContainer" stickPosition={77}>
+        <div className="left">
+          <LeftSidebar />
         </div>
-      </div>
+      </Affix>
+      <ObjectsContainer />
     </div>
-  );
-};
+  </div>
+);
 
 Objects.propTypes = {
   intl: PropTypes.shape().isRequired,
-  history: PropTypes.shape().isRequired,
 };
 
-export default injectIntl(withRouter(Objects));
+export default injectIntl(Objects);
