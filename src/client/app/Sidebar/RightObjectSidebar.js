ee3fc4f1f75d840ff31c11ce6604c992d14163ba
Roman <Waiviogit@gmail.com>
2019-09-06 13:40:59 +0300

    fix right block

--- src/client/app/Sidebar/RightObjectSidebar.js
+++ src/client/app/Sidebar/RightObjectSidebar.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import RightSidebarLoading from '../../../client/app/Sidebar/RightSidebarLoading';
 import ObjectExpertise from '../../components/Sidebar/ObjectExpertise';
 import ForecastBlock from '../../components/ForecastBlock';
 
 const RightObjectSidebar = ({ username, wobject, quoteSecurity }) =>
-  wobject.users ? (
     <React.Fragment>
       <ObjectExpertise username={username} wobject={wobject} />
       {quoteSecurity && <ForecastBlock
@@ -13,11 +11,7 @@ const RightObjectSidebar = ({ username, wobject, quoteSecurity }) =>
         renderPlace={'rightObjectSidebar'}
         quoteSecurity={quoteSecurity}
       />}
-    </React.Fragment>
-  ) : (
-    <RightSidebarLoading />
-  );
-
+    </React.Fragment>;
 RightObjectSidebar.propTypes = {
   username: PropTypes.string.isRequired,
   wobject: PropTypes.shape().isRequired,
