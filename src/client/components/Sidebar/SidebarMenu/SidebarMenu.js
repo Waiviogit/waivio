2b187967f52c647fce2febfb5572dcbbcaaa8030
Vladimir Klimenko <klimbox@gmail.com>
2019-09-17 14:05:56 +0300

    sidebar menu - steem and personal collapsible blocks

--- src/client/components/Sidebar/SidebarMenu/SidebarMenu.js
+++ src/client/components/Sidebar/SidebarMenu/SidebarMenu.js
@@ -1,15 +1,20 @@
 import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
-import { injectIntl } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
+import { NavLink } from 'react-router-dom';
 
+const menuSections = {
+  STEEM: 'Steem',
+  PERSONAL: 'Personal',
+};
 const initialState = {
-  Steem: {
-    name: 'Steem',
+  [menuSections.STEEM]: {
+    name: menuSections.STEEM,
     intlId: 'steem',
     isCollapsed: false,
   },
-  Personal: {
-    name: 'Personal',
+  [menuSections.PERSONAL]: {
+    name: menuSections.PERSONAL,
     intlId: 'personal',
     isCollapsed: false,
   },
@@ -32,18 +37,90 @@ function sidebarMenuReducer(state, action) {
   }
 }
 
-const SidebarMenu = ({ intl }) => {
+const SidebarMenu = ({ intl, location }) => {
   const [menuState, dispatch] = useReducer(sidebarMenuReducer, initialState);
-  const collapseBlock = blockName => () =>
+  const toggleBlock = blockName => () =>
     dispatch({ type: actionType.TOGGLE_BLOCK, payload: { block: blockName } });
+  const getSectionContent = section => {
+    switch (section.name) {
+      case menuSections.STEEM:
+        return (
+          <ul className="Sidenav collapsible-block__content">
+            <li className="collapsible-block__item">
+              <NavLink
+                to={'/trending'}
+                className="sidenav-discover-objects__item"
+                activeClassName="Sidenav__item--active"
+              >
+                <FormattedMessage id="sort_trending" defaultMessage="Trending" />
+              </NavLink>
+            </li>
+            <li className="collapsible-block__item">
+              <NavLink
+                to={'/hot'}
+                className="sidenav-discover-objects__item"
+                activeClassName="Sidenav__item--active"
+              >
+                <FormattedMessage id="sort_hot" defaultMessage="Hot" />
+              </NavLink>
+            </li>
+            <li className="collapsible-block__item">
+              <NavLink
+                to={'/promoted'}
+                className="sidenav-discover-objects__item"
+                activeClassName="Sidenav__item--active"
+              >
+                <FormattedMessage id="sort_promoted" defaultMessage="Promoted" />
+              </NavLink>
+            </li>
+          </ul>
+        );
+      case menuSections.PERSONAL:
+        return (
+          <ul className="Sidenav collapsible-block__content">
+            <li className="collapsible-block__item">
+              <NavLink
+                to={'/'}
+                className="sidenav-discover-objects__item"
+                isActive={() => location.pathname === '/'}
+                activeClassName="Sidenav__item--active"
+              >
+                <FormattedMessage id="my_feed" defaultMessage="My feed" />
+              </NavLink>
+            </li>
+            <li className="collapsible-block__item">
+              <NavLink
+                to={'/notifications'}
+                className="sidenav-discover-objects__item"
+                activeClassName="Sidenav__item--active"
+              >
+                <FormattedMessage id="notifications" defaultMessage="Notifications" />
+              </NavLink>
+            </li>
+            <li className="collapsible-block__item">
+              <NavLink
+                to={'/updates'}
+                className="sidenav-discover-objects__item"
+                isActive={() => location.pathname === '/updates'}
+                activeClassName="Sidenav__item--active"
+              >
+                <FormattedMessage id="updates" defaultMessage="upd" />
+              </NavLink>
+            </li>
+          </ul>
+        );
+      default:
+        return null;
+    }
+  };
   return (
     <div className="collapsible-block SidebarContentBlock__content">
       {Object.values(menuState).map(section => (
-        <div className={`collapsible-block__${section.name}-section`}>
+        <div className={`collapsible-block__${section.name}-section`} key={section.name}>
           <div
             className="collapsible-block__title"
             role="presentation"
-            onClick={collapseBlock(section.name)}
+            onClick={toggleBlock(section.name)}
           >
             <span className="collapsible-block__title-text">
               {intl.formatMessage({ id: section.intlId, defaultMessage: section.name })}
@@ -56,9 +133,7 @@ const SidebarMenu = ({ intl }) => {
               )}
             </span>
           </div>
-          {!section.isCollapsed ? (
-            <div className="collapsible-block__content">{section.name}::Content</div>
-          ) : null}
+          {!section.isCollapsed ? getSectionContent(section) : null}
         </div>
       ))}
     </div>
@@ -67,7 +142,7 @@ const SidebarMenu = ({ intl }) => {
 
 SidebarMenu.propTypes = {
   intl: PropTypes.shape().isRequired,
-  // location: PropTypes.shape().isRequired,
+  location: PropTypes.shape().isRequired,
 };
 
 export default injectIntl(SidebarMenu);
