31d1e149adb853960ab5386aef6f752b7601437a
konstantin <zakharchenko.konstantin@gmail.com>
2019-09-19 10:48:37 +0300

    researched postMessage method

--- src/client/components/Chat/Chat.js
+++ src/client/components/Chat/Chat.js
@@ -3,21 +3,47 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import SearchUsersAutocomplete from '../../components/EditorUser/SearchUsersAutocomplete';
 import './Chat.less';
+import { jsonParse } from '../../helpers/formatter';
 
-const Chat = ({ visibility }) => (
-  <div
-    className={classNames('Chat', {
-      'hide-element': visibility,
-    })}
-  >
-    <div className="Chat__conversations-wrap">
-      <SearchUsersAutocomplete allowClear={false} style={{ width: '100%' }} />
-    </div>
-  </div>
-);
+class Chat extends React.Component {
+  static propTypes = {
+    visibility: PropTypes.bool.isRequired,
+  };
 
-Chat.propTypes = {
-  visibility: PropTypes.bool.isRequired,
-};
+  componentDidMount() {
+    const data = { source: 'chat', message: 'hello from chat' };
+    window.addEventListener('message', event => {
+      if (typeof event.data === 'string' && jsonParse(event.data).source === 'manage') {
+        console.log('DATA_CHAT', jsonParse(event.data).message);
+      }
+    });
+    this.ifr.onload = () => {
+      this.ifr.contentWindow.postMessage(JSON.stringify(data));
+      console.log('Печеньки манага', this.ifr.contentWindow.document.cookie);
+    };
+  }
+
+  render() {
+    return (
+      <div
+        className={classNames('Chat', {
+          'hide-element': this.props.visibility,
+        })}
+      >
+        <div className="Chat__conversations-wrap">
+          <SearchUsersAutocomplete allowClear={false} style={{ width: '100%' }} />
+        </div>
+        <div className="Chat__messages-wrap">
+          <iframe
+            src="/rewards/manage"
+            /* eslint no-return-assign: "error" */
+            ref={f => (this.ifr = f)}
+            title="frame"
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Chat;
--- src/client/rewards/Manage/Manage.js
+++ src/client/rewards/Manage/Manage.js
@@ -9,6 +9,7 @@ import CampaignRewardsTable from './CampaignRewardsTable/CampaignRewardsTable';
 import BalanceTable from './BalanceTable/BalanceTable';
 import { activateCampaign } from '../../user/userActions';
 import { getAuthenticatedUser } from '../../reducers';
+import { jsonParse } from '../../helpers/formatter';
 
 @injectIntl
 @connect(
@@ -35,6 +36,14 @@ class Manage extends React.Component {
   };
 
   componentDidMount() {
+    const manageData = { source: 'manage', message: 'hello from manage' };
+    window.addEventListener('message', event => {
+      if (typeof event.data === 'string' && jsonParse(event.data).source === 'chat') {
+        console.log('DATA_MANAGE', jsonParse(event.data).message);
+        event.source.postMessage(JSON.stringify(manageData));
+        console.log('Печеньки чата', window.top.document.cookie);
+      }
+    });
     const { userName } = this.props;
     if (userName) {
       ApiClient.getCampaignsByGuideName(userName).then(data => {
