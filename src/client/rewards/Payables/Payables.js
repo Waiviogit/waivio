46060357933c9ad1cec02abe69f07c96a7c48d85
konstantin <zakharchenko.konstantin@gmail.com>
2019-10-01 08:58:34 +0300

    added response on API

--- src/client/rewards/Payables/Payables.js
+++ src/client/rewards/Payables/Payables.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import _ from 'lodash';
 import UserPayableCard from './UserPayableCard/UserPayableCrad';
 import './Payables.less';
+import { getLenders } from '../../../waivioApi/ApiClient';
 
-const Payables = ({ intl }) => {
+const Payables = ({ intl, userName }) => {
+  const [lenders, setLenders] = useState({});
+  useEffect(() => {
+    getLenders(userName)
+      .then(data => setLenders(data))
+      .catch(e => console.log(e));
+  }, []);
+  console.log(lenders);
   const fakeUserData = [{ userName: 'siamcat', aliasName: 'TasteemFundationWeKu', debt: 13.1 }];
   return (
     <div className="Payables">
@@ -31,6 +39,7 @@ const Payables = ({ intl }) => {
 
 Payables.propTypes = {
   intl: PropTypes.shape().isRequired,
+  userName: PropTypes.string.isRequired,
 };
 
 export default injectIntl(Payables);
--- src/waivioApi/ApiClient.js
+++ src/waivioApi/ApiClient.js
@@ -561,6 +561,19 @@ export const getAuthenticatedUserMetadata = (
     .then(res => _.omit(res.user_metadata, '_id'));
 };
 
+export const getLenders = (sponsor, user) => {
+  const isUser = !user ? `?sponsor=${sponsor}` : `?sponsor=${sponsor}&userName=${user}`;
+  return new Promise((resolve, reject) => {
+    fetch(`${config.campaignApiPrefix}${config.payables}${isUser}`, {
+      headers,
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(result => resolve(result))
+      .catch(error => reject(error));
+  });
+};
+
 export const updateUserMetadata = (userName, data) =>
   fetch(`${config.apiPrefix}${config.user}/${userName}${config.userMetadata}`, {
     headers: { ...headers, 'access-token': Cookie.get('access_token') },
