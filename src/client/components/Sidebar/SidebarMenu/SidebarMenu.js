ddf71482f2dbf422459d4aaa47a5508d73e578a8
Vladimir Klimenko <klimbox@gmail.com>
2019-10-22 11:47:10 +0300

    sidebar navigation menu redesign

--- src/client/components/Sidebar/SidebarMenu/SidebarMenu.js
+++ src/client/components/Sidebar/SidebarMenu/SidebarMenu.js
@@ -5,23 +5,7 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 import { NavLink } from 'react-router-dom';
 import { getIsAuthenticated } from '../../../reducers';
 
-const menuSections = {
-  STEEM: 'Steem',
-  PERSONAL: 'Personal',
-};
-const initialState = {
-  [menuSections.STEEM]: {
-    name: menuSections.STEEM,
-    intlId: 'steem',
-    isCollapsed: false,
-  },
-  [menuSections.PERSONAL]: {
-    name: menuSections.PERSONAL,
-    intlId: 'personal',
-    isCollapsed: false,
-    requireAuth: true,
-  },
-};
+// todo: sync with dev branch
 
 const actionType = { TOGGLE_BLOCK: 'toggleBlock' };
 function sidebarMenuReducer(state, action) {
@@ -36,15 +20,15 @@ function sidebarMenuReducer(state, action) {
         },
       };
     default:
-      return initialState;
+      return state;
   }
 }
 
-const SidebarMenu = ({ intl }) => {
+const SidebarMenu = ({ intl, menuConfig }) => {
   // redux store
   const authenticated = useSelector(getIsAuthenticated);
   // local state
-  const [menuState, dispatch] = useReducer(sidebarMenuReducer, initialState);
+  const [menuState, dispatch] = useReducer(sidebarMenuReducer, menuConfig);
 
   const toggleBlock = blockName => () =>
     dispatch({ type: actionType.TOGGLE_BLOCK, payload: { block: blockName } });
@@ -54,83 +38,26 @@ const SidebarMenu = ({ intl }) => {
     return match.url !== '' && location.pathname.includes(match.url);
   };
 
-  const getSectionContent = section => {
-    switch (section.name) {
-      case menuSections.STEEM:
-        return (
-          <ul className="Sidenav collapsible-block__content">
-            <li className="collapsible-block__item">
-              <NavLink
-                to={authenticated ? '/trending' : '/'}
-                className="sidenav-discover-objects__item"
-                isActive={checkIsActive}
-                activeClassName="Sidenav__item--active"
-              >
-                <FormattedMessage id="sort_trending" defaultMessage="Trending" />
-              </NavLink>
-            </li>
-            <li className="collapsible-block__item">
-              <NavLink
-                to={'/hot'}
-                className="sidenav-discover-objects__item"
-                isActive={checkIsActive}
-                activeClassName="Sidenav__item--active"
-              >
-                <FormattedMessage id="sort_hot" defaultMessage="Hot" />
-              </NavLink>
-            </li>
-            <li className="collapsible-block__item">
-              <NavLink
-                to={'/created'}
-                className="sidenav-discover-objects__item"
-                isActive={checkIsActive}
-                activeClassName="Sidenav__item--active"
-              >
-                <FormattedMessage id="sort_created" defaultMessage="New" />
-              </NavLink>
-            </li>
-          </ul>
-        );
-      case menuSections.PERSONAL:
-        return (
-          <ul className="Sidenav collapsible-block__content">
-            <li className="collapsible-block__item">
-              <NavLink
-                to={'/'}
-                className="sidenav-discover-objects__item"
-                isActive={checkIsActive}
-                activeClassName="Sidenav__item--active"
-              >
-                <FormattedMessage id="my_feed" defaultMessage="My feed" />
-              </NavLink>
-            </li>
-            <li className="collapsible-block__item">
-              <NavLink
-                to={'/notifications'}
-                className="sidenav-discover-objects__item"
-                isActive={checkIsActive}
-                activeClassName="Sidenav__item--active"
-              >
-                <FormattedMessage id="notifications" defaultMessage="Notifications" />
-              </NavLink>
-            </li>
-            <li className="collapsible-block__item">
-              <NavLink
-                disabled
-                to={'/updates'}
-                className="sidenav-discover-objects__item"
-                isActive={checkIsActive}
-                activeClassName="Sidenav__item--active"
-              >
-                <FormattedMessage id="updates" defaultMessage="Updates" />
-              </NavLink>
-            </li>
-          </ul>
-        );
-      default:
-        return null;
-    }
-  };
+  const getSectionContent = menuSection => (
+    <ul className="Sidenav collapsible-block__content">
+      {menuSection.items.map(sectionItem => {
+        const linkTo = authenticated ? sectionItem.linkTo : sectionItem.unauthLink;
+        return linkTo ? (
+          <li className="collapsible-block__item">
+            <NavLink
+              to={linkTo}
+              className="sidenav-discover-objects__item"
+              isActive={checkIsActive}
+              activeClassName="Sidenav__item--active"
+              disabled={Boolean(sectionItem.disabled)}
+            >
+              <FormattedMessage id={sectionItem.intlId} defaultMessage={sectionItem.name} />
+            </NavLink>
+          </li>
+        ) : null;
+      })}
+    </ul>
+  );
 
   return (
     <div className="collapsible-block SidebarContentBlock__content">
@@ -163,6 +90,18 @@ const SidebarMenu = ({ intl }) => {
 
 SidebarMenu.propTypes = {
   intl: PropTypes.shape().isRequired,
+  menuConfig: PropTypes.shape(),
+};
+
+SidebarMenu.defaultProps = {
+  menuConfig: {
+    'No Data': {
+      name: 'No Data',
+      intlId: 'charts.noData',
+      isCollapsed: true,
+      items: [],
+    },
+  },
 };
 
 export default injectIntl(SidebarMenu);
--- src/client/components/Sidebar/SidebarMenu/index.js
+++ src/client/components/Sidebar/SidebarMenu/index.js
@@ -1,6 +1,62 @@
 import React from 'react';
 import SidebarMenu from './SidebarMenu';
 
-const SidebarMenuContainer = props => <SidebarMenu {...props} />;
+// todo: sync with dev branch
+
+const menuSections = {
+  STEEM: 'Steem',
+  PERSONAL: 'Personal',
+};
+const menuConfig = {
+  [menuSections.STEEM]: {
+    name: menuSections.STEEM,
+    intlId: 'steem',
+    isCollapsed: false,
+    items: [
+      {
+        name: 'Trending',
+        intlId: 'sort_trending',
+        linkTo: '/trending', // linkTo: '/' - for unauthenticated
+        unauthLink: '/',
+      },
+      {
+        name: 'Hot',
+        intlId: 'sort_hot',
+        linkTo: '/hot',
+      },
+      {
+        name: 'New',
+        intlId: 'sort_created',
+        linkTo: '/created',
+      },
+    ],
+  },
+  [menuSections.PERSONAL]: {
+    name: menuSections.PERSONAL,
+    intlId: 'personal',
+    isCollapsed: false,
+    requireAuth: true,
+    items: [
+      {
+        name: 'My feed',
+        intlId: 'my_feed',
+        linkTo: '/',
+      },
+      {
+        name: 'Notifications',
+        intlId: 'notifications',
+        linkTo: '/notifications',
+      },
+      {
+        name: 'Updates',
+        intlId: 'updates',
+        linkTo: '/updates',
+        disabled: true,
+      },
+    ],
+  },
+};
+
+const SidebarMenuContainer = props => <SidebarMenu menuConfig={menuConfig} {...props} />;
 
 export default SidebarMenuContainer;
