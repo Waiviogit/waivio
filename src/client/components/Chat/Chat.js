57c5bedc7cfc5e743721aafb8126f09b1a68a764
konstantin <zakharchenko.konstantin@gmail.com>
2019-10-22 18:06:59 +0300

    edited listener

--- src/client/components/Chat/Chat.js
+++ src/client/components/Chat/Chat.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-// import { jsonParse } from '../../helpers/formatter';
 import { connect } from 'react-redux';
 import { getAuthenticatedUserName } from '../../reducers';
 import { setSessionId } from './chatActions';
@@ -22,66 +21,49 @@ class Chat extends React.Component {
     userName: PropTypes.string.isRequired,
   };
 
-  state = {
-    initResponseData: {
+  componentDidMount() {
+    const initData = {
+      cmd: 'init',
+      args: {
+        username: `${this.props.userName}`,
+      },
+    };
+
+    const initResponseData = {
       cmd: 'auth_connection',
       args: {
-        user_name: '',
-        block_number: 123,
+        user_name: `${this.props.userName}`,
+        block_number: 1,
         transaction_id: '',
       },
-    },
-  };
+    };
 
-  componentDidMount() {
-    this.props.setSessionId('aswasdf_asdf_fagjkas').then(data => console.log('data', data));
-    // const initData = {
-    //   cmd: "init",
-    //   args: {
-    //     username: `${this.props.userName}`
-    //   }
-    // };
-
-    // window.addEventListener('message', event => {
-    // let chatResponseData = jsonParse(event.data);
-    // switch (chatResponseData.cmd) {
-    //   case "connected":
-    //     this.sendChatRequestData(initData);
-    //     break;
-    //   case "init_response":
-    // setSessionId(chatResponseData.args.session_id)
-    //   .then(transactionId => this.setState({
-    //     initResponseData: {
-    //       ...this.state.initResponseData,
-    //       args: {
-    //         ...this.state.initResponseData.args,
-    //         transaction_id: transactionId,
-    //         user_name: `${this.props.userName}`
-    //       }
-    //     }
-    //   })
-    //   .then(() => this.sendChatRequestData(this.state.initResponseData))
-    // break;
-    // case "auth_connection_response":
-    //   console.log(chatResponseData.args.status);
-    //   break;
-    // case "new_event":
-    //   console.log("new_event");
-    //   break;
-    // }
-
-    // });
-    // eslint-disable-next-line react/no-did-mount-set-state
-    this.setState({
-      initResponseData: {
-        ...this.state.initResponseData,
-        args: {
-          ...this.state.initResponseData.args,
-          transaction_id: 'asdgfasdfga',
-          user_name: `${this.props.userName}`,
-        },
-      },
+    window.addEventListener('message', event => {
+      const chatResponseData = JSON.parse(event.data);
+      switch (chatResponseData.cmd) {
+        case 'connected':
+          this.sendChatRequestData(initData);
+          break;
+        case 'init_response':
+          this.props
+            .setSessionId(chatResponseData.args.session_id)
+            // eslint-disable-next-line react/no-did-mount-set-state
+            .then(data => {
+              initResponseData.args.transaction_id = data.value.result.id;
+              initResponseData.args.block_number = data.value.result.block_num;
+            })
+            .then(() => this.sendChatRequestData(initResponseData));
+          break;
+        case 'auth_connection_response':
+          console.log(chatResponseData.args.status);
+          break;
+        case 'new_event':
+          console.log('new_event');
+          break;
+        default:
+      }
     });
+    // eslint-disable-next-line react/no-did-mount-set-state
   }
 
   sendChatRequestData = data => {
@@ -91,7 +73,6 @@ class Chat extends React.Component {
   };
 
   render() {
-    console.log(this.state);
     return (
       <div
         className={classNames('Chat', {
