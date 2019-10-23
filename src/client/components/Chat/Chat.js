032205ca8daaa282b5ee97f54347ceca4d0df018
konstantin <zakharchenko.konstantin@gmail.com>
2019-10-23 18:15:42 +0300

    adapted authorization with iFrame

--- src/client/components/Chat/Chat.js
+++ src/client/components/Chat/Chat.js
@@ -25,7 +25,7 @@ class Chat extends React.Component {
     const initData = {
       cmd: 'init',
       args: {
-        username: `${this.props.userName}`,
+        username: `monterey`,
       },
     };
 
@@ -39,37 +39,35 @@ class Chat extends React.Component {
     };
 
     window.addEventListener('message', event => {
-      const chatResponseData = JSON.parse(event.data);
-      switch (chatResponseData.cmd) {
-        case 'connected':
-          this.sendChatRequestData(initData);
-          break;
-        case 'init_response':
-          this.props
-            .setSessionId(chatResponseData.args.session_id)
-            // eslint-disable-next-line react/no-did-mount-set-state
-            .then(data => {
-              initResponseData.args.transaction_id = data.value.result.id;
-              initResponseData.args.block_number = data.value.result.block_num;
-            })
-            .then(() => this.sendChatRequestData(initResponseData));
-          break;
-        case 'auth_connection_response':
-          console.log(chatResponseData.args.status);
-          break;
-        case 'new_event':
-          console.log('new_event');
-          break;
-        default:
+      if (event && event.data && event.origin === 'https://stchat.cf') {
+        switch (event.data.cmd) {
+          case 'connected':
+            this.sendChatRequestData(initData);
+            break;
+          case 'init_response':
+            this.props
+              .setSessionId(event.data.args.session_id)
+              // eslint-disable-next-line react/no-did-mount-set-state
+              .then(data => {
+                initResponseData.args.transaction_id = data.value.result.id;
+                initResponseData.args.block_number = data.value.result.block_num;
+              })
+              .then(() => this.sendChatRequestData(initResponseData));
+            break;
+          case 'auth_connection_response':
+            console.log(event.data.args.status);
+            break;
+          case 'new_event':
+            console.log('new_event');
+            break;
+          default:
+        }
       }
     });
-    // eslint-disable-next-line react/no-did-mount-set-state
   }
 
   sendChatRequestData = data => {
-    this.ifr.onload = () => {
-      this.ifr.contentWindow.postMessage(JSON.stringify(data));
-    };
+    this.ifr.contentWindow.postMessage(data, 'https://stchat.cf');
   };
 
   render() {
@@ -80,12 +78,16 @@ class Chat extends React.Component {
         })}
       >
         <div className="Chat__wrap">
-          <iframe
-            src="https://stchat.cf/"
-            /* eslint no-return-assign: "error" */
-            ref={f => (this.ifr = f)}
-            title="frame"
-          />
+          {this.props.visibility ? (
+            <iframe
+              src="https://stchat.cf/app.html"
+              /* eslint no-return-assign: "error" */
+              ref={f => (this.ifr = f)}
+              title="frame"
+              sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-top-navigation"
+              name="sandbox"
+            />
+          ) : null}
         </div>
       </div>
     );
