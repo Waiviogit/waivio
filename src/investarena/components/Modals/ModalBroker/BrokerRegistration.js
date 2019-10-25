fc6d2f1ec092fb85337c51d79c2fe81591b336d8
Vadim Burukin <vadimb@wizardsdev.com>
2019-10-25 18:58:16 +0300

    Refactored broker registration modal

--- src/investarena/components/Modals/ModalBroker/BrokerRegistration.js
+++ src/investarena/components/Modals/ModalBroker/BrokerRegistration.js
@@ -1,15 +1,15 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { injectIntl } from 'react-intl';
-import { Form, Input, Select, Checkbox, Button, Radio } from 'antd';
-import { optionsPlatform } from '../../../constants/selectData';
-import { country } from '../../../constants/countryData';
-import { phoneCode } from '../../../constants/phoneCodeData';
-import { agreements } from '../../../configApi/licenseAgreements';
+import {injectIntl} from 'react-intl';
+import {Button, Checkbox, Form, Input, Radio, Select} from 'antd';
+import {optionsPlatform} from '../../../constants/selectData';
+import {country} from '../../../constants/countryData';
+import {phoneCode} from '../../../constants/phoneCodeData';
+import {agreements} from '../../../configApi/licenseAgreements';
 import ObjectCardView from '../../../../client/objectCard/ObjectCardView';
-import { getClientWObj } from '../../../../client/adapters';
-import { getObjectsByIds } from '../../../../waivioApi/ApiClient';
+import {getClientWObj} from '../../../../client/adapters';
+import {getObjectsByIds} from '../../../../waivioApi/ApiClient';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
@@ -42,28 +42,15 @@ class BrokerRegistration extends Component {
     this.setState({ platformsWobjects: wobjectsArray.wobjects });
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-        values.phoneNumber = values.phone.substring(2, values.phone.length - 1);
-        values.phoneOperator = values.phone.substring(0, 2);
-        this.props.changeEmail(values.email);
-        this.props.registerBroker(values);
-      }
-    });
-  };
-
-  handleConfirmBlur = e => {
-    const value = e.target.value;
-    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-  };
-
   compareToFirstPassword = (rule, value, callback) => {
     const form = this.props.form;
     if (value && value !== form.getFieldValue('password')) {
-      callback('Two passwords that you enter is inconsistent!');
+      callback(
+        this.props.intl.formatMessage({
+          id: 'broker_modal_confirm_same_password',
+          defaultMessage: 'Two passwords that you enter is inconsistent!',
+        }),
+      );
     } else {
       callback();
     }
@@ -99,43 +86,47 @@ class BrokerRegistration extends Component {
     this.state.stepNumber === 3 && this.setState({ stepNumber: 2, platformName: '' });
   };
 
-  render() {
-    const { getFieldDecorator } = this.props.form;
+  isExcludedCountry = _.every(optionsPlatform, option => option.excludedCountries.includes(this.state.currentCountryValue));
 
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 16 },
-        sm: { span: 8 },
-      },
-      wrapperCol: {
-        xs: { span: 16 },
-        sm: { span: 16 },
-      },
-    };
-    const tailFormItemLayout = {
-      wrapperCol: {
-        xs: {
-          span: 16,
-          offset: 0,
-        },
-        sm: {
-          span: 16,
-          offset: 8,
-        },
-      },
-    };
-    const prefixSelector = getFieldDecorator('phoneCountry', {
-      initialValue: phoneCode[this.state.currentCountryValue],
-    })(<Select showArrow={false} style={{ width: 70 }} disabled={true} />);
+  formItemLayout = {
+    labelCol: {
+      xs: { span: 16 },
+      sm: { span: 8 },
+    },
+    wrapperCol: {
+      xs: { span: 16 },
+      sm: { span: 16 },
+    },
+  };
 
-    const firstStepRenderer = () => (
-      <FormItem {...formItemLayout} label={<span>Country&nbsp;</span>}>
-        {getFieldDecorator('country', {
+  tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 16,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 8,
+      },
+    },
+  };
+
+  getFieldDecorator = this.props.form.getFieldDecorator;
+
+  // prefixSelector ;
+
+  firstStepRenderer = () => (
+      <FormItem {...this.formItemLayout} label={this.props.intl.formatMessage({
+        id: 'broker_modal_enter_country',
+        defaultMessage: 'Country',
+      })}>
+        {this.getFieldDecorator('country', {
           initialValue: '',
         })(
           <Select
             showSearch
-            style={{ width: '100%' }}
+            style={{width: '100%'}}
             placeholder={this.props.intl.formatMessage({
               id: 'tooltip.empty',
               defaultMessage: 'Please fill in this field',
@@ -155,142 +146,263 @@ class BrokerRegistration extends Component {
       </FormItem>
     );
 
-    const secondStepRenderer = () => (
-      <div>
-        <FormItem {...formItemLayout}>
-          {getFieldDecorator('platform', {
-            // initialValue: optionsPlatform[0].value,
-          })(
-            <Radio.Group
-              style={{ width: '100%' }}
-              placeholder={this.props.intl.formatMessage({
+  secondStepRenderer = () => (
+      <FormItem {...this.formItemLayout} label={this.props.intl.formatMessage({
+        id: 'broker_modal_enter_platform',
+        defaultMessage: 'Platform',
+      })}
+      >
+        {this.getFieldDecorator('platform')(
+          this.isExcludedCountry ? <div>No service provider available in your country</div>
+            : (
+              <Radio.Group
+                style={{width: '100%'}}
+                placeholder={this.props.intl.formatMessage({
+                  id: 'tooltip.empty',
+                  defaultMessage: 'Please fill in this field',
+                })}
+                onChange={this.changePlatform}
+                className="BrokerRegistration__radiogroup"
+              >
+                {_.map(optionsPlatform, option => {
+                  if (!option.excludedCountries.includes(this.state.currentCountryValue)) {
+                    const platformWobject = this.state.platformsWobjects.find(
+                      item => item.author_permlink === option.permlink,
+                    );
+                    const platformClientWobject = getClientWObj(platformWobject);
+                    return (
+                      <Radio
+                        key={option.value}
+                        value={option.value}
+                        className="BrokerRegistration__checkbox"
+                      >
+                        <a
+                          href={`https://www.waivio.com/object/${option.permlink}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <span>Languages: {option.languages.join(', ')}</span>
+                        </a>
+                        <ObjectCardView wObject={platformClientWobject} showSmallVersion/>
+                      </Radio>
+                    );
+                  }
+                })}
+              </Radio.Group>
+            )
+        )}
+      </FormItem>
+  );
+
+  thirdStepRenderer = () => (
+    <React.Fragment>
+      <FormItem {...this.formItemLayout} label={this.props.intl.formatMessage({
+        id: 'broker_modal_enter_first_name',
+        defaultMessage: 'First name',
+      })}>
+        {this.getFieldDecorator('firstName', {
+          rules: [
+            {
+              required: true,
+              message: this.props.intl.formatMessage({
+                id: 'broker_modal_valid_first_name',
+                defaultMessage: 'Please input your first name!',
+              }),
+              whitespace: true,
+            },
+          ],
+        })(<Input />)}
+      </FormItem>
+      <FormItem
+        {...this.formItemLayout}
+        label={this.props.intl.formatMessage({
+          id: 'broker_modal_enter_last_name',
+          defaultMessage: 'Last name',
+        })}
+      >
+        {this.getFieldDecorator('lastName', {
+          rules: [
+            {
+              required: true,
+              message: this.props.intl.formatMessage({
+                id: 'broker_modal_valid_last_name',
+                defaultMessage: 'Please input your last name!',
+              }),
+              whitespace: true,
+            },
+          ],
+        })(<Input />)}
+      </FormItem>
+      <FormItem
+        {...this.formItemLayout}
+        label={this.props.intl.formatMessage({
+          id: 'broker_modal_enter_e-mail',
+          defaultMessage: 'E-mail',
+        })}
+      >
+        {this.getFieldDecorator('email', {
+          rules: [
+            {
+              type: 'email',
+              message: this.props.intl.formatMessage({
+                id: 'tooltip.emailValid',
+                defaultMessage: 'Please enter a valid email',
+              }),
+            },
+            {
+              required: true,
+              message: this.props.intl.formatMessage({
                 id: 'tooltip.empty',
                 defaultMessage: 'Please fill in this field',
-              })}
-              onChange={this.changePlatform}
-              className="BrokerRegistration__radiogroup"
-            >
-              {_.map(optionsPlatform, option => {
-                if (option.countries.includes(this.state.currentCountryValue)) {
-                  const platformWobject = this.state.platformsWobjects.find(
-                    item => item.author_permlink === option.permlink,
-                  );
-                  const platformClientWobject = getClientWObj(platformWobject);
-                  return (
-                    <Radio
-                      key={option.value}
-                      value={option.value}
-                      className="BrokerRegistration__checkbox"
-                    >
-                      <a
-                        href={`https://www.waivio.com/object/${option.permlink}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <span>Broker page</span>
-                      </a>
-                      <ObjectCardView wObject={platformClientWobject} showSmallVersion />
-                    </Radio>
-                  );
-                }
-              })}
-            </Radio.Group>,
-          )}
-        </FormItem>
-      </div>
-    );
+              }),
+            },
+          ],
+        })(<Input />)}
+      </FormItem>
+      <FormItem
+        {...this.formItemLayout}
+        label={this.props.intl.formatMessage({
+          id: 'broker_modal_enter_passowrd',
+          defaultMessage: 'Password',
+        })}
+      >
+        {this.getFieldDecorator('password', {
+          rules: [
+            {
+              required: true,
+              message: this.props.intl.formatMessage({
+                id: 'broker_modal_enter_password',
+                defaultMessage: 'Please input your Password!',
+              }),
+            },
+            {
+              validator: this.validateToNextPassword,
+            },
+            {
+              min: 5,
+              message: this.props.intl.formatMessage({
+                id: 'broker_modal_valid_password',
+                defaultMessage: 'Require more then 5 symbols',
+              }),
+            },
+          ],
+        })(<Input type="password" />)}
+      </FormItem>
+      <FormItem
+        {...this.formItemLayout}
+        label={this.props.intl.formatMessage({
+          id: 'broker_modal_enter_confirmation',
+          defaultMessage: 'Confirmation',
+        })}
+      >
+        {this.getFieldDecorator('confirm', {
+          rules: [
+            {
+              required: true,
+              message: this.props.intl.formatMessage({
+                id: 'broker_modal_confirm_password',
+                defaultMessage: 'Please confirm your password!',
+              }),
+            },
+            { validator: this.compareToFirstPassword },
+          ],
+        })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
+      </FormItem>
 
-    const thirdStepRenderer = () => (
-      <React.Fragment>
-        <FormItem {...formItemLayout} label={<span>First name</span>}>
-          {getFieldDecorator('firstName', {
-            rules: [{ required: true, message: 'Please input your firstName!', whitespace: true }],
-          })(<Input />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label={<span>Last name</span>}>
-          {getFieldDecorator('lastName', {
-            rules: [{ required: true, message: 'Please input your lastName!', whitespace: true }],
-          })(<Input />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="E-mail">
-          {getFieldDecorator('email', {
-            rules: [
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ],
-          })(<Input />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="Password">
-          {getFieldDecorator('password', {
-            rules: [
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-              {
-                validator: this.validateToNextPassword,
-              },
-              { min: 5, message: 'Require more then 5 symbols' },
-            ],
-          })(<Input type="password" />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="Confirmation">
-          {getFieldDecorator('confirm', {
-            rules: [
-              { required: true, message: 'Please confirm your password!' },
-              { validator: this.compareToFirstPassword },
-              { min: 5, message: 'Require more then 5 symbols' },
-            ],
-          })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
-        </FormItem>
+      <FormItem {...this.formItemLayout} label={this.props.intl.formatMessage({
+        id: 'broker_modal_enter_phone_number',
+        defaultMessage: 'Phone Number',
+      })}>
+        {this.getFieldDecorator('phone', {
+          rules: [
+            {
+              required: true,
+              message: this.props.intl.formatMessage({
+                id: 'broker_modal_valid_phone_number',
+                defaultMessage: 'Please input your phone number!',
+              }),
+            },
+            {
+              min: 8,
+              message: this.props.intl.formatMessage({
+                id: 'broker_modal_valid_numbers_phone_number',
+                defaultMessage: 'Require more then 8 numbers',
+              }),
+            },
+            // { type: 'number', message: 'Only numbers' }
+          ],
+        })(
+          <Input addonBefore={this.getFieldDecorator(
+          'phoneCountry', {initialValue: phoneCode[this.state.currentCountryValue],})
+          (<Select showArrow={false} style={{ width: 70 }} disabled={true} />)} style={{ width: '100%' }} />)
+        }
+      </FormItem>
+      <FormItem {...this.tailFormItemLayout}>
+        <Checkbox className="d-flex align-items-center" onClick={this.handleReadChange}>
+          {this.props.intl.formatMessage({
+            id: 'broker_modal_i_have_read',
+            defaultMessage: 'I have read the',
+          })}{' '}
+          <a href={agreements[this.state.platformName]} target="_blank" rel="noopener noreferrer">
+            {this.props.intl.formatMessage({
+              id: 'broker_modal_agreement',
+              defaultMessage: 'agreement',
+            })}
+          </a>
+        </Checkbox>
+      </FormItem>
+    </React.Fragment>
+  );
 
-        <FormItem {...formItemLayout} label="Phone Number">
-          {getFieldDecorator('phone', {
-            rules: [
-              { required: true, message: 'Please input your phone number!' },
-              { min: 8, message: 'Require more then 8 numbers' },
-              // { type: 'number', message: 'Only numbers' }
-            ],
-          })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
-        </FormItem>
-        <FormItem {...tailFormItemLayout}>
-          <Checkbox className="d-flex align-items-center" onClick={this.handleReadChange}>
-            I have read the{' '}
-            <a href={agreements[this.state.platformName]} target="_blank" rel="noopener noreferrer">
-              agreement
-            </a>
-          </Checkbox>
-        </FormItem>
-        <FormItem {...tailFormItemLayout}>
-          <Button
-            className="w-100"
-            type="primary"
-            htmlType="submit"
-            disabled={!this.state.isAgreementRead}
-            loading={this.props.isLoading}
-          >
-            Register
-          </Button>
-        </FormItem>
-      </React.Fragment>
-    );
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        values.phoneNumber = values.phone.substring(2, values.phone.length - 1);
+        values.phoneOperator = values.phone.substring(0, 2);
+        values.country = this.state.currentCountryValue;
+        values.platform = this.state.platformName;
+        this.props.changeEmail(values.email);
+        this.props.registerBroker(values);
+      }
+    });
+  };
+
+  render() {
     return (
       <React.Fragment>
         <Form onSubmit={this.handleSubmit}>
           {this.state.stepNumber === 1
-            ? firstStepRenderer()
+            ? this.firstStepRenderer()
             : this.state.stepNumber === 2
-            ? secondStepRenderer()
-            : this.state.stepNumber === 3
-            ? thirdStepRenderer()
-            : null}
+              ? this.secondStepRenderer()
+              : this.state.stepNumber === 3
+                ? (
+                  <React.Fragment>
+                    {this.thirdStepRenderer()}
+                    <FormItem {...this.tailFormItemLayout}>
+                      <Button
+                        className="w-100"
+                        type="primary"
+                        htmlType="submit"
+                        disabled={!this.state.isAgreementRead}
+                        loading={this.props.isLoading}
+                      >
+                        {this.props.intl.formatMessage({
+                          id: 'broker_modal_register',
+                          defaultMessage: 'Register',
+                        })}
+                      </Button>
+                    </FormItem>
+                  </React.Fragment>
+                )
+                : null}
         </Form>
         <div className="BrokerRegistration__buttons">
           <Button onClick={this.handleStepBack} disabled={this.state.stepNumber === 1}>
