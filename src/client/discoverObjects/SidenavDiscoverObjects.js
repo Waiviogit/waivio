dc8da8417f57e0afed6de44730c2bb7f3f0cd9df
Vadim Burukin <vadimb@wizardsdev.com>
2019-10-30 15:38:05 +0200

    Removed brokers list form sidebar

--- src/client/discoverObjects/SidenavDiscoverObjects.js
+++ src/client/discoverObjects/SidenavDiscoverObjects.js
@@ -57,50 +57,51 @@ const menuConfig = {
   [menuSections.BROKERS]: {
     name: menuSections.BROKERS,
     intlId: 'brokers',
-    isCollapsible: true,
-    isCollapsed: true,
-    items: [
-      {
-        name: '770Capital',
-        intlId: BROKER['770CAPITAL'],
-        linkTo: '/object/gkm-770capital',
-      },
-      {
-        name: 'DowMarkets',
-        intlId: BROKER.DOWMARKETS,
-        linkTo: '/object/mrp-dowmarkets',
-      },
-      {
-        name: 'LimeFx',
-        intlId: BROKER.LIMEFX,
-        linkTo: '/object/xfx-limefx',
-      },
-      {
-        name: 'Maximarkets',
-        intlId: BROKER.MAXIMARKETS,
-        linkTo: '/object/wsp-maximarkets',
-      },
-      {
-        name: 'Maxitrade',
-        intlId: BROKER.MAXITRADE,
-        linkTo: '/object/lir-maxitrade',
-      },
-      {
-        name: 'TradeAllCrypto',
-        intlId: BROKER.TRADEALLCRYPTO,
-        linkTo: '/object/ftt-tradeallcrypto',
-      },
-      {
-        name: 'Tradiva',
-        intlId: BROKER.TRADIVA,
-        linkTo: '/object/wqt-tradiva',
-      },
-      {
-        name: 'Umarkets',
-        intlId: BROKER.UMARKETS,
-        linkTo: '/object/prj-4e4ges-umarkets',
-      },
-    ],
+    isCollapsible: false,
+    linkTo: '/discover-objects/brokers',
+    // isCollapsed: true,
+    // items: [
+    //   {
+    //     name: '770Capital',
+    //     intlId: BROKER['770CAPITAL'],
+    //     linkTo: '/object/gkm-770capital',
+    //   },
+    //   {
+    //     name: 'DowMarkets',
+    //     intlId: BROKER.DOWMARKETS,
+    //     linkTo: '/object/mrp-dowmarkets',
+    //   },
+    //   {
+    //     name: 'LimeFx',
+    //     intlId: BROKER.LIMEFX,
+    //     linkTo: '/object/xfx-limefx',
+    //   },
+    //   {
+    //     name: 'Maximarkets',
+    //     intlId: BROKER.MAXIMARKETS,
+    //     linkTo: '/object/wsp-maximarkets',
+    //   },
+    //   {
+    //     name: 'Maxitrade',
+    //     intlId: BROKER.MAXITRADE,
+    //     linkTo: '/object/lir-maxitrade',
+    //   },
+    //   {
+    //     name: 'TradeAllCrypto',
+    //     intlId: BROKER.TRADEALLCRYPTO,
+    //     linkTo: '/object/ftt-tradeallcrypto',
+    //   },
+    //   {
+    //     name: 'Tradiva',
+    //     intlId: BROKER.TRADIVA,
+    //     linkTo: '/object/wqt-tradiva',
+    //   },
+    //   {
+    //     name: 'Umarkets',
+    //     intlId: BROKER.UMARKETS,
+    //     linkTo: '/object/prj-4e4ges-umarkets',
+    //   },
+    // ],
   },
   [menuSections.HASHTAGS]: {
     name: menuSections.HASHTAGS,
