1d913cc94c91eea720323ecd05f5524ed7be3deb
konstantin <zakharchenko.konstantin@gmail.com>
2019-11-01 15:51:38 +0200

    edited visualization chart, edited titles, edited empty page content

--- src/client/user/UserStatistics/UserStatistics.js
+++ src/client/user/UserStatistics/UserStatistics.js
@@ -4,15 +4,34 @@ import UserAccuracyContainer from './UserAccuracyContainer/UserAccuracyContainer
 import UserForecastInstruments from './UserForecastInstruments/UserForecastInstruments';
 import UserInstrumentsTable from './UserInstrumentsTable/UserInstrumentsTable';
 import './UserStatistics.less';
+import { isEmpty } from 'lodash';
 
-const UserStatistics = ({ accuracy, forecasts, setSortOptions }) => (
-  <div className="UserStatistics">
-    <UserAccuracyContainer accuracy={accuracy} contentType={'forecast'} />
-    <UserAccuracyContainer accuracy={accuracy} contentType={'profitability'} />
-    <UserForecastInstruments forecasts={forecasts} />
-    <UserInstrumentsTable forecasts={forecasts} setSortOptions={setSortOptions} />
-  </div>
-);
+const UserStatistics = ({ accuracy, forecasts, setSortOptions }) => {
+  return (
+    <div className="UserStatistics">
+      {!isEmpty(accuracy) && (
+        <React.Fragment>
+          <UserAccuracyContainer accuracy={accuracy} contentType={'forecast'} />
+          <UserAccuracyContainer accuracy={accuracy} contentType={'profitability'} />
+        </React.Fragment>
+      )}
+      {!isEmpty(forecasts) && (
+        <React.Fragment>
+          <UserForecastInstruments forecasts={forecasts} />
+          <UserInstrumentsTable forecasts={forecasts} setSortOptions={setSortOptions} />
+        </React.Fragment>
+      )}
+      {isEmpty(forecasts) && isEmpty(accuracy) && (
+        <div className="UserStatistics empty-data">
+          {intl.formatMessage({
+            id: 'user_statistics_no_data',
+            defaultMessage: 'There is no statistics data yet',
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 UserStatistics.propTypes = {
   forecasts: PropTypes.arrayOf(PropTypes.shape()).isRequired,
