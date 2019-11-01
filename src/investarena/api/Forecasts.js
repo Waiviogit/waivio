e5b2caef2788968e4336005494fdd65160482acc
Vadim Burukin <vadimb@wizardsdev.com>
2019-11-01 09:28:20 +0200

    Fixed bug

--- src/investarena/api/Forecasts.js
+++ src/investarena/api/Forecasts.js
@@ -6,11 +6,24 @@ export default class Forecasts extends Base {
     super(params);
 
     this.getActiveForecasts = this.getActiveForecasts.bind(this);
+    this.getPostsWithForecastByUser = this.getPostsWithForecastByUser.bind(this);
   }
 
   getActiveForecasts(name = '', quote = '') {
     return this.apiClient
       .get(`${config.forecasts.activeForecasts}?name=${name}&quote=${quote}`)
+      .then(response => response.data);
+  }
+
+  getPostsWithForecastByUser(name = '') {
+    return this.apiClient
+      .get(`${config.posts.withForecastByUser}/${name}`)
+      .then(response => response.data);
+  }
+
+  getPostsWithForecastByWobject(wobjectName = '') {
+    return this.apiClient
+      .get(`${config.posts.withForecastBywobject}/${wobjectName}`)
       .then(response => response.data);
   }
 }
