a84bd835a4bbbd6fa657d9a270225bd331ac0b99
Roman <Waiviogit@gmail.com>
2019-11-06 22:18:43 +0200

    return back

--- webpack/configUtils.js
+++ webpack/configUtils.js
@@ -40,9 +40,7 @@ const DEFINE_PLUGIN = new webpack.DefinePlugin({
   'process.env.STEEMCONNECT_CLIENT_ID': JSON.stringify(
     process.env.STEEMCONNECT_CLIENT_ID || 'www.waivio.com',
   ),
-  'process.env.STEEMCONNECT_REDIRECT_URL': JSON.stringify(
-    process.env.STEEMCONNECT_REDIRECT_URL || `http://${CALLBACK_HOST_URL}/callback`,
-  ),
+  'process.env.STEEMCONNECT_REDIRECT_URL': JSON.stringify(`https://${CALLBACK_HOST_URL}/callback`),
   'process.env.STEEMCONNECT_HOST': JSON.stringify(
     process.env.STEEMCONNECT_HOST || 'https://steemconnect.com',
   ),
