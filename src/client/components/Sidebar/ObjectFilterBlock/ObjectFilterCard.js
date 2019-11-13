b0d9b966bc0f45b7092b93f2e0818eabbacf3bc2
konstantin <zakharchenko.konstantin@gmail.com>
2019-11-13 11:17:04 +0200

    added tooltips filter block count

--- src/client/components/Sidebar/ObjectFilterBlock/ObjectFilterCard.js
+++ src/client/components/Sidebar/ObjectFilterBlock/ObjectFilterCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Checkbox } from 'antd';
+import { Checkbox, Tooltip } from 'antd';
+import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import ObjectCard from '../ObjectCard';
 import './ObjectFilterBlock.less';
@@ -12,7 +13,15 @@ const ObjectFilterCard = ({ wobject }) => (
       </div>
       <ObjectCard key={wobject.author_permlink} wobject={wobject} showFollow={false} />
     </div>
-    <div className="ObjectFilterCard__count">{wobject.count}</div>
+    <div className="ObjectFilterCard__count">
+      <Tooltip
+        title={
+          <FormattedMessage id="object_filter_number_of_posts" defaultMessage="Number of posts" />
+        }
+      >
+        {wobject.count}
+      </Tooltip>
+    </div>
   </div>
 );
 
