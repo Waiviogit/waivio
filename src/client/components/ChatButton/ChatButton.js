b5ff202d3b4ad481b226e73a40eafe3ee173cb04
konstantin <zakharchenko.konstantin@gmail.com>
2019-11-28 17:19:12 +0200

    hide button on mobile

--- src/client/components/ChatButton/ChatButton.js
+++ src/client/components/ChatButton/ChatButton.js
@@ -1,41 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Button, Icon } from 'antd';
 import classNames from 'classnames';
+import { getScreenSize } from '../../reducers';
 import './ChatButton.less';
 
-const ChatButton = ({ authentication, openChat, isChat, messagesCount }) => (
-  <div className={classNames('ChatButton', { 'hide-element': !authentication })}>
-    <div className={classNames('ChatButton__item')}>
-      <Button onClick={openChat} type="primary" shape="circle">
-        {// eslint-disable-next-line no-nested-ternary
-        !isChat && messagesCount ? (
-          messagesCount > 99 ? (
-            '99+'
+const ChatButton = ({ authentication, openChat, isChat, messagesCount, screenSize }) => {
+  const isMobile = screenSize === 'xsmall' || screenSize === 'small';
+  return (
+    <div className={classNames('ChatButton', { 'hide-element': !authentication || isMobile })}>
+      <div className={classNames('ChatButton__item')}>
+        <Button onClick={openChat} type="primary" shape="circle">
+          {// eslint-disable-next-line no-nested-ternary
+          !isChat && messagesCount ? (
+            messagesCount > 99 ? (
+              '99+'
+            ) : (
+              messagesCount
+            )
+          ) : !isChat ? (
+            !messagesCount ? (
+              <Icon style={{ fontSize: '30px' }} type="message" />
+            ) : null
           ) : (
-            messagesCount
-          )
-        ) : !isChat ? (
-          !messagesCount ? (
-            <Icon style={{ fontSize: '30px' }} type="message" />
-          ) : null
-        ) : (
-          <Icon style={{ fontSize: '30px' }} type="close" />
-        )}
-      </Button>
+            <Icon style={{ fontSize: '30px' }} type="close" />
+          )}
+        </Button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ChatButton.propTypes = {
   isChat: PropTypes.bool.isRequired,
   openChat: PropTypes.func.isRequired,
   authentication: PropTypes.bool.isRequired,
   messagesCount: PropTypes.number,
+  screenSize: PropTypes.string,
 };
 
 ChatButton.defaultProps = {
   messagesCount: 0,
+  screenSize: '',
 };
 
-export default ChatButton;
+export default connect(state => ({
+  screenSize: getScreenSize(state),
+}))(ChatButton);
