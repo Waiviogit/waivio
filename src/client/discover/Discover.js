20ab2efeeef8cf1f97c8799d1aaffd6ba76adb5e
Roman <Waiviogit@gmail.com>
2019-12-11 12:40:02 +0200

    og image to discover objects

--- src/client/discover/Discover.js
+++ src/client/discover/Discover.js
@@ -34,12 +34,6 @@ const Discover = ({ intl }) => (
         })}{' '}
         - Waivio
       </title>
-      <meta
-        property="og:image"
-        content={
-          'https://cdn.steemitimages.com/DQmWxwUb1hpd3X2bSL9VrWbJvNxKXDS2kANWoGTkwi4RdwV/unknown.png'
-        }
-      />
     </Helmet>
     <div className="feed-layout container">
       <Affix className="leftContainer" stickPosition={77}>
