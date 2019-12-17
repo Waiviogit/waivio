f0502cef07674656e237fb02c35921f4907aec52
konstantin <zakharchenko.konstantin@gmail.com>
2019-12-17 17:20:55 +0200

    added authorization

--- src/client/rewards/Manage/Manage.js
+++ src/client/rewards/Manage/Manage.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { injectIntl } from 'react-intl';
-import './Manage.less';
 import * as ApiClient from '../../../waivioApi/ApiClient';
 import CampaignRewardsTable from './CampaignRewardsTable/CampaignRewardsTable';
 import BalanceTable from './BalanceTable/BalanceTable';
 import { activateCampaign, inactivateCampaign } from '../../user/userActions';
 import { getAuthenticatedUser } from '../../reducers';
+import Error401 from '../../statics/Error401';
+import './Manage.less';
 
 @injectIntl
 @connect(
@@ -92,47 +93,53 @@ class Manage extends React.Component {
 
   render() {
     // eslint-disable-next-line no-shadow
-    const { intl, activateCampaign, inactivateCampaign, user } = this.props;
+    const { intl, activateCampaign, inactivateCampaign, user, userName } = this.props;
     const { budgetTotal, campaigns } = this.state;
     const balanceContent = this.balanceContent();
     const rewardsCampaignContent = this.rewardsCampaignContent();
     return (
       <div className="Manage">
-        <div className="Manage__account-balance-wrap">
-          <div className="Manage__account-balance-wrap-title">
-            {intl.formatMessage({
-              id: 'manage_page_account_balance',
-              defaultMessage: `Account balance (STEEM)`,
-            })}
-          </div>
-          <BalanceTable intl={intl} budgetTotal={budgetTotal} user={user} />
-          <div className="Manage__account-balance-wrap-text-content">{balanceContent}</div>
-          <div className="Manage__rewards-campaign-wrap">
-            <div className="Manage__rewards-campaign-wrap-title">
-              {intl.formatMessage({
-                id: 'manage_page_manage_campaign',
-                defaultMessage: `Manage rewards campaign`,
-              })}
+        {userName ? (
+          <React.Fragment>
+            <div className="Manage__account-balance-wrap">
+              <div className="Manage__account-balance-wrap-title">
+                {intl.formatMessage({
+                  id: 'manage_page_account_balance',
+                  defaultMessage: `Account balance (STEEM)`,
+                })}
+              </div>
+              <BalanceTable intl={intl} budgetTotal={budgetTotal} user={user} />
+              <div className="Manage__account-balance-wrap-text-content">{balanceContent}</div>
+              <div className="Manage__rewards-campaign-wrap">
+                <div className="Manage__rewards-campaign-wrap-title">
+                  {intl.formatMessage({
+                    id: 'manage_page_manage_campaign',
+                    defaultMessage: `Manage rewards campaign`,
+                  })}
+                </div>
+                <CampaignRewardsTable
+                  activateCampaign={activateCampaign}
+                  inactivateCampaign={inactivateCampaign}
+                  campaigns={campaigns}
+                  userName={user.name}
+                />
+                <div className="Manage__rewards-campaign-wrap-text-content">
+                  {rewardsCampaignContent}
+                </div>
+              </div>
             </div>
-            <CampaignRewardsTable
-              activateCampaign={activateCampaign}
-              inactivateCampaign={inactivateCampaign}
-              campaigns={campaigns}
-              userName={user.name}
-            />
-            <div className="Manage__rewards-campaign-wrap-text-content">
-              {rewardsCampaignContent}
-            </div>
-          </div>
-        </div>
-        <button className="Manage__button">
-          <Link to={`/rewards/create`}>
-            {intl.formatMessage({
-              id: 'createNewCampaign',
-              defaultMessage: `Create new campaign`,
-            })}{' '}
-          </Link>
-        </button>
+            <button className="Manage__button">
+              <Link to={`/rewards/create`}>
+                {intl.formatMessage({
+                  id: 'createNewCampaign',
+                  defaultMessage: `Create new campaign`,
+                })}{' '}
+              </Link>
+            </button>
+          </React.Fragment>
+        ) : (
+          <Error401 />
+        )}
       </div>
     );
   }
