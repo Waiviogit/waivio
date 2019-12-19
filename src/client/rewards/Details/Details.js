906e22fa8937806b033033176b60ad2b533d570c
konstantin <zakharchenko.konstantin@gmail.com>
2019-12-19 10:35:09 +0200

    edited detail modal title, added local

--- src/client/rewards/Details/Details.js
+++ src/client/rewards/Details/Details.js
@@ -4,6 +4,7 @@ import { Button, Modal } from 'antd';
 import { injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import Avatar from '../../components/Avatar';
+import './Details.less';
 
 const Details = ({
   intl,
@@ -14,76 +15,77 @@ const Details = ({
   reserveOnClickHandler,
   assigned,
   isReserved,
-}) => {
-  console.log(objectDetails);
-  return (
-    <Modal
-      title={intl.formatMessage({
-        id: 'rewards_details_modal_details',
-        defaultMessage: 'Details',
-      })}
-      closable
-      onCancel={toggleModal}
-      maskClosable={false}
-      visible={isModalDetailsOpen}
-      wrapClassName="Rewards-modal"
-      footer={null}
-    >
-      <div className="Proposition__title">{objectDetails.name}</div>
-      <div className="Proposition__header">
-        <div className="Proposition__-type">{`${intl.formatMessage({
-          id: 'rewards_details_modal_sponsored',
-          defaultMessage: 'Sponsored',
-        })}: ${objectDetails.type === 'reviews' &&
-          intl.formatMessage({
-            id: 'rewards_details_modal_reviews',
-            defaultMessage: 'reviews',
-          })}`}</div>
-        <div className="Proposition__reward">{`${intl.formatMessage({
-          id: 'rewards_details_modal_reward',
-          defaultMessage: 'Reward',
-        })}: ${objectDetails.reward} STEEM`}</div>
-      </div>
-      <div className="Proposition__footer">
-        <div className="Proposition__author">
-          <div className="Proposition__author-title">
-            {intl.formatMessage({
-              id: 'rewards_details_modal_sponsor',
-              defaultMessage: 'Sponsor',
-            })}
-            :
-          </div>
-          <div className="Rewards-modal__user-card">
-            <Link to={`/@${objectDetails.guide.name}`}>
-              <Avatar username={objectDetails.guide.name} size={34} />
-            </Link>
-            <Link to={`/@${objectDetails.guide.name}`} title={objectDetails.guide.name}>
-              <span className="username">{objectDetails.guide.name}</span>
-            </Link>
-          </div>
-        </div>
-        <div>{`${intl.formatMessage({
-          id: 'rewards_details_modal_paid_rewards',
-          defaultMessage: 'Paid rewards',
-        })}: ${objectDetails.payed} STEEM (${objectDetails.payedPercent}%)`}</div>
-      </div>
-      <div className="Proposition__body">
-        <div className="Proposition__body-description">{objectDetails.description}</div>
-      </div>
-      <Button
-        type="primary"
-        loading={loading}
-        disabled={!(assigned !== null && !assigned && !isReserved) || loading}
-        onClick={reserveOnClickHandler}
-      >
+}) => (
+  <Modal
+    title={
+      <div className="Details__title">
         {intl.formatMessage({
-          id: 'reserve',
-          defaultMessage: `Reserve`,
+          id: 'rewards_details_seek_honest_reviews',
+          defaultMessage: 'We seek honest reviews',
         })}
-      </Button>
-    </Modal>
-  );
-};
+        !
+      </div>
+    }
+    closable
+    onCancel={toggleModal}
+    maskClosable={false}
+    visible={isModalDetailsOpen}
+    wrapClassName="Rewards-modal"
+    footer={null}
+  >
+    <div className="Details__header">
+      <div className="Proposition__-type">{`${intl.formatMessage({
+        id: 'rewards_details_modal_sponsored',
+        defaultMessage: 'Sponsored',
+      })}: ${objectDetails.type === 'reviews' &&
+        intl.formatMessage({
+          id: 'rewards_details_modal_reviews',
+          defaultMessage: 'reviews',
+        })}`}</div>
+      <div className="Proposition__reward">{`${intl.formatMessage({
+        id: 'rewards_details_modal_reward',
+        defaultMessage: 'Reward',
+      })}: ${objectDetails.reward} STEEM`}</div>
+    </div>
+    <div className="Proposition__footer">
+      <div className="Proposition__author">
+        <div className="Proposition__author-title">
+          {intl.formatMessage({
+            id: 'rewards_details_modal_sponsor',
+            defaultMessage: 'Sponsor',
+          })}
+          :
+        </div>
+        <div className="Rewards-modal__user-card">
+          <Link to={`/@${objectDetails.guide.name}`}>
+            <Avatar username={objectDetails.guide.name} size={34} />
+          </Link>
+          <Link to={`/@${objectDetails.guide.name}`} title={objectDetails.guide.name}>
+            <span className="username">{objectDetails.guide.name}</span>
+          </Link>
+        </div>
+      </div>
+      <div>{`${intl.formatMessage({
+        id: 'rewards_details_modal_paid_rewards',
+        defaultMessage: 'Paid rewards',
+      })}: ${objectDetails.payed} STEEM (${objectDetails.payedPercent}%)`}</div>
+    </div>
+    <div className="Proposition__body">
+      <div className="Proposition__body-description">{objectDetails.description}</div>
+    </div>
+    <Button
+      type="primary"
+      loading={loading}
+      disabled={!(assigned !== null && !assigned && !isReserved) || loading}
+      onClick={reserveOnClickHandler}
+    >
+      {intl.formatMessage({
+        id: 'reserve',
+        defaultMessage: `Reserve`,
+      })}
+    </Button>
+  </Modal>
+);
 
 Details.propTypes = {
   intl: PropTypes.shape().isRequired,
