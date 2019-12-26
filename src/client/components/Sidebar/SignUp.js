35743b308b0843b7bd252613ac8218b96cdd4ea0
Vladimir Klimenko <klimbox@gmail.com>
2019-12-26 15:48:14 +0200

    steem ninja's disabled

--- src/client/components/Sidebar/SignUp.js
+++ src/client/components/Sidebar/SignUp.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import './SidebarBlock.less';
-import ModalSignUp from '../Navigation/ModalSignUp/ModalSignUp';
+// import ModalSignUp from '../Navigation/ModalSignUp/ModalSignUp';
 
 const SignUp = () => (
   <div className="SidebarBlock">
     <h3 className="SidebarBlock__title">
       <FormattedMessage id="new_to_busy" defaultMessage="New to InvestArena?" />
     </h3>
-    <ModalSignUp isButton />
+    {/* <ModalSignUp isButton /> */}
+    <a target="_blank" rel="noopener noreferrer" href={process.env.SIGNUP_URL}>
+      <button className="SidebarBlock__button">
+        <FormattedMessage id="signup" defaultMessage="Sign up" />
+      </button>
+    </a>
   </div>
 );
 
