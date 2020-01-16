4177668d129f427632a81ec2df032b9b71953da5
Roman <Waiviogit@gmail.com>
2020-01-16 10:00:09 +0200

    remove unused code

--- src/investarena/redux/selectors/dealsSelectors.js
+++ src/investarena/redux/selectors/dealsSelectors.js
@@ -4,6 +4,7 @@ const getDealsState = state => state.deals;
 // reselect function
 export const getOpenDealsState = createSelector([getDealsState], deals => deals.open);
 export const getClosedDealsState = createSelector([getDealsState], deals => deals.closed);
+
 export const makeGetPostDealsState = () =>
   createSelector(
     getDealsState,
