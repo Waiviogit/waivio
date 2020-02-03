d6608d07849a3a2eac449a774e074f1def5f044f
Vladimir Klimenko <klimbox@gmail.com>
2020-02-03 16:27:28 +0200

    firefox regexp issue

--- src/client/helpers/postHelpers.js
+++ src/client/helpers/postHelpers.js
@@ -37,7 +37,7 @@ export function dropCategory(url) {
 }
 
 export const replaceBotWithGuestName = (url, guestInfo) =>
-  url && guestInfo && guestInfo.userId ? url.replace(botNameRegex, guestInfo.userId) : url;
+  url && guestInfo && guestInfo.userId ? url.replace(botNameRegex, `@${guestInfo.userId}`) : url;
 
 /**
  * Gets app data from a post.
