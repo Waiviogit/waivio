cf347b4eb692e5ddd5f3c9bcfbe44a89ae0d7ce4
Vladimir Klimenko <klimbox@gmail.com>
2020-02-04 10:51:29 +0200

    waiv-873::post side buttons redesign

--- src/client/components/EditorExtended/components/sides/VideoSideButton.js
+++ src/client/components/EditorExtended/components/sides/VideoSideButton.js
@@ -58,7 +58,10 @@ class VideoSideButton extends Component {
         })}
         onClick={this.onClick}
       >
-        <Icon type="video-camera" className="video-btn" />
+        <Icon type="video-camera" className="btn-icon" />
+        <span className="action-btn__caption">
+          {this.props.intl.formatMessage({ id: 'video', defaultMessage: 'Video' })}
+        </span>
       </button>
     );
   }
