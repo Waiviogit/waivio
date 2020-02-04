00a7cb99b37e370aea1bc9b61c6ef142e584d10e
Vladimir Klimenko <klimbox@gmail.com>
2020-02-04 17:05:50 +0200

    waiv-873::embedded dTube video

--- src/client/components/EditorExtended/util/videoHelper.js
+++ src/client/components/EditorExtended/util/videoHelper.js
@@ -1,12 +1,16 @@
 const YOUTUBEMATCH_URL = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
 const VIMEOMATCH_URL = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)/; // eslint-disable-line no-useless-escape
+const DTUBEMATCH_URL = /^https:\/\/(emb\.)?d\.tube(\/#!)?(\/v)?\/([^/"]+\/[^/"]+)$/;
 
 const YOUTUBE_PREFIX = 'https://www.youtube.com/embed/';
 const VIMEO_PREFIX = 'https://player.vimeo.com/video/';
+const DTUBE_PREFIX = 'https://emb.d.tube/#!/';
 
 export const isYoutube = url => YOUTUBEMATCH_URL.test(url);
 
 export const isVimeo = url => VIMEOMATCH_URL.test(url);
+
+export const isDTube = url => DTUBEMATCH_URL.test(url);
 
 export const getYoutubeSrc = url => {
   const id = url && url.match(YOUTUBEMATCH_URL)[1];
@@ -26,6 +30,15 @@ export const getVimeoSrc = url => {
   };
 };
 
+export const getDTubeSrc = url => {
+  const id = url.match(DTUBEMATCH_URL)[4];
+  return {
+    srcID: id,
+    srcType: 'dtube',
+    url,
+  };
+};
+
 export const getSrc = ({ src }) => {
   if (isYoutube(src)) {
     const { srcID } = getYoutubeSrc(src);
@@ -35,6 +48,11 @@ export const getSrc = ({ src }) => {
     const { srcID } = getVimeoSrc(src);
     return `${VIMEO_PREFIX}${srcID}`;
   }
+  if (isDTube(src)) {
+    const { srcID } = getDTubeSrc(src);
+    return `${DTUBE_PREFIX}${srcID}`;
+  }
+
   return undefined;
 };
 
