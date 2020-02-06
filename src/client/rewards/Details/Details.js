c3447cf9698f124980923c00332004aadad5b950
konstantin <zakharchenko.konstantin@gmail.com>
2020-02-06 16:01:04 +0200

    added cards on discover

--- src/client/rewards/Details/Details.js
+++ src/client/rewards/Details/Details.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Modal, Checkbox } from 'antd';
+import { Button, Modal } from 'antd';
 import { injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import { isEmpty } from 'lodash';
@@ -44,53 +44,53 @@ const Details = ({
     >
       <CampaignCardHeader campaignData={objectDetails} isDetails />
       <div className="Details__text-wrap">
-        {!isReviewDetails && (
-          <React.Fragment>
-            <div className="Details__text fw6 mv3">{messageData.eligibilityRequirements}:</div>
-            <div className="Details__text mv3">{messageData.eligibilityCriteriaParticipate}</div>
-            <div className="Details__criteria-wrap">
-              <div className="Details__criteria-row">
-                <Checkbox checked={objectDetails.requirement_filters.expertise} disabled />
-                <div>{`${messageData.minimumWaivioExpertise}: ${objectDetails.userRequirements.minExpertise}`}</div>
-              </div>
-              <div className="Details__criteria-row">
-                <Checkbox checked={objectDetails.requirement_filters.followers} disabled />
-                <div>{`${messageData.minimumNumberFollowers}: ${objectDetails.userRequirements.minFollowers}`}</div>
-              </div>
-              <div className="Details__criteria-row">
-                <Checkbox checked={objectDetails.requirement_filters.posts} disabled />
-                <div>{`${messageData.minimumNumberPosts}: ${objectDetails.userRequirements.minPosts}`}</div>
-              </div>
-              {!!objectDetails.frequency_assign && (
-                <div className="Details__criteria-row">
-                  <Checkbox checked disabled />
-                  <div>
-                    {messageData.receivedRewardFrom}
-                    <Link
-                      to={`/@${objectDetails.guide.name}`}
-                    >{` @${objectDetails.guide.name} `}</Link>
-                    {messageData.forReviewing}
-                    <Link
-                      className="nowrap"
-                      to={`/object/${objectDetails.requiredObject}`}
-                    >{` ${objectDetails.name} `}</Link>
-                    {messageData.inTheLast}
-                  </div>
-                </div>
-              )}
-              <div className="Details__criteria-row">
-                <Checkbox checked={objectDetails.requirement_filters.not_blacklisted} disabled />
-                <div>
-                  {messageData.accountNotBlacklisted}
-                  <Link
-                    to={`/@${objectDetails.guide.name}`}
-                  >{` @${objectDetails.guide.name} `}</Link>
-                  {messageData.referencedAccounts}
-                </div>
-              </div>
-            </div>
-          </React.Fragment>
-        )}
+        {/* {!isReviewDetails && ( */}
+        {/*  <React.Fragment>* /}
+        {/*    <div className="Details__text fw6 mv3">{messageData.eligibilityRequirements}:</div> */}
+        {/*    <div className="Details__text mv3">{messageData.eligibilityCriteriaParticipate}</div> */}
+        {/*    <div className="Details__criteria-wrap"> */}
+        {/*      <div className="Details__criteria-row"> */}
+        {/*        <Checkbox checked={objectDetails.requirement_filters.expertise} disabled /> */}
+        {/*        <div>{`${messageData.minimumWaivioExpertise}: ${objectDetails.userRequirements.minExpertise}`}</div> */}
+        {/*      </div> */}
+        {/*      <div className="Details__criteria-row"> */}
+        {/*        <Checkbox checked={objectDetails.requirement_filters.followers} disabled /> */}
+        {/*        <div>{`${messageData.minimumNumberFollowers}: ${objectDetails.userRequirements.minFollowers}`}</div> */}
+        {/*      </div> */}
+        {/*      <div className="Details__criteria-row"> */}
+        {/*        <Checkbox checked={objectDetails.requirement_filters.posts} disabled /> */}
+        {/*        <div>{`${messageData.minimumNumberPosts}: ${objectDetails.userRequirements.minPosts}`}</div> */}
+        {/*      </div> */}
+        {/*      {!!objectDetails.frequency_assign && ( */}
+        {/*        <div className="Details__criteria-row"> */}
+        {/*          <Checkbox checked disabled /> */}
+        {/*          <div> */}
+        {/*            {messageData.receivedRewardFrom} */}
+        {/*            <Link  */}
+        {/*              to={`/@${objectDetails.guide.name}`} */}
+        {/*            >{` @${objectDetails.guide.name} `}</Link> */}
+        {/*            {messageData.forReviewing} */}
+        {/*            <Link */}
+        {/*              className="nowrap" */}
+        {/*              to={`/object/${objectDetails.requiredObject}`} */}
+        {/*            >{` ${objectDetails.name} `}</Link> */}
+        {/*            {messageData.inTheLast} */}
+        {/*          </div> */}
+        {/*        </div> */}
+        {/*      )} */}
+        {/*      <div className="Details__criteria-row"> */}
+        {/*        <Checkbox checked={objectDetails.requirement_filters.not_blacklisted} disabled /> */}
+        {/*        <div> */}
+        {/*          {messageData.accountNotBlacklisted} */}
+        {/*          <Link */}
+        {/*            to={`/@${objectDetails.guide.name}`} */}
+        {/*          >{` @${objectDetails.guide.name} `}</Link> */}
+        {/*          {messageData.referencedAccounts} */}
+        {/*        </div> */}
+        {/*      </div> */}
+        {/*    </div> */}
+        {/*  </React.Fragment> */}
+        {/* )} */}
         <div className="Details__text fw6 mv3">{messageData.postRequirements}:</div>
         <div className="Details__text mv3">{messageData.reviewEligibleAward}</div>
         <div className="Details__criteria-wrap">
