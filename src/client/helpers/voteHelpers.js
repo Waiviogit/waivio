657168280bf2b108763aa360b2c34e699bb5b674
LucyHrinishyna <liudmylah@wizardsdev.com>
2020-02-24 14:28:03 +0200

    style code

--- src/client/helpers/voteHelpers.js
+++ src/client/helpers/voteHelpers.js
@@ -3,12 +3,16 @@ import { includes } from 'lodash';
 export const getUpvotes = activeVotes => activeVotes.filter(vote => vote.percent > 0);
 
 export const getAppendUpvotes = activeVotes =>
-  activeVotes.filter(vote => vote.percent > 0 && vote.percent % 10 === 0 || vote.percent <= 100 && vote.percent % 10 !== 0);
+  activeVotes.filter(
+    vote =>
+      (vote.percent > 0 && vote.percent % 10 === 0) ||
+      (vote.percent <= 100 && vote.percent % 10 !== 0),
+  );
 
 export const getDownvotes = activeVotes => activeVotes.filter(vote => vote.percent < 0);
 
 export const getAppendDownvotes = activeVotes =>
-  activeVotes.filter(vote => vote.percent > 100 && vote.percent % 10 !== 0 || vote.percent < 0);
+  activeVotes.filter(vote => (vote.percent > 100 && vote.percent % 10 !== 0) || vote.percent < 0);
 
 export const getFollowingUpvotes = (activeVotes, following) =>
   getUpvotes(activeVotes).filter(vote => includes(following, vote.voter));
