46c09512e26f380212856ecd7c65db9e728f05fa
Vladimir Klimenko <klimbox@gmail.com>
2020-03-13 15:52:44 +0200

    set api prefixes to get data from crypto-investarena server

--- src/waivioApi/routes.js
+++ src/waivioApi/routes.js
@@ -1,12 +1,15 @@
 import config from './config.json';
+import iaConfig from '../investarena/etc/client-config.json';
 
 export const baseUrl = config[process.env.NODE_ENV].protocol + config[process.env.NODE_ENV].host;
+const cryptoArenaBaseUrl = iaConfig[process.env.NODE_ENV].apiPrefix;
 
 const routes = {
   ...config,
   appName: config[process.env.NODE_ENV].appName,
   baseUrl,
   apiPrefix: baseUrl + config.apiPrefix,
+  bxyApiPrefix: cryptoArenaBaseUrl + config.bxyApiPrefix,
   objectsBotApiPrefix: baseUrl + config.objectsBotApiPrefix,
   campaignApiPrefix: baseUrl + config.campaignApiPrefix,
 };
