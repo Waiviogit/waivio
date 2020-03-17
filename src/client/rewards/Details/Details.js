aba8f48583031cf231073cec40f9949ab78ea823
oksana <oksanana@wizardsdev.com>
2020-03-17 16:57:29 +0200

    waiv-1369

--- src/client/rewards/Details/Details.js
+++ src/client/rewards/Details/Details.js
@@ -133,7 +133,7 @@ const Details = ({
             {/* eslint-disable-next-line no-plusplus */}
             {`${indexItem++}. ${messageData.linkTo}`}
             <Link className="ml1" to={`/object/${objectDetails.requiredObject}`}>
-              {objectDetails.name}
+              {objectDetails.required_object.default_name}
             </Link>
             <span className="no-visible">
               :
