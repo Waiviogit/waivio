e74f6b87fa3c5d103011c4374c7816cb491ad2e5
konstantin <zakharchenko.konstantin@gmail.com>
2020-03-27 19:25:12 +0200

    check error on beaxy session

--- src/client/auth/authActions.js
+++ src/client/auth/authActions.js
@@ -82,7 +82,6 @@ export const beaxyLogin = (userData, bxySessionData) => (dispatch, getState, { w
           userMetaData,
           socialNetwork: 'beaxy',
           isGuestUser: true,
-          isBeaxyUser: true,
         });
       } catch (e) {
         dispatch(notify(e.error.details[0].message));
