2ba18c284f88997bdee1dcefd7151b65d12c875e
konstantin1390 <51738018+konstantin1390@users.noreply.github.com>
2020-03-31 18:33:40 +0300

    Update authActions.js

--- src/client/auth/authActions.js
+++ src/client/auth/authActions.js
@@ -105,7 +105,7 @@ export const login = (oAuthToken = '', socialNetwork = '', regData = '') => asyn
   { steemConnectAPI, waivioAPI },
 ) => {
   // todo: call beaxy login
-  if (socialNetwork === 'beaxy') 
+  if (socialNetwork === 'beaxy')
     return dispatch(beaxyLogin(regData.userData, regData.bxySessionData));
   const state = getState();
   let promise = Promise.resolve(null);
