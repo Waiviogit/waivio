66fba817aa8a9a82686d9be582b7ebe435d2a0fc
Vladimir Klimenko <klimbox@gmail.com>
2020-04-02 10:54:44 +0300

    waiv-1540::prop types reorder

--- src/investarena/components/CreatePostForecast/CreatePostForecast.js
+++ src/investarena/components/CreatePostForecast/CreatePostForecast.js
@@ -31,14 +31,17 @@ import './CreatePostForecast.less';
 }))
 class CreatePostForecast extends Component {
   static propTypes = {
+    /* infectIntl */
     intl: PropTypes.shape().isRequired,
+    /* connect */
     quotesSettings: PropTypes.shape(),
+    optionsQuote: PropTypes.arrayOf(PropTypes.shape()),
     quoteSelected: PropTypes.shape(),
+    /* passed props */
     isPosted: PropTypes.bool,
     isUpdating: PropTypes.bool,
     forecastValues: PropTypes.shape(),
     onChange: PropTypes.func,
-    optionsQuote: PropTypes.arrayOf(PropTypes.shape()),
   };
 
   static defaultProps = {
