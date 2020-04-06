5b476be698cd2bb5ee06cc6c79c10b4f5c288c3a
AlexandrGavrilov <56414833+AlexandrGavrilov@users.noreply.github.com>
2020-04-06 16:52:51 +0300

    add tests for components and reducers

--- src/client/components/Button/__tests__/Action.js
+++ src/client/components/Button/__tests__/Action.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Action from '../Action';
 
 describe('<Action />', () => {
@@ -11,5 +11,45 @@ describe('<Action />', () => {
     };
     const wrapper = shallow(<Action {...props}>Example text</Action>);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should include Action--primary in className', () => {
+    const props = {
+      loading: true,
+      disabled: false,
+      primary: true,
+    };
+    const wrapper = shallow(<Action {...props}>Example text</Action>);
+    expect(wrapper.props().className).toBe('Action Action--primary');
+  });
+
+  it('should include Action--big in className', () => {
+    const props = {
+      loading: true,
+      disabled: false,
+      big: true,
+    };
+    const wrapper = shallow(<Action {...props}>Example text</Action>);
+    expect(wrapper.props().className).toBe('Action Action--big');
+  });
+
+  it('should render Icon', () => {
+    const props = {
+      loading: true,
+      disabled: false,
+      big: true,
+    };
+    const wrapper = mount(<Action {...props}>Example text</Action>);
+    expect(wrapper.find('Icon')).not.toBeUndefined();
+  });
+
+  it('should have Text as a child', () => {
+    const props = {
+      loading: true,
+      disabled: false,
+      big: true,
+    };
+    const wrapper = mount(<Action {...props}>Example text</Action>);
+    expect(wrapper.text()).toBe('Example text');
   });
 });
