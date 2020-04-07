857c07082eac0f540dda17b11dcf546940de3dc4
konstantin <zakharchenko.konstantin@gmail.com>
2020-04-07 17:19:55 +0300

    added on logged out menu

--- src/client/components/Navigation/MobileMenu/MobileMenu.js
+++ src/client/components/Navigation/MobileMenu/MobileMenu.js
@@ -66,18 +66,18 @@ const MobileMenu = props => {
     const notificationsCount = isUndefined(lastSeenTimestamp)
       ? size(notifications)
       : size(
-        filter(
-          notifications,
-          notification =>
-            lastSeenTimestamp < notification.timestamp &&
-            includes(PARSED_NOTIFICATIONS, notification.type),
-        ),
-      );
+          filter(
+            notifications,
+            notification =>
+              lastSeenTimestamp < notification.timestamp &&
+              includes(PARSED_NOTIFICATIONS, notification.type),
+          ),
+        );
     const displayBadge = notificationsCount > 0;
     const notificationsCountDisplay = notificationsCount > 99 ? '99+' : notificationsCount;
     return (
       <div className="Topnav__menu-container">
-        <ModalBroker/>
+        <ModalBroker />
         <Menu selectedKeys={[]} className="Topnav__menu" mode="horizontal">
           <Menu.Item className="Topnav__menu-item" key="write">
             <BTooltip
@@ -87,7 +87,7 @@ const MobileMenu = props => {
               mouseEnterDelay={1}
             >
               <Link to="/editor" className="Topnav__link Topnav__link--action">
-                <i className="iconfont icon-write"/>
+                <i className="iconfont icon-write" />
               </Link>
             </BTooltip>
           </Menu.Item>
@@ -124,7 +124,7 @@ const MobileMenu = props => {
                   {displayBadge ? (
                     <div className="Topnav__notifications-count">{notificationsCountDisplay}</div>
                   ) : (
-                    <i className="iconfont icon-remind"/>
+                    <i className="iconfont icon-remind" />
                   )}
                 </a>
               </PopoverContainer>
@@ -135,33 +135,47 @@ const MobileMenu = props => {
     );
   };
 
-  const menuContainer = () => (
-    <div className="MenuContainer">
+  const loggedInMenuContainer = (
+    <React.Fragment>
       <Link to="/my_feed" className="MenuContainer__link">
-        <FormattedMessage id="my_feed" defaultMessage="My feed"/>
+        <FormattedMessage id="my_feed" defaultMessage="My feed" />
       </Link>
       <Link to="/discover" className="MenuContainer__link">
-        <FormattedMessage id="discover" defaultMessage="Discover"/>
+        <FormattedMessage id="discover" defaultMessage="Discover" />
       </Link>
       <Link to="/drafts" className="MenuContainer__link">
-        <FormattedMessage id="drafts" defaultMessage="Drafts"/>
+        <FormattedMessage id="drafts" defaultMessage="Drafts" />
       </Link>
       <Link to="/settings" className="MenuContainer__link">
-        <FormattedMessage id="settings" defaultMessage="Settings"/>
+        <FormattedMessage id="settings" defaultMessage="Settings" />
       </Link>
       <Link to="/replies" className="MenuContainer__link">
-        <FormattedMessage id="replies" defaultMessage="Replies"/>
+        <FormattedMessage id="replies" defaultMessage="Replies" />
       </Link>
       <Link to="/wallet" className="MenuContainer__link">
-        <FormattedMessage id="wallet" defaultMessage="Wallet"/>
+        <FormattedMessage id="wallet" defaultMessage="Wallet" />
       </Link>
       <Link to="/about" className="MenuContainer__link">
-        <FormattedMessage id="about" defaultMessage="About"/>
+        <FormattedMessage id="about" defaultMessage="About" />
       </Link>
       <div className="MenuContainer__link" onClick={onLogout} role="presentation">
-        <FormattedMessage id="logout" defaultMessage="Logout"/>
+        <FormattedMessage id="logout" defaultMessage="Logout" />
       </div>
-    </div>
+    </React.Fragment>
+  );
+
+  const loggedOutMenuContainer = (
+    <React.Fragment>
+      <Link to="/" className="MenuContainer__link">
+        <FormattedMessage id="home" defaultMessage="Home" />
+      </Link>
+      <Link to="/discover" className="MenuContainer__link">
+        <FormattedMessage id="discover" defaultMessage="Discover" />
+      </Link>
+      <Link to="/about" className="MenuContainer__link">
+        <FormattedMessage id="about" defaultMessage="About" />
+      </Link>
+    </React.Fragment>
   );
 
   return (
@@ -170,11 +184,11 @@ const MobileMenu = props => {
         {username && (
           <div className="UserData">
             <span className="UserData__broker-balance">
-              <BrokerBalance isMobile/>
+              <BrokerBalance isMobile />
             </span>
             <span className="UserData__user">
               <Link to={`/@${username}`} onClick={handleScrollToTop}>
-                <Avatar username={username} size={50}/>
+                <Avatar username={username} size={50} />
               </Link>
             </span>
           </div>
@@ -206,16 +220,16 @@ const MobileMenu = props => {
               autoCorrect="off"
             />
           </AutoComplete>
-          <i className="iconfont icon-search"/>
+          <i className="iconfont icon-search" />
         </div>
-        <div>
-          {buttonsMenuContainer()}
-          {menuContainer()}
+        <div>{buttonsMenuContainer()}</div>
+        <div className="MenuContainer">
+          {username ? loggedInMenuContainer : loggedOutMenuContainer}
         </div>
         {!isBeaxyUser && platformName === 'beaxy' && (
           <div className="MobileMenu__disconnect-broker">
             <Button onClick={disconnectPlatform}>
-              <FormattedMessage id="disconnect_broker" defaultMessage="Disconnect broker"/>
+              <FormattedMessage id="disconnect_broker" defaultMessage="Disconnect broker" />
             </Button>
           </div>
         )}
