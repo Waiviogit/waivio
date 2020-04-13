1294c3d119c4b6d77ef4f567b7d31f8838da021d
Vladimir Klimenko <klimbox@gmail.com>
2020-04-13 19:15:25 +0300

    following instruments - change lib for chart

--- src/investarena/components/InstrumentsPage/Instrument/InstrumentChart/InstrumentChart.js
+++ src/investarena/components/InstrumentsPage/Instrument/InstrumentChart/InstrumentChart.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { AreaChart } from 'react-easy-chart';
+import { maxBy, minBy, memoize } from 'lodash';
+import Chart from 'react-google-charts';
 
 const InstrumentChart = props => {
   const { chart, width, height, noDataMsg, onClick } = props;
+  const getMaxValue = memoize(chartData => {
+      const point = maxBy(chartData, p => p.y);
+      return point && point.y;
+    }
+  );
+  const getMinValue = memoize(chartData => {
+      const point = minBy(chartData, p => p.y);
+      return point && point.y;
+    }
+  );
   return chart && chart.length !== 0 ? (
     <div role="presentation" className="st-card__chart" onClick={onClick}>
-      <AreaChart width={width} height={height} areaColors={['#3a79ee']} data={[chart]} />
+      <Chart
+        width={width}
+        height={height}
+        chartType="AreaChart"
+        data={[['', ''], ...chart]}
+        options={{
+          backgroundColor: 'transparent',
+          colors: ['#3a79ee'],
+          chartArea: { left: 0, top: 0, width: '100%', height: '100%' },
+          legend: 'none',
+          enableInteractivity: false,
+          height,
+          lineWidth: 1,
+          hAxis: {
+            baselineColor: 'transparent',
+            gridlines: {
+              color: 'transparent',
+            },
+          },
+          vAxis: {
+            baselineColor: 'transparent',
+            gridlines: {
+              color: 'transparent',
+            },
+            viewWindow: {
+              max: getMaxValue(chart),
+              min: getMinValue(chart),
+            },
+          },
+        }}
+      />
     </div>
   ) : (
     <div className="st-assets-chart-no-data">{noDataMsg}</div>
