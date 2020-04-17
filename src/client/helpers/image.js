e136bffd027d9ba3eca8fd0ec9396ef1f44db5e5
Vladimir Klimenko <klimbox@gmail.com>
2020-04-17 11:54:22 +0300

    fix

--- src/client/helpers/image.js
+++ src/client/helpers/image.js
@@ -1,17 +1,22 @@
 import filesize from 'filesize';
 
-const IMG_PREVIEW = '?width=800&height=600&format=webp&mode=fit';
-const IMG_PROXY_SMALL = 'https://images.hive.blog/p/';
+const IMG_PROXY = 'https://images.hive.blog/0x0/';
+const IMG_PROXY_SMALL = 'https://images.hive.blog/40x40/';
+const IMG_PROXY_PREVIEW = 'https://images.hive.blog/800x600/';
 
 export const MAXIMUM_UPLOAD_SIZE = 15728640;
 export const MAXIMUM_UPLOAD_SIZE_HUMAN = filesize(MAXIMUM_UPLOAD_SIZE);
 
 export const getProxyImageURL = (url, type) => {
-  if (type === 'preview') {
-    return `${url}${IMG_PREVIEW}`;
-  } else if (type === 'small') {
-    return `${IMG_PROXY_SMALL}${url}`;
-  }
+  if (
+    url.includes('ipfs.io') ||
+    url.indexOf('https://ipfs.busy.org') === 0 ||
+    url.indexOf('https://img.esteem.ws') === 0
+  )
+    return `${IMG_PROXY}${url}`;
+  else if (type === 'preview') return `${IMG_PROXY_PREVIEW}${url}`;
+  else if (type === 'small') return `${IMG_PROXY_SMALL}${url}`;
+
   return url;
 };
 
