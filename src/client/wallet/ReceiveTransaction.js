12366942a14ad66b063015366aeed83608acede4
oksana <oksanana@wizardsdev.com>
2020-04-27 14:04:29 +0300

    fixed 1852

--- src/client/wallet/ReceiveTransaction.js
+++ src/client/wallet/ReceiveTransaction.js
@@ -1,48 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import classNames from 'classnames';
 import { FormattedMessage, FormattedRelative, FormattedDate, FormattedTime } from 'react-intl';
 import BTooltip from '../components/BTooltip';
+import Avatar from '../components/Avatar';
+import { isGuestUser } from '../reducers';
 
-const ReceiveTransaction = ({ from, memo, amount, timestamp }) => (
-  <div className="UserWalletTransactions__transaction">
-    <div className="UserWalletTransactions__icon-container">
-      <i className="iconfont icon-success_fill UserWalletTransactions__icon" />
-    </div>
-    <div className="UserWalletTransactions__content">
-      <div className="UserWalletTransactions__content-recipient">
-        <div>
-          <FormattedMessage
-            id="author_rewards"
-            defaultMessage="Author rewards"
-            values={{
-              username: (
-                <Link to={`/@${from}`}>
-                  <span className="username">{from}</span>
-                </Link>
-              ),
-            }}
-          />
+const ReceiveTransaction = ({ from, memo, amount, timestamp }) => {
+  const isGuest = useSelector(isGuestUser);
+  return (
+    <div className="UserWalletTransactions__transaction">
+      {isGuest ? (
+        <div className="UserWalletTransactions__icon-container">
+          <i className="iconfont icon-success_fill UserWalletTransactions__icon" />
         </div>
-        <div className="UserWalletTransactions__received">{amount}</div>
-      </div>
-      <span className="UserWalletTransactions__timestamp">
-        <BTooltip
-          title={
+      ) : (
+        <div className="UserWalletTransactions__avatar">
+          <Avatar username={from} size={40} />
+        </div>
+      )}
+      <div className="UserWalletTransactions__content">
+        <div className="UserWalletTransactions__content-recipient">
+          <div>
+            <FormattedMessage
+              id={isGuest ? 'author_rewards' : 'received_from'}
+              defaultMessage={isGuest ? 'Author rewards' : 'Received from {username}'}
+              values={{
+                username: (
+                  <Link to={`/@${from}`}>
+                    <span className="username">{from}</span>
+                  </Link>
+                ),
+              }}
+            />
+          </div>
+          <div className={classNames(`UserWalletTransactions__received ${isGuest ? 'guest' : ''}`)}>
+            {isGuest ? '' : '+ '}
+            {amount}
+          </div>
+        </div>
+        <span className="UserWalletTransactions__timestamp">
+          <BTooltip
+            title={
+              <span>
+                <FormattedDate value={`${timestamp}Z`} /> <FormattedTime value={`${timestamp}Z`} />
+              </span>
+            }
+          >
             <span>
-              <FormattedDate value={`${timestamp}Z`} /> <FormattedTime value={`${timestamp}Z`} />
+              <FormattedRelative value={`${timestamp}Z`} />
             </span>
-          }
-        >
-          <span>
-            <FormattedRelative value={`${timestamp}Z`} />
-          </span>
-        </BTooltip>
-      </span>
-      <span className="UserWalletTransactions__memo">{memo}</span>
+          </BTooltip>
+        </span>
+        <span className="UserWalletTransactions__memo">{memo}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ReceiveTransaction.propTypes = {
   from: PropTypes.string,
