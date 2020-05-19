94a9bcc9634b2dd4affb79d07fdf3bc1d9a1b71f
oksana <oksanana@wizardsdev.com>
2020-05-19 11:26:33 +0300

    fixed campaignRequests

--- src/waivioApi/ApiClient.js
+++ src/waivioApi/ApiClient.js
@@ -7,6 +7,7 @@ import config from './routes';
 import { getValidTokenData } from '../client/helpers/getToken';
 import { ACCOUNT_UPDATE, CUSTOM_JSON } from '../common/constants/accountHistory';
 import { message } from 'antd';
+import { getUrl } from '../client/rewards/rewardsHelper';
 
 let headers = {
   Accept: 'application/json',
@@ -571,6 +572,7 @@ export const getPropositions = ({
   area,
   coordinates,
   sort,
+  match,
 }) =>
   new Promise((resolve, reject) => {
     const reqData = {
@@ -580,6 +582,7 @@ export const getPropositions = ({
       approved,
       requiredObject,
       sort,
+      userName,
     };
 
     if (!_.isEmpty(coordinates)) {
@@ -594,7 +597,9 @@ export const getPropositions = ({
     if (!_.isEmpty(userName)) reqData.userName = userName;
     if (currentUserName) reqData.currentUserName = currentUserName;
 
-    fetch(`${config.campaignApiPrefix}${config.campaigns}`, {
+    const url = getUrl(match);
+
+    fetch(url, {
       headers,
       method: 'POST',
       body: JSON.stringify(reqData),
