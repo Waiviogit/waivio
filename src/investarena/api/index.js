27be5c83135a3235088c3f65d72c08a8b68c458c
RomanPrisyazhnuk <montereydsm@gmail.com>
2020-05-25 13:45:12 +0300

    splitcode with loadable

--- src/investarena/api/index.js
+++ src/investarena/api/index.js
@@ -1,18 +1,15 @@
 import Brokers from './Brokers';
 import Deals from './Deals';
 import Charts from './Charts';
-import Signals from './Signals';
 import Authentications from './authentications/Authentications';
 import Performers from './Performers';
 import Forecasts from './Forecasts';
 import Statistics from './Statistics';
 import ApiClient from './ApiClient';
-import QuickForecast from './QuickForecast';
 import Platform from './Platform';
 
 export default function({ apiPrefix } = {}) {
   const api = new ApiClient({ prefix: apiPrefix });
-  const apiForecast = new ApiClient({ prefix: '' });
   const apiPlatform = new ApiClient({ prefix: '' });
 
   return {
@@ -20,10 +17,8 @@ export default function({ apiPrefix } = {}) {
     brokers: new Brokers({ apiClient: api }),
     deals: new Deals({ apiClient: api }),
     charts: new Charts({ apiClient: api }),
-    signals: new Signals({ apiClient: api }),
     performers: new Performers({ apiClient: api }),
     forecasts: new Forecasts({ apiClient: api }),
-    quickForecast: new QuickForecast({ apiClient: apiForecast }),
     statistics: new Statistics({ apiClient: api }),
     platform: new Platform({ apiClient: apiPlatform }),
   };
--- src/investarena/constants/blackListQuotes.js
+++ src/investarena/constants/blackListQuotes.js
@@ -1,17 +1,4 @@
 export const blackListQuotes = [
-  'COPUSD',
-  'USDIndex',
-  'AEX25',
-  'Micex',
-  'FTSGBP',
-  'BankofChina',
-  'PetroChina',
-  'ICBankChina',
-  'PacificSec',
-  'SP5USD',
-  'E50EUR',
-  'BilbaoXETRA',
-  'CninaRail',
 ];
 
 export default blackListQuotes;
