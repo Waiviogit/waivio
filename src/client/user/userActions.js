41c1d3c2374af7da53d81038a0ff834875d559d9
oksana <oksanana@wizardsdev.com>
2020-05-29 18:53:22 +0300

    added link to primary object

--- src/client/user/userActions.js
+++ src/client/user/userActions.js
@@ -229,7 +229,6 @@ export const assignProposition = ({
     primaryObjectName,
     secondaryObjectName,
   );
-  console.log('proposition', proposition);
   const commentOp = [
     'comment',
     {
