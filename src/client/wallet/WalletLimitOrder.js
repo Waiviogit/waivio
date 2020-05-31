f154d6f1015809956f20edba8399a027197a4de7
sergei <sergeij@wizardsdev.com>
2020-05-31 19:55:41 +0300

    Added new orders

---
+++ src/client/wallet/WalletLimitOrder.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage, FormattedRelative } from 'react-intl';
+import BTooltip from '../components/BTooltip';
+import { epochToUTC } from '../helpers/formatter';
+
+const WalletLimitOrder = ({ transactionDetails, timestamp }) => (
+  <div className="UserWalletTransactions__transaction">
+    <div className="UserWalletTransactions__icon-container">
+      <i className="iconfont icon-success_fill UserWalletTransactions__icon" />
+    </div>
+    <div className="UserWalletTransactions__content">
+      <div className="UserWalletTransactions__content-recipient">
+        <div>
+          <FormattedMessage
+            id="limit_order"
+            defaultMessage="Limit order to buy {open_pays}"
+            values={{
+              open_pays: <span>{transactionDetails.open_pays}</span>,
+            }}
+          />
+        </div>
+        <div className="UserWalletTransactions__transfer">
+          {'- '}
+          {transactionDetails.current_pays}
+        </div>
+      </div>
+      <span className="UserWalletTransactions__timestamp">
+        <BTooltip
+          title={
+            <span>
+              <FormattedRelative value={epochToUTC(timestamp)} />
+            </span>
+          }
+        >
+          <span>
+            <FormattedRelative value={epochToUTC(timestamp)} />
+          </span>
+        </BTooltip>
+      </span>
+    </div>
+  </div>
+);
+
+WalletLimitOrder.propTypes = {
+  transactionDetails: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  timestamp: PropTypes.number,
+  current_pays: PropTypes.string,
+};
+
+WalletLimitOrder.defaultProps = {
+  timestamp: 0,
+  current_pays: '',
+};
+
+export default WalletLimitOrder;
--- src/client/wallet/WalletTransaction.js
+++ src/client/wallet/WalletTransaction.js
@@ -9,6 +9,8 @@ import PowerUpTransaction from './PowerUpTransaction';
 import ClaimReward from './ClaimReward';
 import './UserWalletTransactions.less';
 import WalletFillOrderTransferred from './WalletFillOrderTransferred';
+import WalletLimitOrder from './WalletLimitOrder';
+import WalletCancelOrder from './WalletCancelOrder';
 
 const getFormattedTransactionAmount = (amount, currency) => {
   if (!amount) {
@@ -89,9 +91,23 @@ const WalletTransaction = ({
           timestamp={transaction.timestamp}
         />
       );
+    case accountHistoryConstants.LIMIT_ORDER:
+      return (
+        <WalletLimitOrder
+          transactionDetails={transactionDetails}
+          timestamp={transaction.timestamp}
+        />
+      );
     case accountHistoryConstants.FILL_ORDER:
       return (
         <WalletFillOrderTransferred
+          transactionDetails={transactionDetails}
+          timestamp={transaction.timestamp}
+        />
+      );
+    case accountHistoryConstants.CANCEL_ORDER:
+      return (
+        <WalletCancelOrder
           transactionDetails={transactionDetails}
           timestamp={transaction.timestamp}
         />
