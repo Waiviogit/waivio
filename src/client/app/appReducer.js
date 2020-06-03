b88bdf7eae4631d13148ec8facde3fe2e6eb960a
sergei <sergeij@wizardsdev.com>
2020-06-03 14:43:55 +0300

    shortened method "getTranslationByKey"

--- src/client/app/appReducer.js
+++ src/client/app/appReducer.js
@@ -154,8 +154,4 @@ export const getCryptosPriceHistory = state => state.cryptosPriceHistory;
 export const getShowPostModal = state => state.showPostModal;
 export const getCurrentShownPost = state => state.currentShownPost;
 export const getIsMobile = state => state.isMobile;
-export const getTranslationByKey = (state, key, defaultMessage = '') => {
-  const foundTranslation = get(getTranslations(state), key, defaultMessage);
-  if (foundTranslation) return foundTranslation;
-  return defaultMessage;
-};
+export const getTranslationByKey = (state, key, defaultMessage = '') => get(getTranslations(state), key, defaultMessage);
