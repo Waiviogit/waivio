8422ce68054ae5c531b5d99ee2d8a63eddaa0d6a
Sergei <sergeij@wizardsdev.com>
2020-07-07 16:21:22 +0300

    Fixed WAIV-2489, WAIV-2485, WAIV-2388

--- src/client/wallet/WalletFillOrderTransferred.js
+++ src/client/wallet/WalletFillOrderTransferred.js
@@ -1,52 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, FormattedRelative } from 'react-intl';
+import { Link } from 'react-router-dom';
 import BTooltip from '../components/BTooltip';
 import Avatar from '../components/Avatar';
 import WalletFillOrderGet from './WalletFillOrderGet';
 import { epochToUTC } from '../helpers/formatter';
 
-const WalletFillOrderTransferred = ({ transactionDetails, timestamp, currentPays }) => (
-  <React.Fragment>
-    <WalletFillOrderGet transactionDetails={transactionDetails} timestamp={timestamp} />
-    <div className="UserWalletTransactions__transaction">
-      <div className="UserWalletTransactions__avatar">
-        <Avatar username={transactionDetails.account} size={40} />
-      </div>
-      <div className="UserWalletTransactions__content">
-        <div className="UserWalletTransactions__content-recipient">
-          <div>
-            <FormattedMessage
-              id="fillOrder_wallet_transferred"
-              defaultMessage="Sold {current_pays} to {exchanger}"
-              values={{
-                current_pays: <span>{currentPays}</span>,
-                exchanger: <span>{transactionDetails.exchanger}</span>,
-              }}
-            />
-          </div>
-          <div className="UserWalletTransactions__transfer">
-            {'- '}
-            {currentPays}
-          </div>
+const WalletFillOrderTransferred = ({ transactionDetails, timestamp, currentPays }) => {
+  const url = `/@${transactionDetails.exchanger}`;
+  return (
+    <React.Fragment>
+      <WalletFillOrderGet transactionDetails={transactionDetails} timestamp={timestamp} />
+      <div className="UserWalletTransactions__transaction">
+        <div className="UserWalletTransactions__avatar">
+          <Avatar username={transactionDetails.account} size={40} />
         </div>
-        <span className="UserWalletTransactions__timestamp">
-          <BTooltip
-            title={
+        <div className="UserWalletTransactions__content">
+          <div className="UserWalletTransactions__content-recipient">
+            <div>
+              <FormattedMessage
+                id="fillOrder_wallet_transferred"
+                defaultMessage="Sold {current_pays} to {exchanger}"
+                values={{
+                  current_pays: <span>{currentPays}</span>,
+                  exchanger: (
+                    <Link to={url}>
+                      <span className="username">{transactionDetails.exchanger}</span>
+                    </Link>
+                  ),
+                }}
+              />
+            </div>
+            <div className="UserWalletTransactions__transfer">
+              {'- '}
+              {currentPays}
+            </div>
+          </div>
+          <span className="UserWalletTransactions__timestamp">
+            <BTooltip
+              title={
+                <span>
+                  <FormattedRelative value={epochToUTC(timestamp)} />
+                </span>
+              }
+            >
               <span>
                 <FormattedRelative value={epochToUTC(timestamp)} />
               </span>
-            }
-          >
-            <span>
-              <FormattedRelative value={epochToUTC(timestamp)} />
-            </span>
-          </BTooltip>
-        </span>
+            </BTooltip>
+          </span>
+        </div>
       </div>
-    </div>
-  </React.Fragment>
-);
+    </React.Fragment>
+  );
+};
 
 WalletFillOrderTransferred.propTypes = {
   transactionDetails: PropTypes.arrayOf(PropTypes.shape()).isRequired,
