10c8a9a1e5582863bbfcadf737399a2fd0d4e4c8
Marie <mariiao98@wizardsdev.com>
2020-07-08 22:08:34 +0300

    fix label Re-blog

--- src/client/components/Navigation/GuestSignUpForm/GuestSignUpFormContent.js
+++ src/client/components/Navigation/GuestSignUpForm/GuestSignUpFormContent.js
@@ -6,7 +6,6 @@ import { GUEST_PREFIX } from '../../../../common/constants/waivio';
 import ImageSetter from '../../ImageSetter/ImageSetter';
 import LANGUAGES from '../../../translations/languages';
 import { getLanguageText } from '../../../translations';
-import './GuestSignUpForm.less';
 
 const GuestSignUpFormContent = ({
   getFieldDecorator,
@@ -62,13 +61,7 @@ const GuestSignUpFormContent = ({
               },
             ],
           })(
-            <Input
-              placeholder="Enter nickname"
-              addonBefore={`@${GUEST_PREFIX}`}
-              maxLength={16}
-              type="text"
-              className="input-nickname"
-            />,
+            <Input placeholder="Enter nickname" addonBefore={`@${GUEST_PREFIX}`} maxLength={16} />,
           )}
         </Form.Item>
 
