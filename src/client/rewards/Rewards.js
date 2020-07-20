6b4b3a35e0f302c64f831360c348a259df7e8c34
oksanana <oksanana@wizardsdev.com>
2020-07-20 14:25:44 +0300

    fixed loader on buttons

--- src/client/rewards/Rewards.js
+++ src/client/rewards/Rewards.js
@@ -636,6 +636,7 @@ class Rewards extends React.Component {
       activeMessagesFilters,
       sortHistory,
       sortMessages,
+      loadingAssignDiscard,
     } = this.state;
     const mapWobjects = map(wobjects, wobj => getClientWObj(wobj.required_object, usedLocale));
     const IsRequiredObjectWrap =
@@ -669,6 +670,7 @@ class Rewards extends React.Component {
       sort,
       handleSortChange: this.handleSortChange,
       loadingCampaigns,
+      loadingAssignDiscard,
       campaignsLayoutWrapLayout: this.campaignsLayoutWrapLayout,
       handleLoadMore: this.handleLoadMore,
       filterData: activePayableFilters,
