2b5b8785fe6c8f9aff3d29fd4dea92249ce4dab5
Marie <mariiao98@wizardsdev.com>
2020-08-06 15:44:46 +0300

    add translations, add night mod

--- src/client/widgets/ErrorBoundary.js
+++ src/client/widgets/ErrorBoundary.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
+import { FormattedMessage } from 'react-intl';
+// import classNames from 'classnames';
 
 export function getDisplayName(ComposedComponent) {
   if (ComposedComponent.displayName) {
@@ -37,17 +39,28 @@ const ErrorBoundary = ComposedComponent => {
             alt="errorBoundary"
             className="errorBoundary__image"
           />
-          <span className="errorBoundary__mainText">Something went wrong</span>
-          <span className="errorBoundary__secondaryText">
-            Brace yourself till we get the error fixed.
+          <span className="errorBoundary__mainText">
+            <FormattedMessage id="error_boundary_page" defaultMessage="Something went wrong" />
           </span>
           <span className="errorBoundary__secondaryText">
-            You may also refresh the page or try again later.
+            <FormattedMessage
+              id="error_boundary_page_brace"
+              defaultMessage="Brace yourself till we get the error fixed."
+            />
           </span>
           <span className="errorBoundary__secondaryText">
-            You can head back to{' '}
+            <FormattedMessage
+              id="error_boundary_page_try_reload"
+              defaultMessage="You may also refresh the page or try again later."
+            />
+          </span>
+          <span className="errorBoundary__secondaryText">
+            <FormattedMessage
+              id="error_boundary_page_go_to"
+              defaultMessage="You can head back to"
+            />
             <a href="/" className="errorBoundary__link">
-              the home page.
+              <FormattedMessage id="error_boundary_page_home" defaultMessage="the home page." />
             </a>
           </span>
         </div>
