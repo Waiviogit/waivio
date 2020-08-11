23acd2b0b4f27f4cfd8ef0fdf4208216e72a4796
Sergei <sergeij@wizardsdev.com>
2020-08-11 10:00:10 +0300

    Reopened WAIV-1414

--- src/client/components/EditorExtended/components/sides/ObjectSideButton.js
+++ src/client/components/EditorExtended/components/sides/ObjectSideButton.js
@@ -9,7 +9,7 @@ import * as apiConfig from '../../../../../waivioApi/config.json';
 
 const objectSearchInput = props => {
   const handleSelectObject = selectedObject => {
-    const objectType = selectedObject.type || selectedObject.object_type;
+    const objectType = selectedObject.default_name || selectedObject.name;
     const editorState = props.getEditorState();
     let contentState = editorState.getCurrentContent();
     const selectionState = editorState.getSelection();
