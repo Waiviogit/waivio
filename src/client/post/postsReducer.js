5a3d4d6198594aa9160cee153a39c217ac5402ff
Sergei <sergeij@wizardsdev.com>
2020-08-12 12:59:09 +0300

    Finish WAIV-2873

--- src/client/post/postsReducer.js
+++ src/client/post/postsReducer.js
@@ -263,7 +263,11 @@ export default posts;
 
 export const getPosts = state => state.list;
 export const getPostContent = (state, permlink, author) =>
-  Object.values(state.list).find(post => post.permlink === permlink && post.author === author);
+  Object.values(state.list).find(post =>
+    post.guestInfo
+      ? post.permlink === permlink && post.guestInfo.userId === author
+      : post.permlink === permlink && post.author === author,
+  );
 export const getPendingLikes = state => state.pendingLikes;
 export const getIsPostFetching = (state, author, permlink) =>
   get(state, ['postsStates', `${author}/${permlink}`, 'fetching']);
