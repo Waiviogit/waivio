a7db6ebd666782b81fcd6e262ff1d3b7ed1daf2a
oksanana <oksanana@wizardsdev.com>
2020-09-03 11:52:55 +0300

    fixed 3111

--- src/client/rewards/RewardsList/RewardsList.js
+++ src/client/rewards/RewardsList/RewardsList.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
-import { map } from 'lodash';
+import { map, isEmpty } from 'lodash';
 import { getFollowingSponsorsRewards } from '../rewardsActions';
 import Campaign from '../Campaign/Campaign';
 import { getAuthenticatedUserName } from '../../reducers';
@@ -12,27 +12,53 @@ const RewardsList = ({ intl }) => {
   const dispatch = useDispatch();
   const userName = useSelector(getAuthenticatedUserName);
   const [followingRewards, setFollowingRewards] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     dispatch(getFollowingSponsorsRewards()).then(data => {
       const { campaigns } = data.value;
       setFollowingRewards(campaigns);
+      setLoading(false);
     });
   }, []);
+  const content = useMemo(() => {
+    if (!isEmpty(followingRewards)) {
+      return (
+        <React.Fragment>
+          {map(followingRewards, reward => (
+            <Campaign
+              proposition={reward}
+              key={`${reward.required_object.author_permlink}${reward.required_object.createdAt}`}
+              filterKey={'all'}
+              userName={userName}
+            />
+          ))}
+        </React.Fragment>
+      );
+    } else if (isEmpty(followingRewards) && !loading) {
+      return (
+        <React.Fragment>
+          {intl.formatMessage({
+            id: 'no_rewards_posted_by_sponsors_you_are_following',
+            defaultMessage: 'No rewards posted by the sponsors you are following',
+          })}
+        </React.Fragment>
+      );
+    }
+    return null;
+  }, [followingRewards, loading]);
   return (
-    <div className="RewardsList">
-      {intl.formatMessage({
-        id: 'rewards_from_sponsors_you_are_following',
-        defaultMessage: 'Rewards from the sponsors you are following:',
-      })}
-
-      {map(followingRewards, reward => (
-        <Campaign
-          proposition={reward}
-          key={`${reward.required_object.author_permlink}${reward.required_object.createdAt}`}
-          userName={userName}
-        />
-      ))}
-    </div>
+    <React.Fragment>
+      <div className="RewardsList">
+        <span className="RewardsList__title">
+          {intl.formatMessage({
+            id: 'rewards_from_sponsors_you_are_following',
+            defaultMessage: 'Rewards from the sponsors you are following:',
+          })}
+        </span>
+        {content}
+      </div>
+    </React.Fragment>
   );
 };
 
