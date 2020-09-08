0c32684bebd20c1bc72775ef99f08efb86315ad3
Marie <mariiao98@wizardsdev.com>
2020-09-08 09:38:23 +0300

    fix object followers list

--- src/client/object/WobjFollowers.js
+++ src/client/object/WobjFollowers.js
@@ -3,15 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getWobjectFollowers } from '../../waivioApi/ApiClient';
 import UserDynamicList from '../user/UserDynamicList';
-import { getAuthenticatedUserName } from '../reducers';
+import { getAuthenticatedUserName, getAuthorizationUserFollowSort } from '../reducers';
 
 @connect(state => ({
   user: getAuthenticatedUserName(state),
+  sort: getAuthorizationUserFollowSort(state),
 }))
 class WobjFollowers extends React.Component {
   static propTypes = {
     match: PropTypes.shape().isRequired,
     user: PropTypes.string.isRequired,
+    sort: PropTypes.string,
+  };
+
+  static defaultProps = {
+    sort: 'recency',
   };
 
   static limit = 50;
@@ -23,6 +29,7 @@ class WobjFollowers extends React.Component {
       skip.length,
       WobjFollowers.limit,
       this.props.user,
+      this.props.sort,
     );
     return { users: response, hasMore: response.length === WobjFollowers.limit };
   };
