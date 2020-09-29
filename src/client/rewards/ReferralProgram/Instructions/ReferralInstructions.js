2cd21e6e80bb9be9296766d4d449a25f2dc26034
Sergei <sergeij@wizardsdev.com>
2020-09-29 17:13:42 +0300

    started work with second page

--- src/client/rewards/ReferralProgram/Instructions/ReferralInstructions.js
+++ src/client/rewards/ReferralProgram/Instructions/ReferralInstructions.js
@@ -1,6 +1,32 @@
-const ReferralInstructions = () => {
-  console.log('ReferralInstructions page');
+// import React from 'react';
+import PropTypes from 'prop-types';
+import { injectIntl } from 'react-intl';
+import { connect } from 'react-redux';
+import { getBlacklist } from '../../rewardsActions';
+import { getAuthenticatedUserName } from '../../../reducers';
+
+// const handleUserBlackList = () => {
+//
+// }
+
+const ReferralInstructions = props => {
+  props.getBlacklist('vallon').then(res => console.log('log: ', res));
+  console.log('authUserName: ', props.authUserName);
   return null;
 };
 
-export default ReferralInstructions;
+ReferralInstructions.propTypes = {
+  getBlacklist: PropTypes.func.isRequired,
+};
+
+ReferralInstructions.defaultProps = {};
+
+const mapStateToProps = state => ({
+  authUserName: getAuthenticatedUserName(state),
+});
+
+export default injectIntl(
+  connect(mapStateToProps, {
+    getBlacklist,
+  })(ReferralInstructions),
+);
