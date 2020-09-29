b2cdd61c88715c4964a9fa5a0d25e603bdb636a3
LucyHrinishyna <liudmylah@wizardsdev.com>
2020-09-29 15:21:35 +0300

    fix feeds routes

--- src/common/routes/routes.js
+++ src/common/routes/routes.js
@@ -332,6 +332,7 @@ const routes = {
       path: `/:sortBy(${URL.FEED.tabs})?/:category?`,
       component: Page,
       exact: true,
+      pathScope: '',
       routes: [
         {
           path: '/confirmation',
