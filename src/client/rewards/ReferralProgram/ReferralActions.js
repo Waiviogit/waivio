4a9234b918c8fb1b3bfb3417ca12ae7b1a4fdcc5
Sergei <sergeij@wizardsdev.com>
2020-10-07 10:06:22 +0300

    draft

--- src/client/rewards/ReferralProgram/ReferralActions.js
+++ src/client/rewards/ReferralProgram/ReferralActions.js
@@ -5,6 +5,16 @@ import { createAsyncActionType } from '../../helpers/stateHelpers';
 import { subscribeMethod, subscribeTypes } from '../../../common/constants/blockTypes';
 
 export const GET_USER_REFERRAL_INFO = createAsyncActionType('@referral/GET_USER_REFERRAL_INFO');
+export const GET_USER_REFERRAL_DETAILS = createAsyncActionType(
+  '@referral/GET_USER_REFERRAL_DETAILS',
+);
+export const GET_IS_USER_IN_BLACKLIST = createAsyncActionType('@referral/GET_IS_USER_IN_BLACKLIST');
+export const REFERRAL_CONFIRM_RULES = createAsyncActionType('@referral/REFERRAL_CONFIRM_RULES');
+export const REFERRAL_GET_ADDITION_FIELDS = createAsyncActionType(
+  '@referral/REFERRAL_GET_ADDITION_FIELDS',
+);
+export const REFERRAL_REJECT_RULES = createAsyncActionType('@referral/REFERRAL_REJECT_RULES');
+
 export const getUserReferralInfo = username => dispatch => {
   dispatch({
     type: GET_USER_REFERRAL_INFO.ACTION,
@@ -12,26 +22,17 @@ export const getUserReferralInfo = username => dispatch => {
   });
 };
 
-export const GET_USER_REFERRAL_DETAILS = createAsyncActionType(
-  '@referral/GET_USER_REFERRAL_DETAILS',
-);
 export const getUserReferralDetails = () => dispatch =>
   dispatch({
     type: GET_USER_REFERRAL_DETAILS.ACTION,
     payload: ApiClient.getReferralDetails(),
   });
 
-export const GET_IS_USER_IN_BLACKLIST = createAsyncActionType('@referral/GET_IS_USER_IN_BLACKLIST');
 export const getIsUserInBlackList = username => dispatch =>
   dispatch({
     type: GET_IS_USER_IN_BLACKLIST.ACTION,
     payload: ApiClient.getUserIsBlackListed(username),
   });
-
-export const REFERRAL_CONFIRM_RULES = createAsyncActionType('@referral/REFERRAL_CONFIRM_RULES');
-export const REFERRAL_GET_ADDITION_FIELDS = createAsyncActionType(
-  '@referral/REFERRAL_GET_ADDITION_FIELDS',
-);
 
 export const getChangeOnConfirmReferral = (username, isGuestName, blockNum) => (
   dispatch,
@@ -90,39 +91,59 @@ export const referralConfirmRules = (username, isGuest) => (
     });
 };
 
-export const REFERRAL_REJECT_RULES = createAsyncActionType('@referral/REFERRAL_REJECT_RULES');
+export const getChangeOnRejectReferral = (username, isGuestName, blockNum) => (
+  dispatch,
+  getState,
+  { busyAPI },
+) => {
+  busyAPI.sendAsync(subscribeMethod, [username, blockNum, subscribeTypes.posts]);
+  busyAPI.subscribe((response, mess) => {
+    if (subscribeTypes.posts === mess.type && mess.notification.blockParsed === blockNum) {
+      dispatch({
+        type: REFERRAL_GET_ADDITION_FIELDS.ACTION,
+        payload: {
+          promise: ApiClient.getUserAccount(username)
+            .then(res => {
+              dispatch({
+                type: REFERRAL_REJECT_RULES.SUCCESS,
+              });
+              return {
+                referralStatus: res.referralStatus,
+                referralList: res.referral,
+              };
+            })
+            .catch(error => error),
+        },
+      });
+    }
+  });
+};
+
 export const referralRejectRules = (username, isGuest) => (
   dispatch,
   getState,
   { steemConnectAPI },
 ) => {
-  const data = {
-    username,
-    isGuest,
-  };
   dispatch({
     type: REFERRAL_REJECT_RULES.START,
-    payload: data,
   });
   steemConnectAPI
     .referralRejectRules(username, isGuest)
-    .then(res => {
+    .then(async res => {
       if (!res.message) {
-        return dispatch({
-          type: REFERRAL_REJECT_RULES.SUCCESS,
-          payload: data,
-        });
+        const data = await res;
+        return dispatch(
+          getChangeOnRejectReferral(username, isGuest, get(data, 'result.block_num')),
+        );
       }
       return dispatch({
         type: REFERRAL_REJECT_RULES.ERROR,
-        payload: data,
       });
     })
     .catch(err => {
       message.error(err.message);
       return dispatch({
         type: REFERRAL_REJECT_RULES.ERROR,
-        payload: data,
       });
     });
 };
