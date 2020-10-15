ed27d551afe91e2764b4eea96d3422da90b7b043
Sergei <sergeij@wizardsdev.com>
2020-10-15 11:49:32 +0300

    refactor details page

--- src/client/rewards/ReferralProgram/Details/ReferralDetails.js
+++ src/client/rewards/ReferralProgram/Details/ReferralDetails.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { referralDetailContent } from '../ReferralTextHelper';
 import { getCurrentFeesValue, getCurrentOfferPercent } from '../ReferralHelper';
 import {
   getAuthenticatedUserName,
@@ -16,6 +15,7 @@ import {
   getSuspendedTimer,
 } from '../../../reducers';
 import { getUserReferralDetails } from '../ReferralActions';
+import ReferralDetailsView from './ReferralDetailsView';
 
 import './ReferralDetails.less';
 
@@ -57,57 +57,8 @@ const ReferralDetail = props => {
     additionData,
     suspendedTimer,
   };
-  const {
-    detailTitle,
-    detailDescription,
-    detailsCommissionsTitle,
-    detailsCommissionsDescription,
-    detailsCommissionsExample,
-    detailsCommissionsSponsor,
-    detailsCommissionsProcessing,
-    detailsCommissionsWaivioCampaign,
-    detailsCommissionsWaivioIndex,
-    detailsCommissionsReferrals,
-    detailsCommissionsPayments,
-    detailsReferralPeriod,
-    detailsReferralSessions,
-    detailsReferralPartners,
-  } = referralDetailContent(data);
 
-  return (
-    <React.Fragment>
-      {isAuthenticated && (
-        <div className="ReferralDetail">
-          <h2 className="ReferralDetail__title">{detailTitle}</h2>
-          <div className="ReferralDetail__description">{detailDescription}</div>
-          <div className="ReferralDetail__commissions-title">{detailsCommissionsTitle}</div>
-          <div className="ReferralDetail__commissions-description">
-            {detailsCommissionsDescription}
-          </div>
-          <div className="ReferralDetail__commissions-example">{detailsCommissionsExample}</div>
-          <div className="ReferralDetail__commissions-sponsor">{detailsCommissionsSponsor}</div>
-          <div className="ReferralDetail__commissions-processing">
-            {detailsCommissionsProcessing}
-          </div>
-          <div className="ReferralDetail__commissions-distribution">
-            <div className="commissions-distribution__waivio-campaigns">
-              {detailsCommissionsWaivioCampaign}
-            </div>
-            <div className="commissions-distribution__waivio-index">
-              {detailsCommissionsWaivioIndex}
-            </div>
-            <div className="commissions-distribution__waivio-referrals">
-              {detailsCommissionsReferrals}
-            </div>
-          </div>
-          <div className="ReferralDetail__commissions-payments">{detailsCommissionsPayments}</div>
-          <div className="ReferralDetail__referral-period">{detailsReferralPeriod}</div>
-          <div className="ReferralDetail__referral-sessions">{detailsReferralSessions}</div>
-          <div className="ReferralDetail__referral-partners">{detailsReferralPartners}</div>
-        </div>
-      )}
-    </React.Fragment>
-  );
+  return ReferralDetailsView(isAuthenticated, data);
 };
 
 ReferralDetail.propTypes = {
---
+++ src/client/rewards/ReferralProgram/Details/ReferralDetailsView.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { referralDetailContent } from '../ReferralTextHelper';
+
+const ReferralDetailsView = (isAuthenticated, data) => {
+  const {
+    detailTitle,
+    detailDescription,
+    detailsCommissionsTitle,
+    detailsCommissionsDescription,
+    detailsCommissionsExample,
+    detailsCommissionsSponsor,
+    detailsCommissionsProcessing,
+    detailsCommissionsWaivioCampaign,
+    detailsCommissionsWaivioIndex,
+    detailsCommissionsReferrals,
+    detailsCommissionsPayments,
+    detailsReferralPeriod,
+    detailsReferralSessions,
+    detailsReferralPartners,
+  } = referralDetailContent(data);
+
+  return (
+    <React.Fragment>
+      {isAuthenticated && (
+        <div className="ReferralDetail">
+          <h2 className="ReferralDetail__title">{detailTitle}</h2>
+          <div className="ReferralDetail__description">{detailDescription}</div>
+          <div className="ReferralDetail__commissions-title">{detailsCommissionsTitle}</div>
+          <div className="ReferralDetail__commissions-description">
+            {detailsCommissionsDescription}
+          </div>
+          <div className="ReferralDetail__commissions-example">{detailsCommissionsExample}</div>
+          <div className="ReferralDetail__commissions-sponsor">{detailsCommissionsSponsor}</div>
+          <div className="ReferralDetail__commissions-processing">
+            {detailsCommissionsProcessing}
+          </div>
+          <div className="ReferralDetail__commissions-distribution">
+            <div className="commissions-distribution__waivio-campaigns">
+              {detailsCommissionsWaivioCampaign}
+            </div>
+            <div className="commissions-distribution__waivio-index">
+              {detailsCommissionsWaivioIndex}
+            </div>
+            <div className="commissions-distribution__waivio-referrals">
+              {detailsCommissionsReferrals}
+            </div>
+          </div>
+          <div className="ReferralDetail__commissions-payments">{detailsCommissionsPayments}</div>
+          <div className="ReferralDetail__referral-period">{detailsReferralPeriod}</div>
+          <div className="ReferralDetail__referral-sessions">{detailsReferralSessions}</div>
+          <div className="ReferralDetail__referral-partners">{detailsReferralPartners}</div>
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default ReferralDetailsView;
