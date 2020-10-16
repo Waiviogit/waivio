6f131133b052fc8b2f809e6e290ab6590c064e39
Marie <mariiao98@wizardsdev.com>
2020-10-16 13:34:32 +0300

    fix remarks

--- src/client/object/AppendModal/AppendForm.js
+++ src/client/object/AppendModal/AppendForm.js
@@ -355,8 +355,7 @@ export default class AppendForm extends Component {
         case TYPES_OF_MENU_ITEM.PAGE:
         case TYPES_OF_MENU_ITEM.LIST: {
           const alias = getFieldValue('menuItemName');
-          const objectType =
-            this.state.selectedObject.type || this.state.selectedObject.object_type;
+          const objectType = this.state.selectedObject.object_type;
           const displayName = `${this.state.selectedObject.name} (type: ${objectType})`;
           const objectUrl = `${apiConfig.production.protocol}${apiConfig.production.host}/object/${appendValue}`;
           return `@${author} added ${currentField} (${langReadable}):\n[${displayName}](${objectUrl})${
