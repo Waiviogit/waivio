7863c7acf01f8acde85026b9185e94a8264841cd
Sergei <sergeij@wizardsdev.com>
2020-10-16 18:17:54 +0300

    fix

--- src/client/rewards/ReferralProgram/Instructions/__tests__/ReferralInstructionsView.test.js
+++ src/client/rewards/ReferralProgram/Instructions/__tests__/ReferralInstructionsView.test.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 import { shallow } from 'enzyme';
 import ReferralInstructionsView from '../../Instructions/ReferralInstructionsView';
-import { mockDataView } from '../__mock__/mockData';
+import { mockDataConfirm, mockDataIsModal, mockDataView } from '../__mock__/mockData';
 
 describe('ReferralInstructionsView', () => {
   const handleCopyTextButton = jest.fn();
@@ -23,12 +23,57 @@ describe('ReferralInstructionsView', () => {
     );
   });
 
+  afterEach(() => jest.clearAllMocks());
+
   it('should create snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should simulate chackbox button', () => {
-    wrapper.find('Checkbox').simulate('change');
+    const container = wrapper.find('Checkbox');
+    container.simulate('change');
     expect(props.handleAgreeRulesCheckbox).toHaveBeenCalled();
+  });
+
+  it('should simulate click on copy-icon', () => {
+    props = mockDataConfirm;
+    wrapper = shallow(
+      <BrowserRouter>
+        <IntlProvider locale="en">
+          {ReferralInstructionsView(props, handleCopyTextButton, widget)}
+        </IntlProvider>
+      </BrowserRouter>,
+    );
+    const container = wrapper.find('Icon');
+    container.simulate('click');
+    expect(handleCopyTextButton).toHaveBeenCalled();
+  });
+
+  it('should simulate click on okay modal button', () => {
+    props = mockDataIsModal;
+    wrapper = shallow(
+      <BrowserRouter>
+        <IntlProvider locale="en">
+          {ReferralInstructionsView(props, handleCopyTextButton, widget)}
+        </IntlProvider>
+      </BrowserRouter>,
+    );
+    const container = wrapper.find('Modal');
+    container.simulate('ok');
+    expect(props.handleOkButton).toHaveBeenCalled();
+  });
+
+  it('should simulate click on cancel modal button', () => {
+    props = mockDataIsModal;
+    wrapper = shallow(
+      <BrowserRouter>
+        <IntlProvider locale="en">
+          {ReferralInstructionsView(props, handleCopyTextButton, widget)}
+        </IntlProvider>
+      </BrowserRouter>,
+    );
+    const container = wrapper.find('Modal');
+    container.simulate('cancel');
+    expect(props.handleCancelButton).toHaveBeenCalled();
   });
 });
