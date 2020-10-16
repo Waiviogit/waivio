ca3ef21f8d6e3e4acf188edf4ab722b0d500b6ab
Sergei <sergeij@wizardsdev.com>
2020-10-16 17:27:50 +0300

    fix

--- src/client/rewards/ReferralProgram/Instructions/__tests__/ReferralInstructionsView.test.js
+++ src/client/rewards/ReferralProgram/Instructions/__tests__/ReferralInstructionsView.test.js
@@ -1,77 +1,34 @@
-// import React from 'react';
-// import {BrowserRouter} from 'react-router-dom';
-// import configureStore from 'redux-mock-store';
-// import {shallow, mount} from 'enzyme';
-// import ReferralInstructionsView from '../../Instructions/ReferralInstructionsView';
-// import {mockDataView} from '../__mock__/mockData';
-// import {Provider} from "react-redux";
-// import {IntlProvider} from "react-intl";
-//
-//
-// describe('ReferralInstructionsView', () => {
-//   const mockStore = configureStore();
-//   const handleCopyTextButton = jest.fn();
-//   const widget = `<iframe></iframe>`;
-//
-//   let props;
-//   let wrapper;
-//   let store;
-//
-//   beforeEach(() => {
-//     props = mockDataView;
-//
-//     store = mockStore();
-//     wrapper = shallow(
-//       <Provider store={store}>
-//         <IntlProvider locale="en">
-//           {ReferralInstructionsView(props, handleCopyTextButton, widget)}
-//         </IntlProvider>
-//       </Provider>
-//     )
-//   })
-//
-//   it('should create snapshot', () => {
-//     expect(wrapper).toMatchSnapshot();
-//   });
-//
-//   it("should simulate chackbox button", () => {
-//     wrapper = shallow(
-//       <BrowserRouter>
-//         <IntlProvider locale="en">
-//           {ReferralInstructionsView(props, handleCopyTextButton, widget)}
-//         </IntlProvider>
-//       </BrowserRouter>
-//     )
-//
-//     const checkbox = wrapper.find('Checkbox')
-//     console.log('checkbox: ', checkbox.debug())
-//
-//     checkbox.simulate('change');
-//     expect(props.handleAgreeRulesCheckbox).toHaveBeenCalled();
-//   });
-// })
-//
-// // describe('ReferralInstructionsView', () => {
-// //   const mockStore = configureStore();
-// //   const widget = `<iframe></iframe>`;
-// //   const handleAgreeRulesCheckbox = jest.fn();
-// //
-// //   it("create InstructionsView's page snapshot", () => {
-// //     const wrapper = mountWithIntl(
-// //       <BrowserRouter>
-// //         <Route
-// //           path="/rewards/referral-details/vallon"
-// //           component={ReferralInstructionsView(mockDataView, handleAgreeRulesCheckbox, widget)}
-// //         />
-// //       </BrowserRouter>,
-// //     );
-// //     act(() => {
-// //       wrapper.update();
-// //     });
-// //     expect(wrapper).toMatchSnapshot();
-// //   });
-// //
-// //   it("should simulate copy text button", () => {
-// //
-// //   })
-// // });
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
+import { shallow } from 'enzyme';
+import ReferralInstructionsView from '../../Instructions/ReferralInstructionsView';
+import { mockDataView } from '../__mock__/mockData';
+
+describe('ReferralInstructionsView', () => {
+  const handleCopyTextButton = jest.fn();
+  const widget = `<iframe></iframe>`;
+
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = mockDataView;
+    wrapper = shallow(
+      <BrowserRouter>
+        <IntlProvider locale="en">
+          {ReferralInstructionsView(props, handleCopyTextButton, widget)}
+        </IntlProvider>
+      </BrowserRouter>,
+    );
+  });
+
+  it('should create snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should simulate chackbox button', () => {
+    wrapper.find('Checkbox').simulate('change');
+    expect(props.handleAgreeRulesCheckbox).toHaveBeenCalled();
+  });
+});
