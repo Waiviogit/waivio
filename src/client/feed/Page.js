32742de476b6b72bb382dde2844ab32266cf9108
Marie <mariiao98@wizardsdev.com>
2020-12-10 11:54:24 +0200

    fix

--- src/client/feed/Page.js
+++ src/client/feed/Page.js
@@ -56,7 +56,7 @@ class Page extends React.Component {
   render() {
     const { authenticated, history, wobject, match } = this.props;
     const isPageMode = true;
-    const sortBy = match.params.sortBy || 'trending';
+    const sortBy = authenticated ? match.params.sortBy : match.params.sortBy || 'trending';
 
     return (
       <div>
