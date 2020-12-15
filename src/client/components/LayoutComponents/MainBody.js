d97c666dc03f3af2c802e6671a2afcfd250f07cf
LucyHrinishyna <liudmylah@wizardsdev.com>
2020-12-15 16:09:18 +0200

    add current page in mainbody

--- src/client/components/LayoutComponents/MainBody.js
+++ src/client/components/LayoutComponents/MainBody.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Page from '../../feed/Page';
 import WebsiteBody from '../../websites/WebsiteLayoutComponents/Body/WebsiteBody';
+import { getMainPage } from '../../reducers';
 
 const MainBody = ({ currPage, route }) => {
   switch (currPage) {
@@ -19,4 +21,6 @@ MainBody.propTypes = {
   route: PropTypes.shape({}).isRequired,
 };
 
-export default MainBody;
+export default connect(state => ({
+  currPage: getMainPage(state),
+}))(MainBody);
