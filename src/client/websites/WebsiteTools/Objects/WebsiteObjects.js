3b9fee92e8168ba668c101d1269c685512320600
MrVallon <vallonkings@gmail.com>
2020-12-18 14:41:23 +0200

    main layout

--- src/client/websites/WebsiteTools/Objects/WebsiteObjects.js
+++ src/client/websites/WebsiteTools/Objects/WebsiteObjects.js
@@ -41,13 +41,19 @@ const WebsiteObjects = ({ intl }) => {
   return (
     <div className="WebsiteObjects">
       <h1 className="WebsiteObjects__heading">
-        <FormattedMessage id="website_objects_heading" defaultMessage="Select objects" />
+        {intl.formatMessage({
+          id: 'website_objects_heading',
+          defaultMessage: 'Select objects',
+        })}
       </h1>
-      {intl.formatMessage({
-        id: 'website_objects_description',
-        defaultMessage:
-          'All objects (restaurants, dishes, drinks) located in the areas specified on the map will appear on the website. If you want to have more control over the list of objects, you can use the Authorities to do so.',
-      })}
+      <p className="WebsiteObjects__explanations">
+        {intl.formatMessage({
+          id: 'website_objects_description',
+          defaultMessage:
+            'All objects (restaurants, dishes, drinks) located in the areas specified on the map will appear on the website. If you want to have more control over the list of objects, you can use the Authorities to do so.',
+        })}
+      </p>
+
       <div className="MapWrap">
         <div className="MapWrap__new-aria-btn">
           <Icon type="plus-circle" theme="filled" />
@@ -63,6 +69,16 @@ const WebsiteObjects = ({ intl }) => {
           // }}
         />
       </div>
+      <p className="WebsiteObjects__rules-selections">
+        <FormattedMessage
+          id="website_object_rules_of_selections"
+          defaultMessage="Press {plusSymbol} to add a new area. Click & drag corners to resize. Click {cancelSymbol} to remove the area. Zoom in for controls."
+          values={{
+            plusSymbol: <span style={{ color: '#f87007' }}>+</span>,
+            cancelSymbol: <span style={{ color: '#f87007' }}>X</span>,
+          }}
+        />
+      </p>
     </div>
   );
 };
