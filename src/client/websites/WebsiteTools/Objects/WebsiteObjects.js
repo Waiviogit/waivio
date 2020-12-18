7053437428c736675968d6245125c680fc5d79ed
MrVallon <vallonkings@gmail.com>
2020-12-18 16:57:28 +0200

    Finished with buttons logic of map

--- src/client/websites/WebsiteTools/Objects/WebsiteObjects.js
+++ src/client/websites/WebsiteTools/Objects/WebsiteObjects.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'antd';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { FormattedMessage, injectIntl } from 'react-intl';
+import { isEmpty } from 'lodash';
 import Map from 'pigeon-maps';
 import mapProvider from '../../../helpers/mapProvider';
+import { getUserLocation } from '../../../reducers';
+import { getCoordinates } from '../../../user/userActions';
 import './WebsiteObjects.less';
 
-const WebsiteObjects = ({ intl }) => {
+const WebsiteObjects = props => {
+  const [currentCenter, setCurrentCenter] = useState([]);
+  const [currentZoom, setCurrentZoom] = useState(11);
+
+  useEffect(() => {
+    if (isEmpty(props.userLocation)) {
+      props.getCoordinates();
+    }
+  });
+
+  const startOwnLocation = [+props.userLocation.lat, +props.userLocation.lon];
+  const setMapCenter = () => (isEmpty(currentCenter) ? startOwnLocation : currentCenter);
+
+  const setPosition = () => setCurrentCenter(startOwnLocation);
+  const incrementZoom = () => setCurrentZoom(currentZoom + 1);
+  const decrementZoom = () => setCurrentZoom(currentZoom - 1);
+
   const zoomButtonsLayout = () => (
     <div className="WebsiteObjectsControl">
       <div className="WebsiteObjectsControl__gps">
-        <div
-          role="presentation"
-          className="WebsiteObjectsControl__locateGPS"
-          // onClick={setPosition}
-        >
+        <div role="presentation" className="WebsiteObjectsControl__locateGPS" onClick={setPosition}>
           <img src="/images/icons/aim.png" alt="aim" className="MapOS__locateGPS-button" />
         </div>
       </div>
@@ -23,14 +39,14 @@ const WebsiteObjects = ({ intl }) => {
         <div
           role="presentation"
           className="WebsiteObjectsControl__zoom__button"
-          // onClick={incrementZoom}
+          onClick={incrementZoom}
         >
           +
         </div>
         <div
           role="presentation"
           className="WebsiteObjectsControl__zoom__button"
-          // onClick={decrementZoom}
+          onClick={decrementZoom}
         >
           -
         </div>
@@ -41,13 +57,13 @@ const WebsiteObjects = ({ intl }) => {
   return (
     <div className="WebsiteObjects">
       <h1 className="WebsiteObjects__heading">
-        {intl.formatMessage({
+        {props.intl.formatMessage({
           id: 'website_objects_heading',
           defaultMessage: 'Select objects',
         })}
       </h1>
       <p className="WebsiteObjects__explanations">
-        {intl.formatMessage({
+        {props.intl.formatMessage({
           id: 'website_objects_description',
           defaultMessage:
             'All objects (restaurants, dishes, drinks) located in the areas specified on the map will appear on the website. If you want to have more control over the list of objects, you can use the Authorities to do so.',
@@ -59,16 +75,20 @@ const WebsiteObjects = ({ intl }) => {
           <Icon type="plus-circle" theme="filled" />
         </div>
         {zoomButtonsLayout()}
-        <Map
-          center={[48.539885811540834, 35.338863789062486]}
-          zoom={8}
-          height={400}
-          provider={mapProvider}
-          // onBoundsChanged={state => {
-          //   onBoundsChanged(state);
-          // }}
-        />
+        {!isEmpty(props.userLocation) && (
+          <Map
+            center={setMapCenter()}
+            zoom={currentZoom}
+            height={400}
+            provider={mapProvider}
+            onBoundsChanged={({ center, zoom }) => {
+              setCurrentCenter(center);
+              setCurrentZoom(zoom);
+            }}
+          />
+        )}
       </div>
+
       <p className="WebsiteObjects__rules-selections">
         <FormattedMessage
           id="website_object_rules_of_selections"
@@ -87,6 +107,24 @@ WebsiteObjects.propTypes = {
   intl: PropTypes.shape({
     formatMessage: PropTypes.func,
   }).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      site: PropTypes.string,
+    }),
+    path: PropTypes.string.isRequired,
+  }).isRequired,
+  userLocation: PropTypes.shape({
+    lat: PropTypes.string,
+    lon: PropTypes.string,
+  }).isRequired,
+  getCoordinates: PropTypes.func.isRequired,
 };
 
-export default withRouter(injectIntl(WebsiteObjects));
+export default connect(
+  state => ({
+    userLocation: getUserLocation(state),
+  }),
+  {
+    getCoordinates,
+  },
+)(withRouter(injectIntl(WebsiteObjects)));
