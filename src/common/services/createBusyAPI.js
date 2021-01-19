eb41a5c42496c2398f82318238b44b0b4b36c144
LucyHrinishyna <liudmylah@wizardsdev.com>
2021-01-19 13:33:24 +0200

    rewrite websocet conection

--- src/common/services/createBusyAPI.js
+++ src/common/services/createBusyAPI.js
@@ -3,19 +3,23 @@ import apiConfig from '../../waivioApi/routes';
 function createBusyAPI() {
   const socket = new WebSocket(`wss://${apiConfig[process.env.NODE_ENV].host}/notifications-api`);
 
-  socket.sendAsync = (message, params) =>
+  socket.sendAsync = (message, params) => {
+    if (socket.readyState === socket.CLOSED) {
+      const client = createBusyAPI();
+      client.sendAsync(message, params);
+    }
+
     socket.send(
       JSON.stringify({
         method: message,
         params,
       }),
     );
+  };
 
   socket.subscribe = callback => {
     socket.addEventListener('message', e => callback(null, JSON.parse(e.data)));
   };
-
-  if (socket.readyState === socket.CLOSED) createBusyAPI();
 
   return socket;
 }
