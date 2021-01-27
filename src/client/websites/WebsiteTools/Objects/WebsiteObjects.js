28d72141b1bd7717f4d205533d854c396e46bbea
MrVallon <vallonkings@gmail.com>
2021-01-27 19:12:21 +0200

    changes in websites object

--- src/client/websites/WebsiteTools/Objects/WebsiteObjects.js
+++ src/client/websites/WebsiteTools/Objects/WebsiteObjects.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Icon, Modal } from 'antd';
+import { Icon } from 'antd';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { FormattedMessage, injectIntl } from 'react-intl';
+import uuidv4 from 'uuid/v4';
 import { isEmpty, isEqual } from 'lodash';
 import Map from 'pigeon-maps';
 import Overlay from 'pigeon-overlay';
@@ -15,25 +16,55 @@ import { getCurrStyleAfterZoom } from '../../helper';
 import './WebsiteObjects.less';
 
 const WebsiteObjects = props => {
-  const [showMap, setShowMap] = useState(false);
-  const [modalMapData, setModalMapData] = useState([]);
+  const [mapData, setMapData] = useState([]);
   const [currStyle, setCurrStyle] = useState({ width: 300, height: 250 });
-  const [area, setArea] = useState({ center: [], zoom: 11, bounds: [] });
+  const [area, setArea] = useState({
+    center: [],
+    zoom: 11,
+    bounds: { topPoint: [], bottomPoint: [] },
+  });
 
   useEffect(() => {
     if (isEmpty(props.userLocation)) {
       props.getCoordinates();
     }
-    if (!isEqual(props.usersSelectedAreas, modalMapData)) {
-      setModalMapData(props.usersSelectedAreas);
+    if (!isEqual(props.usersSelectedAreas, mapData)) {
+      setMapData(props.usersSelectedAreas);
     }
   }, [props.userLocation, props.usersSelectedAreas]);
+
+  const [currAreaData, setCurrAreaData] = useState([]);
 
   useEffect(() => {
     props
       .getWebsiteObjectsCoordinates(props.match.params.site)
       .then(res => {
-        setModalMapData(res.value);
+        const { value } = res;
+        setMapData(value);
+        console.log('value: ', value);
+        const arr = [];
+
+        // eslint-disable-next-line array-callback-return
+        value.map(currValue => {
+          const topPoint = currValue.topPoint;
+          const buttonPoint = currValue.bottomPoint;
+          const center = currValue.center;
+
+          const data = {
+            topLeftPoint: [buttonPoint[1], buttonPoint[0]],
+            topRightPoint: [topPoint[1], topPoint[0]],
+            buttonLeftPoint: [topPoint[1], buttonPoint[0]],
+            buttonRightPoint: [buttonPoint[1], topPoint[0]],
+            topMiddlePoint: [topPoint[1], (buttonPoint[0] + topPoint[0]) / 2],
+            buttonMiddlePoint: [buttonPoint[1], (buttonPoint[0] + topPoint[0]) / 2],
+            rightMiddlePoint: [(buttonPoint[1] + topPoint[1]) / 2, topPoint[0]],
+            leftMiddlePoint: [(buttonPoint[1] + topPoint[1]) / 2, buttonPoint[0]],
+            id: uuidv4(),
+            removeAreaID: center[0],
+          };
+          arr.push(data);
+        });
+        setCurrAreaData(arr);
       })
       .catch(err => console.error('Error: ', err));
   }, []);
@@ -42,44 +73,15 @@ const WebsiteObjects = props => {
     getCurrStyleAfterZoom(area.zoom, setCurrStyle, currStyle);
   }, [area.zoom]);
 
-  const getLongitudeToTile = (lon, zoom) => ((lon + 180) / 360) * zoom ** 2;
-  const getLatitudeToTile = (lat, zoom) =>
-    ((1 -
-      Math.log(Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180)) / Math.PI) /
-      2) *
-    2 ** zoom;
-
-  const latLngToPixel = (latLng, center, zoom) => {
-    const tileCenterX = getLongitudeToTile(center[1], zoom);
-
-    const tileX = getLongitudeToTile(latLng[1], zoom);
-
-    return (tileX - tileCenterX) * 256.0 + 600 / 2;
-  };
-
-  const lonLngToPixel = (lonLng, center, zoom) => {
-    const tileCenterY = getLatitudeToTile(center[0], zoom);
-
-    const tileY = getLatitudeToTile(lonLng[0], zoom);
-
-    return (tileY - tileCenterY) * 256.0 + 400 / 2;
-  };
-  console.log('area: ', area);
   const startOwnLocation = [+props.userLocation.lat, +props.userLocation.lon];
-  // const setMapCenter = () => (isEmpty(currentCenter) ? startOwnLocation : currentCenter);
+
   const setMapCenter = () => (isEmpty(area.center) ? startOwnLocation : area.center);
-
-  // const setPosition = () => setCurrentCenter(startOwnLocation);
   const setPosition = () => setArea({ ...area, center: startOwnLocation });
-  // const incrementZoom = () => setCurrentZoom(Math.round(currentZoom) + 1);
-  // const decrementZoom = () => setCurrentZoom(Math.round(currentZoom) - 1);
-
   const incrementZoom = () => setArea({ ...area, zoom: area.zoom + 1 });
   const decrementZoom = () => setArea({ ...area, zoom: area.zoom - 1 });
 
-  const currClassName = showMap ? 'WebsiteObjectsControl modal-view' : 'WebsiteObjectsControl';
   const zoomButtonsLayout = () => (
-    <div className={currClassName}>
+    <div className="WebsiteObjectsControl">
       <div className="WebsiteObjectsControl__gps">
         <div role="presentation" className="WebsiteObjectsControl__locateGPS" onClick={setPosition}>
           <img src="/images/icons/aim.png" alt="aim" className="MapOS__locateGPS-button" />
@@ -114,62 +116,24 @@ const WebsiteObjects = props => {
   };
 
   const removeArea = id => {
-    const filteredAreas = modalMapData.filter(currArea => !isEqual(+id, currArea.center[0]));
-    setModalMapData(filteredAreas);
+    const filteredAreas = mapData.filter(currArea => !isEqual(+id, currArea.center[0]));
+    setMapData(filteredAreas);
     saveCurrentAreas(filteredAreas);
   };
 
-  const handleModalOkButton = currData => {
-    const width = latLngToPixel(
-      [+currData.topPoint[1], +currData.topPoint[0]],
-      area.center,
-      area.zoom,
-    );
-    const height = lonLngToPixel(
-      [+currData.bottomPoint[1], +currData.bottomPoint[0]],
-      area.center,
-      area.zoom,
-    );
-    setCurrStyle({ width, height });
+  const addArea = () => {
     const data = [
-      ...modalMapData,
-      Object.assign(currData, { width: `${width}px`, height: `${height}px` }),
+      ...mapData,
+      {
+        topPoint: area.bounds.topPoint,
+        bottomPoint: area.bounds.bottomPoint,
+        center: area.center,
+        zoom: area.zoom,
+        width: 'empty',
+        height: 'empty',
+      },
     ];
-    setShowMap(!showMap);
     saveCurrentAreas(data);
-  };
-
-  const modalMap = () => {
-    let currData = {};
-    return (
-      <Modal
-        title={props.intl.formatMessage({
-          id: 'website_objects_area',
-          defaultMessage: 'Select area',
-        })}
-        closable
-        onCancel={() => setShowMap(!showMap)}
-        onOk={() => handleModalOkButton(currData)}
-        visible={showMap}
-      >
-        {!isEmpty(props.userLocation) && (
-          <Map
-            center={setMapCenter()}
-            zoom={area.zoom}
-            height={400}
-            provider={mapProvider}
-            onBoundsChanged={data => {
-              currData = {
-                topPoint: [data.bounds.ne[1], data.bounds.ne[0]],
-                bottomPoint: [data.bounds.sw[1], data.bounds.sw[0]],
-                center: data.center,
-                zoom: data.zoom,
-              };
-            }}
-          />
-        )}
-      </Modal>
-    );
   };
 
   return (
@@ -188,14 +152,10 @@ const WebsiteObjects = props => {
         })}
       </p>
       <div className="MapWrap">
-        <div
-          role="presentation"
-          className="MapWrap__new-aria-btn"
-          onClick={() => setShowMap(!showMap)}
-        >
+        <div role="presentation" className="MapWrap__new-aria-btn" onClick={addArea}>
           <Icon type="plus-circle" theme="filled" />
         </div>
-        {!showMap && zoomButtonsLayout()}
+        {zoomButtonsLayout()}
         {!isEmpty(props.userLocation) && (
           <Map
             center={setMapCenter()}
@@ -203,37 +163,51 @@ const WebsiteObjects = props => {
             height={400}
             provider={mapProvider}
             onBoundsChanged={({ center, zoom, bounds }) => {
-              // setCurrentCenter(center);
-              setArea({ center, zoom, bounds });
-              // setCurrentZoom(Math.round(zoom));
+              setArea({
+                center,
+                zoom,
+                bounds: {
+                  topPoint: [bounds.ne[1], bounds.ne[0]],
+                  bottomPoint: [bounds.sw[1], bounds.sw[0]],
+                },
+              });
             }}
           >
-            {!isEmpty(modalMapData) &&
-              modalMapData.map(currArea => {
-                const id = currArea.center[0];
-                return (
-                  <Overlay key={id} anchor={currArea.center}>
-                    <div
-                      className="MapWrap__rect"
-                      style={{ ...currStyle, width: currArea.width, height: currArea.height }}
-                    >
-                      <span
-                        id={id}
-                        role="presentation"
-                        className="MapWrap__cancel"
-                        onClick={event => removeArea(event.target.id)}
-                      >
-                        &#9746;
-                      </span>
-                    </div>
-                    <div className="MapWrap__dots" />
-                  </Overlay>
-                );
-              })}
+            {!isEmpty(currAreaData) &&
+              currAreaData.map(data =>
+                Object.values(data).map((item, index) => {
+                  console.log('index: ', index);
+                  if (index === 1) {
+                    return (
+                      typeof item === 'object' && (
+                        <Overlay key={uuidv4()} anchor={item}>
+                          <span
+                            id={data.removeAreaID}
+                            role="presentation"
+                            className="MapWrap__cancel"
+                            onClick={event => removeArea(event.target.id)}
+                          >
+                            &#9746;
+                          </span>
+                        </Overlay>
+                      )
+                    );
+                  }
+                  return (
+                    typeof item === 'object' && (
+                      <Overlay key={uuidv4()} anchor={item}>
+                        <div
+                          className="MapWrap__area"
+                          style={{ width: '10px', height: '10px', background: 'red' }}
+                        />
+                      </Overlay>
+                    )
+                  );
+                }),
+              )}
           </Map>
         )}
       </div>
-      {showMap && modalMap()}
       <p className="WebsiteObjects__rules-selections">
         <FormattedMessage
           id="website_object_rules_of_selections"
