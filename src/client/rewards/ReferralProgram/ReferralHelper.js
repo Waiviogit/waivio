6bc348e1a188065c357bb88711c1de151f5cd56a
Andrey <andreybe@wizardsdev.com>
2021-05-19 14:51:04 +0300

    Fix instructions on widget

--- src/client/rewards/ReferralProgram/ReferralHelper.js
+++ src/client/rewards/ReferralProgram/ReferralHelper.js
@@ -57,7 +57,7 @@ export const getBeneficiaresInfo = sponsor =>
     : null;
 
 export const widget = username =>
-  `<iframe src="https://waivio.com/rewards/all/?display=widget&userName=someUserName&ref=${username}" height="400" width="350" style="border: none;">Can't load Rewards widget.</iframe>`;
+  `<iframe src="https://www.waivio.com/rewards/all/?display=widget!ref=${username}" height="400" width="350" style="border: none;">Can't load Rewards widget.</iframe>`;
 
 export const getCopyTextButtonResult = (setIsCopyButton, username) => {
   const reservoir = document.createElement('textarea');
