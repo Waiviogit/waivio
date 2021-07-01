782c6ddd8cdf5005b06236ce936bf87ffb4785cd
Andrey <andreybe@wizardsdev.com>
2021-07-01 18:04:17 +0300

    try to fix error

--- src/client/helpers/voteHelpers.js
+++ src/client/helpers/voteHelpers.js
@@ -1,22 +1,26 @@
-import { includes } from 'lodash';
+import { includes, filter } from 'lodash';
 
-export const getUpvotes = activeVotes => activeVotes.filter(vote => vote.percent > 0);
+export const getUpvotes = activeVotes => filter(activeVotes, vote => vote.percent > 0) || [];
 
 export const getAppendUpvotes = (activeVotes = []) =>
-  activeVotes.filter(
+  filter(
+    activeVotes,
     vote =>
       (vote.percent > 100 && vote.percent % 10 === 0) || (vote.percent > 0 && vote.percent <= 100),
-  );
+  ) || [];
 
-export const getDownvotes = activeVotes => activeVotes.filter(vote => vote.percent < 0);
+export const getDownvotes = activeVotes => filter(activeVotes, vote => vote.percent < 0) || [];
 
 export const getAppendDownvotes = (activeVotes = []) =>
-  activeVotes.filter(vote => (vote.percent > 100 && vote.percent % 10 !== 0) || vote.percent < 0);
+  filter(
+    activeVotes,
+    vote => (vote.percent > 100 && vote.percent % 10 !== 0) || vote.percent < 0,
+  ) || [];
 
 export const getFollowingUpvotes = (activeVotes, following) =>
-  getUpvotes(activeVotes).filter(vote => includes(following, vote.voter));
+  filter(getUpvotes(activeVotes), vote => includes(following, vote.voter)) || [];
 
 export const getFollowingDownvotes = (activeVotes, following) =>
-  getDownvotes(activeVotes).filter(vote => includes(following, vote.voter));
+  filter(getDownvotes(activeVotes), vote => includes(following, vote.voter)) || [];
 
 export const sortVotes = (votes, sortBy) => votes.sort((a, b) => a[sortBy] - b[sortBy]);
