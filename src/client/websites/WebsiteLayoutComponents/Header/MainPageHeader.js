e588ca60af17c83adc4082837d1bf38fc7a62546
LucyHrinishyna <liudmylah@wizardsdev.com>
2021-07-22 13:53:14 +0300

    hide sing in after auth

--- src/client/websites/WebsiteLayoutComponents/Header/MainPageHeader.js
+++ src/client/websites/WebsiteLayoutComponents/Header/MainPageHeader.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ModalSignIn from '../../../components/Navigation/ModlaSignIn/ModalSignIn';
+import { useSelector } from 'react-redux';
 
-const MainPageHeader = () => (
-  <div className="MainPageHeader">
-    <div className="MainPageHeader__logo">
-      <img src={'/images/dining.gifts.png'} className="MainPageHeader__logoImg" alt="logo" />
-      <b>Dining.Gifts</b> <span>Eat out, earn crypto.</span>
+import ModalSignIn from '../../../components/Navigation/ModlaSignIn/ModalSignIn';
+import { getAuthenticatedUserName } from '../../../../store/authStore/authSelectors';
+import Avatar from '../../../components/Avatar';
+
+const MainPageHeader = () => {
+  const username = useSelector(getAuthenticatedUserName);
+
+  return (
+    <div className="MainPageHeader">
+      <div className="MainPageHeader__logo">
+        <img src={'/images/dining.gifts.png'} className="MainPageHeader__logoImg" alt="logo" />
+        <b>Dining.Gifts</b> <span>Eat out, earn crypto.</span>
+      </div>
+      <div className="MainPageHeader__listLink">
+        <Link to="/object/mds-dining-gifts/newsFilter/dininggifts-dw09owbl6bh">Rewiews</Link>
+        <Link to="/map">map</Link>
+        <Link to="/object/mds-dining-gifts/page#voy-business-3-0">partners</Link>
+        {username ? (
+          <Link className="Topnav__user" to={`/@${username}`}>
+            <Avatar username={username} size={36} />
+          </Link>
+        ) : (
+          <ModalSignIn buttonClassName="MainPageHeader__signIn" isButton />
+        )}
+      </div>
     </div>
-    <div className="MainPageHeader__listLink">
-      <Link to="/object/mds-dining-gifts/newsFilter/dininggifts-dw09owbl6bh">Rewiews</Link>
-      <Link to="/map">map</Link>
-      <Link to="/object/mds-dining-gifts/page#voy-business-3-0">partners</Link>
-      <ModalSignIn buttonClassName="MainPageHeader__signIn" isButton />
-    </div>
-  </div>
-);
+  );
+};
 
 export default MainPageHeader;
