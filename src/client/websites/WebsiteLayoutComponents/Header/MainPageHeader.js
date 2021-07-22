ee9b9fe7ba33cb41c4a5cdec9824e9f47f918a52
LucyHrinishyna <liudmylah@wizardsdev.com>
2021-07-22 17:01:11 +0300

    fix modal styles

--- src/client/websites/WebsiteLayoutComponents/Header/MainPageHeader.js
+++ src/client/websites/WebsiteLayoutComponents/Header/MainPageHeader.js
@@ -16,7 +16,7 @@ const MainPageHeader = () => {
         <b>Dining.Gifts</b> <span>Eat out, earn crypto.</span>
       </div>
       <div className="MainPageHeader__listLink">
-        <Link to="/object/mds-dining-gifts/newsFilter/dininggifts-dw09owbl6bh">Rewiews</Link>
+        <Link to="/object/mds-dining-gifts/newsFilter/dininggifts-dw09owbl6bh">Reviews</Link>
         <Link to="/map">map</Link>
         <Link to="/object/mds-dining-gifts/page#voy-business-3-0">partners</Link>
         {username ? (
