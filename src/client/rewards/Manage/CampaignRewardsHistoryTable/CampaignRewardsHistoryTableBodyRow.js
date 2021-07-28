c24ec3a95509efc6f93dbdb8b23f42832c37d775
LucyHrinishyna <liudmylah@wizardsdev.com>
2021-07-28 10:21:37 +0300

    add current currency in manage react, fix infinity hive sum

--- src/client/rewards/Manage/CampaignRewardsHistoryTable/CampaignRewardsHistoryTableBodyRow.js
+++ src/client/rewards/Manage/CampaignRewardsHistoryTable/CampaignRewardsHistoryTableBodyRow.js
@@ -4,29 +4,32 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { isEmpty } from 'lodash';
 
-const CampaignRewardsHistoryTableBodyRow = ({ currentItem }) => (
-  <React.Fragment>
-    <tr>
-      <td>{moment(currentItem.createdAt).format('DD-MMM-YYYY')}</td>
-      <td>{moment(currentItem.expired_at).format('DD-MMM-YYYY')}</td>
-      <td>
-        {/* eslint-disable-next-line no-underscore-dangle */}
-        <Link to={`/rewards/details/${currentItem._id}`} title="View">
-          {currentItem.name}
-        </Link>
-      </td>
-      <td>{currentItem.status}</td>
-      <td className="Campaign-rewards-history hide-element">{currentItem.type}</td>
-      <td className="Campaign-rewards-history hide-element">{currentItem.reward.toFixed(2)}</td>
-      <td className="Campaign-rewards-history hide-element">USD</td>
-      <td className="Campaign-rewards-history hide-element">{currentItem.completedTotal}</td>
-      <td className="Campaign-rewards-history hide-element">{currentItem.payed || 0}</td>
-    </tr>
-  </React.Fragment>
+const CampaignRewardsHistoryTableBodyRow = ({ currentItem, currencyInfo }) => (
+  <tr>
+    <td>{moment(currentItem.createdAt).format('DD-MMM-YYYY')}</td>
+    <td>{moment(currentItem.expired_at).format('DD-MMM-YYYY')}</td>
+    <td>
+      <Link to={`/rewards/details/${currentItem._id}`} title="View">
+        {currentItem.name}
+      </Link>
+    </td>
+    <td>{currentItem.status}</td>
+    <td className="Campaign-rewards-history hide-element">{currentItem.type}</td>
+    <td className="Campaign-rewards-history hide-element">
+      {(currentItem.reward * currencyInfo.rate).toFixed(2)}
+    </td>
+    <td className="Campaign-rewards-history hide-element">{currencyInfo.type}</td>
+    <td className="Campaign-rewards-history hide-element">{currentItem.completedTotal}</td>
+    <td className="Campaign-rewards-history hide-element">{currentItem.payed || 0}</td>
+  </tr>
 );
 
 CampaignRewardsHistoryTableBodyRow.propTypes = {
   currentItem: PropTypes.shape().isRequired,
+  currencyInfo: PropTypes.shape({
+    type: PropTypes.string,
+    rate: PropTypes.number,
+  }).isRequired,
 };
 
 export default CampaignRewardsHistoryTableBodyRow;
