23ed09ebfd51a1ed3df6a257e77c0bc7505c1cba
LucyHrinishyna <liudmylah@wizardsdev.com>
2021-07-30 11:14:37 +0300

    fix infunity

--- src/common/routes/switchRoutes.js
+++ src/common/routes/switchRoutes.js
@@ -2,7 +2,7 @@ import websitesRoutes from './configs/websitesRoutes';
 import routes from './configs/routes';
 
 const switchRoutes = host => {
-  if (host.includes('host')) return websitesRoutes(host);
+  if (host.includes('dining')) return websitesRoutes(host);
 
   return routes;
 };
