38f230fa1cb04ae31e8ebad6d0dc296e91b3bae6
LucyHrinishyna <liudmylah@wizardsdev.com>
2021-11-16 16:20:40 +0200

    add wallet type

--- src/client/wallet/Wallets.js
+++ src/client/wallet/Wallets.js
@@ -9,13 +9,22 @@ import WAIVwallet from './WAIVwallet/WAIVwallet';
 import { getTokenRates, setWalletType } from '../../store/walletStore/walletActions';
 
 const Wallets = props => {
+  const query = new URLSearchParams(props.location.search);
+  const walletsType = query.get('type');
+
   useEffect(() => {
+    props.setWalletType(walletsType);
     props.getTokenRates('WAIV');
   }, []);
 
+  const handleOnChange = key => {
+    props.setWalletType(key);
+    props.history.push(`?type=${key}`);
+  };
+
   return (
     <React.Fragment>
-      <Tabs defaultActiveKey="WAIV" onChange={key => props.setWalletType(key)}>
+      <Tabs defaultActiveKey={walletsType} onChange={handleOnChange}>
         <Tabs.TabPane tab="WAIV wallet" key="WAIV">
           <WAIVwallet />
         </Tabs.TabPane>
@@ -31,7 +40,12 @@ const Wallets = props => {
 Wallets.propTypes = {
   getTokenRates: PropTypes.func.isRequired,
   setWalletType: PropTypes.func.isRequired,
-  history: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string,
+  }).isRequired,
 };
 
 export default connect(null, { getTokenRates, setWalletType })(Wallets);
