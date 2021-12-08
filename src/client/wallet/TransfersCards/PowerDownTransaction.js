bde81138a1002ce37ca3e0c23971b38c6b6dbd9a
otoradze <oto.toradze@gmail.com>
2021-12-08 16:46:05 +0400

    update

--- src/client/wallet/TransfersCards/PowerDownTransaction.js
+++ src/client/wallet/TransfersCards/PowerDownTransaction.js
@@ -13,14 +13,13 @@ const PowerDownTransaction = ({ timestamp, amount, description }) => {
       <div className="UserWalletTransactions__content">
         <div className="UserWalletTransactions__content-recipient">
           {description}
-          <span className={UserWalletTransactions__marginLeft}>{formatNumber(amount)}</span>
+          <span className="UserWalletTransactions__marginLeft">{formatNumber(amount)}</span>
         </div>
         <CardsTimeStamp timestamp={timestamp} />
       </div>
     </div>
   );
 };
-
 
 PowerDownTransaction.propTypes = {
   timestamp: PropTypes.number.isRequired,
