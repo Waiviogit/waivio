e77107709582c07b9608693831e7ecff286b2148
lucykolosova <avavom61>
2022-01-26 19:07:32 +0200

    fix swap tokens request

--- src/client/wallet/SwapTokens/SwapTokens.js
+++ src/client/wallet/SwapTokens/SwapTokens.js
@@ -30,13 +30,22 @@ import TokensSelect from './components/TokensSelect';
 import { getImpact } from '../../helpers/swapWalletHelpers';
 
 import './SwapTokens.less';
+import { getFeeInfo } from '../../../waivioApi/ApiClient';
 
 const SwapTokens = props => {
   const [impact, setImpact] = useState(0);
   const [fromAmount, setFromAmount] = useState(0);
   const [toAmount, setToAmount] = useState(0);
+  const [param, setParams] = useState(0);
+
+  const setFeeInfo = async () => {
+    const data = await getFeeInfo();
+
+    setParams(data);
+  };
 
   useLayoutEffect(() => {
+    setFeeInfo();
     props.getSwapList();
 
     return () => props.resetModalData();
@@ -58,6 +67,7 @@ const SwapTokens = props => {
       pool,
       slippage: 0,
       from: isFrom,
+      params: param,
     });
   };
 
