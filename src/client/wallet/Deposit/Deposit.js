d98d68e9ea76a7814b17f3c8e3f390f36e346889
lucykolosova <avavom61>
2022-01-26 09:41:24 +0200

    delete extra text in deposite modal

--- src/client/wallet/Deposit/Deposit.js
+++ src/client/wallet/Deposit/Deposit.js
@@ -64,10 +64,7 @@ const Deposit = () => {
           . Once the deposit is made, you will receive an equal amount of the SWAP version of the
           deposited token, which can be exchanged for other tokens on the Hive-Engine blockchain.
         </p>
-        <p>
-          There is a 1% fee on deposits. ETH, ERC-20, BNB, BEP-20, Polygon (MATIC) and Polygon
-          ERC-20 deposits have no deposit fees.
-        </p>
+        <p>There is a 1% fee on deposits.</p>
         <p>
           Please note that you will also have to pay standard network fees when sending cryptos.
         </p>
