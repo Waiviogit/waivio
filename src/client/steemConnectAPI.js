86602cff119e6502110b714767556dc90c1889a3
lucykolosova <avavom61>
2022-01-28 17:13:02 +0200

    add delegation modal

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -547,7 +547,10 @@ function sc2Extended() {
           required_auths: [],
           required_posting_auths: [user],
           id: 'waivio_hive_engine',
-          json: JSON.stringify(data),
+          json: JSON.stringify({
+            action: 'createDepositRecord',
+            payload: data,
+          }),
         };
 
         return this.broadcast([['custom_json', params]]);
