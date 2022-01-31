d1734f592b048114fdbf65638ca7bcc1ec230e35
lucykolosova <avavom61>
2022-01-31 15:34:37 +0200

    fix staging crash

--- src/client/wallet/WithdrawModal/WithdrawModal.js
+++ src/client/wallet/WithdrawModal/WithdrawModal.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Modal } from 'antd';
-import TokensSelect from '../SwapTokens/components/TokensSelect';
+import {useDispatch, useSelector} from "react-redux";
+import {isEmpty} from "lodash";
 
-const WithdrawModal = () => (
-  <Modal>
-    <section>
-      <p>
-        All crypto withdrawals are processed by{' '}
-        <a href="https://hive-engine.com/">Hive-Engine.com</a>.
-      </p>
-      <p>There is a 1% fee on withdrawals.</p>
-      <p>
-        Please note that standard network transfer fees will also be subtracted from the amount.
-      </p>
-    </section>
-    <div>
-      <h3 className="SwapTokens__title">Withdraw:</h3>
-      <TokensSelect
-        list={[]}
-        setToken={() => {}}
-        inputWrapClassList={'inputWrapClassList'}
-        amount={0}
-        handleChangeValue={() => {}}
-        token={{}}
-        handleClickBalance={() => {}}
-      />
-    </div>
-  </Modal>
-);
+import TokensSelect from '../SwapTokens/components/TokensSelect';
+import {getWithdrawList} from "../../../store/depositeWithdrawStore/depositWithdrawSelector";
+import {getDepositWithdrawPairs, resetSelectPair} from "../../../store/depositeWithdrawStore/depositeWithdrawAction";
+
+const WithdrawModal = () => {
+  const withdraList = useSelector(getWithdrawList);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isEmpty(withdraList)) dispatch(getDepositWithdrawPairs());
+
+    return () => {
+      dispatch(resetSelectPair());
+    };
+  }, []);
+
+  return (
+    <Modal visible>
+      <section>
+        <p>
+          All crypto withdrawals are processed by{' '}
+          <a href="https://hive-engine.com/">Hive-Engine.com</a>.
+        </p>
+        <p>There is a 1% fee on withdrawals.</p>
+        <p>
+          Please note that standard network transfer fees will also be subtracted from the amount.
+        </p>
+      </section>
+      <div>
+        <h3 className="SwapTokens__title">Withdraw:</h3>
+        <TokensSelect
+          list={withdraList}
+          setToken={() => {
+          }}
+          amount={0}
+          handleChangeValue={() => {}}
+          token={withdraList[0]}
+          handleClickBalance={() => {
+          }}
+        />
+      </div>
+    </Modal>
+  )
+};
 
 export default WithdrawModal;
