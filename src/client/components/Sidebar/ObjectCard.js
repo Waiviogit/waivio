ed3023c94f6b88ae52afebbaf9e17b3921a76bdd
stasy <bernastya@inbox.ru>
2022-02-17 15:38:40 +0300

    Fix avatar

--- src/client/components/Sidebar/ObjectCard.js
+++ src/client/components/Sidebar/ObjectCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { string } from 'prop-types';
 import { Link } from 'react-router-dom';
 import { isEmpty } from 'lodash';
 
 import ObjectAvatar from '../ObjectAvatar';
 import FollowButton from '../../widgets/FollowButton';
 import { getObjectName } from '../../../common/helpers/wObjectHelper';
-
 import './ObjectCard.less';
 
-const ObjectCard = ({ wobject, alt, showFollow, isNewWindow, unfollow, follow }) => {
+const ObjectCard = ({ wobject, alt, showFollow, isNewWindow, unfollow, follow, parent }) => {
   if (!isEmpty(wobject)) {
+    const updatedWObject = { ...wobject };
+    if (!wobject.avatar && isEmpty(wobject.parent)) updatedWObject.avatar = parent.avatar;
     const name = getObjectName(wobject);
     const pathname = wobject.defaultShowLink;
 
@@ -19,7 +20,7 @@ const ObjectCard = ({ wobject, alt, showFollow, isNewWindow, unfollow, follow })
         <div className="ObjectCard__top">
           <div className="ObjectCard__links">
             <Link to={pathname} title={name} target={isNewWindow ? '_blank' : null}>
-              <ObjectAvatar item={wobject} size={34} />
+              <ObjectAvatar item={updatedWObject} size={34} />
             </Link>
             <Link
               to={pathname}
@@ -51,7 +52,16 @@ const ObjectCard = ({ wobject, alt, showFollow, isNewWindow, unfollow, follow })
 };
 
 ObjectCard.propTypes = {
-  wobject: PropTypes.shape(),
+  parent: PropTypes.shape({
+    avatar: string,
+  }),
+  wobject: PropTypes.shape({
+    avatar: PropTypes.string,
+    defaultShowLink: PropTypes.string,
+    author_permlink: PropTypes.string,
+    youFollows: PropTypes.bool,
+    parent: PropTypes.shape(),
+  }),
   alt: PropTypes.node,
   showFollow: PropTypes.bool,
   isNewWindow: PropTypes.bool,
@@ -66,6 +76,7 @@ ObjectCard.defaultProps = {
   unfollow: () => {},
   follow: () => {},
   wobject: {},
+  parent: {},
 };
 
 export default ObjectCard;
--- src/client/components/Sidebar/ObjectsRelated/ObjectsRelatedContent/index.js
+++ src/client/components/Sidebar/ObjectsRelated/ObjectsRelatedContent/index.js
@@ -2,11 +2,13 @@ import { connect } from 'react-redux';
 
 import ObjectsRelatedContent from './ObjectsRelatedContent';
 import {
+  getObject,
   getRelatedObjectsForSidebar,
   getRelatedObjectsIsLoading,
 } from '../../../../../store/wObjectStore/wObjectSelectors';
 
 const mapStateToProps = state => ({
+  currWobject: getObject(state),
   isLoading: getRelatedObjectsIsLoading(state),
   objects: getRelatedObjectsForSidebar(state),
 });
