8a244d4f437fe28d92c66c751bb6b08ba6ff6018
katerynapadafa <padafa2000@gmail.com>
2022-02-23 17:54:26 +0200

    solved 3584 issue

--- src/client/components/Navigation/Notifications/Notifications.js
+++ src/client/components/Navigation/Notifications/Notifications.js
@@ -164,7 +164,7 @@ class Notifications extends React.Component {
                     values={{
                       username: <span className="username">{notification.author}</span>,
                     }}
-                    url={`/@${currentAuthUsername}/${notification.parentPermlink}/#@${notification.author}/${notification.permlink}`}
+                    url={`/@${currentAuthUsername}/${notification.parent_permlink}/#@${notification.author}/${notification.permlink}`}
                     key={key}
                     notification={notification}
                     currentAuthUsername={currentAuthUsername}
