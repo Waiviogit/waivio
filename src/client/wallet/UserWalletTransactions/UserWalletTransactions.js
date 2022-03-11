a9c0c5ea0e108b31760eaadf0ca6488bf929291a
katerynapadafa <padafa2000@gmail.com>
2022-03-11 10:10:29 +0200

    changed text color

--- src/client/wallet/UserWalletTransactions/UserWalletTransactions.js
+++ src/client/wallet/UserWalletTransactions/UserWalletTransactions.js
@@ -9,8 +9,8 @@ import { guestUserRegex } from '../../../common/helpers/regexHelpers';
 import { getTransferDetails } from '../../../waivioApi/ApiClient';
 import { handleLoadMoreTransactions } from '../WalletHelper';
 import BlocktraidsTransactionModal from '../BlocktraidsTransactionModal';
-
 import './UserWalletTransactions.less';
+import './UserWalletTransactions-nightmode.less';
 
 @injectIntl
 class UserWalletTransactions extends React.Component {
