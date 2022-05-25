3d7af04788dce0d6ff9a6570deb76a75fc1423c3
lucykolosova <avavom61>
2022-05-25 13:07:24 +0300

    fix campaing assing

--- src/common/constants/blockTypes.js
+++ src/common/constants/blockTypes.js
@@ -4,4 +4,5 @@ export const subscribeTypes = {
   votes: 'last_vote_block_num',
   posts: 'last_block_num',
   campaigns: 'campaign_last_block_num',
+  subscribeCampaignAssign: 'subscribeCampaignAssign',
 };
