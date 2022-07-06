d383a0de607bea5fdc5ce3a3e1aab7f6ecfa603e
lucykolosova <avavom61>
2022-07-06 13:38:20 +0300

    add details modal

---
+++ src/client/newRewards/DetailsModal/DetailsModal.js
@@ -0,0 +1,180 @@
+import React, { useEffect, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { get } from 'lodash';
+import { Button, Modal } from 'antd';
+import { injectIntl } from 'react-intl';
+import { useLocation } from 'react-router';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { getIsWaivio } from '../../../store/appStore/appSelectors';
+// import WebsiteReservedButtons from '../Proposition/WebsiteReservedButtons/WebsiteReservedButtons';
+// import getDetailsMessages from './detailsMessagesData';
+// import CampaignCardHeader from '../CampaignCardHeader/CampaignCardHeader';
+// import DetailsBody from './DetailsBody';
+// import DetailsPostRequirments from './DetailsPostRequirments';
+// import { clearAllSessionProposition, getSessionData } from '../rewardsHelper';
+import withAuthActions from '../../auth/withAuthActions';
+import { clearAllSessionProposition } from '../../rewards/rewardsHelper';
+import WebsiteReservedButtons from '../../rewards/Proposition/WebsiteReservedButtons/WebsiteReservedButtons';
+import { getAuthenticatedUserName } from '../../../store/authStore/authSelectors';
+import DetailsModalBody from './DetailsBody';
+import { validateEgibilitiesForUser } from '../../../waivioApi/ApiClient';
+
+// import './DetailsModal.less';
+
+const DetailsModal = ({
+  proposition,
+  toggleModal,
+  isModalDetailsOpen,
+  loading,
+  reserveOnClickHandler,
+  history,
+  removeToggleFlag,
+  onActionInitiated,
+}) => {
+  const location = useLocation();
+  const authorizedUserName = useSelector(getAuthenticatedUserName);
+  const isAuth = !!authorizedUserName;
+  const isWidget = new URLSearchParams(history.location.search).get('display');
+  const isReserved = new URLSearchParams(location.search).get('toReserved');
+  const isWaivio = useSelector(getIsWaivio);
+  const requiredObject = get(proposition.object, 'parent');
+  const userName = useSelector(getAuthenticatedUserName);
+  const [requirements, setRequirements] = useState({});
+  const isEligible = Object.values(requirements).every(req => req);
+
+  useEffect(() => {
+    validateEgibilitiesForUser({
+      userName,
+      activationPermlink: proposition?.activationPermlink,
+    }).then(res => {
+      setRequirements(res);
+    });
+  }, [proposition?.activationPermlink, userName]);
+  // const requiredObjectName = getObjectName(proposition?.object?.parent)
+  // const userName = getSessionData('userName');
+
+  // const isExpired = objectDetails.status === 'expired';
+  // const isInActive = objectDetails.status === 'inactive';
+  // const proposedWobjName = getObjectName(proposition.object);
+
+  const handleTypeReserveButton = () => (isAuth ? 'primary' : 'default');
+
+  const onClick = () => onActionInitiated(reserveOnClickHandler);
+  // const disabled = (isAuth && !isEligible) || isInActive || isExpired;
+
+  // const mainObjectPermLink = get(objectDetails, 'required_object.author_permlink');
+  // const mainObject = `[${requiredObjectName}](${'mainObjectPermLink'})`;
+  // const secondaryObject = `[${proposedWobjName}](${proposition.object.author_permlink})`;
+
+  // const urlConfig = {
+  //   pathname: '/editor',
+  //   search: `?object=${mainObject}&object=${secondaryObject}`,
+  //   state: {
+  //     mainObject,
+  //     secondaryObject,
+  //     // campaign: objectDetails._id,
+  //   },
+  // };
+
+  // const toCurrentWobjLink = `/object/${proposition.object.author_permlink}`;
+  const reserveButton = isWaivio ? (
+    <Button
+      type={handleTypeReserveButton()}
+      loading={loading}
+      disabled={!isEligible}
+      onClick={onClick}
+    >
+      Reserve
+    </Button>
+  ) : (
+    <WebsiteReservedButtons
+      dish={proposition?.object}
+      restaurant={requiredObject}
+      handleReserve={reserveOnClickHandler}
+    />
+  );
+
+  // const handleWriteReviewBtn = () => {
+  //   if (!isAuth) return;
+  //   if (userName) {
+  //     const historyUrl = isEqual(userName, authorizedUserName) ? urlConfig : toCurrentWobjLink;
+  //
+  //     history.push(historyUrl);
+  //     clearAllSessionProposition();
+  //
+  //     return;
+  //   }
+  //   history.push(urlConfig);
+  // };
+
+  const handleCancelModalBtn = value => {
+    clearAllSessionProposition();
+    if (!isWidget && isReserved) {
+      removeToggleFlag();
+      history.push(`/object/${proposition.object.author_permlink}`);
+    }
+
+    return toggleModal(value);
+  };
+
+  return (
+    <Modal
+      title={<div className="Details__modal-title">We seek honest reviews!</div>}
+      closable
+      onCancel={handleCancelModalBtn}
+      maskClosable={false}
+      visible={!isWidget && isModalDetailsOpen}
+      wrapClassName="Details"
+      footer={null}
+      width={768}
+    >
+      <div>
+        <p className="Proposition-new__title">
+          Share {proposition?.requirements?.minPhotos} photos of the dish and earn crypto
+        </p>
+        <div className="Proposition-new__sponsorInfo">
+          <div className="Proposition-new__infoItem Proposition-new__infoItem--right">
+            <Link to={`/@${proposition?.guideName}`}>Waivio Service (Sponsor)</Link>
+            <Link to={`/@${proposition?.guideName}`}>@{proposition?.guideName}</Link>
+          </div>
+          <div className="Proposition-new__infoItem Proposition-new__infoItem--left">
+            <span>Total paid (liquid):</span>
+            <span>
+              {proposition?.totalPayed || 0} {proposition?.payoutToken}
+            </span>
+          </div>
+        </div>
+      </div>
+      <DetailsModalBody proposition={proposition} requirements={requirements} />
+      <div className="Details__footer">
+        <div className="Details__footer-reserve-btn">
+          <Button onClick={handleCancelModalBtn}>Cancel</Button>
+          {reserveButton}
+          {proposition?.countReservationDays &&
+            isWaivio &&
+            `for days ${proposition?.countReservationDays} days`}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+DetailsModal.propTypes = {
+  toggleModal: PropTypes.func.isRequired,
+  isModalDetailsOpen: PropTypes.bool.isRequired,
+  loading: PropTypes.bool,
+  reserveOnClickHandler: PropTypes.func.isRequired,
+  history: PropTypes.shape().isRequired,
+  removeToggleFlag: PropTypes.func,
+  proposition: PropTypes.shape().isRequired,
+  onActionInitiated: PropTypes.func.isRequired,
+};
+
+DetailsModal.defaultProps = {
+  loading: false,
+  assigned: false,
+  isAuth: false,
+  removeToggleFlag: () => {},
+};
+export default withRouter(injectIntl(withAuthActions(DetailsModal)));
--- src/client/newRewards/PropositionList/PropositionList.js
+++ src/client/newRewards/PropositionList/PropositionList.js
@@ -1,28 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { isEmpty } from 'lodash';
-
+import { useSelector } from 'react-redux';
+import { getAuthenticatedUserName } from '../../../store/authStore/authSelectors';
 import { getObject, getPropositionByCampaingObjectPermlink } from '../../../waivioApi/ApiClient';
 import ReduxInfiniteScroll from '../../vendor/ReduxInfiniteScroll';
 import Loading from '../../components/Icon/Loading';
 import EmptyCampaing from '../../statics/EmptyCampaing';
 import Proposition from '../reuseble/Proposition/Proposition';
+import { getObjectName } from '../../../common/helpers/wObjectHelper';
+
+import './PropositionList.less';
 
 const PropositionList = () => {
   const { requiredObject } = useParams();
+  const authUserName = useSelector(getAuthenticatedUserName);
   const [propositions, setPropositions] = useState();
   const [hasMore, setHasMore] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [parent, setParent] = useState(null);
 
   const getPropositionList = async () => {
-    const res = await getPropositionByCampaingObjectPermlink(requiredObject);
-    const campParent = await getObject(requiredObject);
+    if (requiredObject) {
+      const campParent = await getObject(requiredObject);
+
+      setParent(campParent);
+    }
+
+    const res = await getPropositionByCampaingObjectPermlink(requiredObject, authUserName);
 
     setPropositions(res.rewards);
     setHasMore(res.hasMore);
     setLoading(false);
-    setParent(campParent);
   };
 
   useEffect(() => {
@@ -34,6 +43,7 @@ const PropositionList = () => {
     try {
       const res = await getPropositionByCampaingObjectPermlink(
         requiredObject,
+        authUserName,
         propositions?.length,
       );
 
@@ -45,9 +55,19 @@ const PropositionList = () => {
     }
   };
 
+  if (loading && isEmpty(propositions)) return <Loading />;
+
   return (
     <div>
-      <h2>All rewards</h2>
+      <div className="PropositionList__breadcrumbs">
+        <h2>All rewards</h2>
+        {requiredObject && (
+          <div className="PropositionList__parent">
+            <span className="PropositionList__icon">&#62;</span>{' '}
+            <span>{getObjectName(parent)}</span>
+          </div>
+        )}
+      </div>
       {isEmpty(propositions) ? (
         <EmptyCampaing />
       ) : (
