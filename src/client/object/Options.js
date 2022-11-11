ab3d8f9ed38dd21c6c85177a192b9c807dc5050d
katerynapadafa <padafa2000@gmail.com>
2022-11-11 10:46:52 +0200

    changed names

--- src/client/object/Options.js
+++ src/client/object/Options.js
@@ -18,12 +18,12 @@ const Options = ({ wobject, isEditMode, setHoveredOption, history }) => {
   const optionsDiv = useRef();
 
   useEffect(() => {
-    const reffff = document.getElementById('ObjectHeaderId');
+    const objectHeaderEl = document.getElementById('ObjectHeaderId');
 
     activeStoreOption &&
       isMobile() &&
       window.scrollTo({
-        top: reffff?.offsetHeight,
+        top: objectHeaderEl?.offsetHeight,
         behavior: 'smooth',
       });
   }, [wobject]);
