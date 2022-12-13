d8c2deb0f22870bee9ae7d22d811c3754b55c632
katerynapadafa <padafa2000@gmail.com>
2022-12-13 16:23:05 +0200

    changed title

--- src/client/websites/WebsiteLayoutComponents/Header/MainPageHeader.js
+++ src/client/websites/WebsiteLayoutComponents/Header/MainPageHeader.js
@@ -27,7 +27,7 @@ const MainPageHeader = props => {
         <div className="MainPageHeader__logo">
           <Link to="/" className="MainPageHeader__logoLink">
             <img src={currentLogo} className="MainPageHeader__logoImg" alt="logo" />
-            <b className="MainPageHeader__name">{currHost.toUpperCase()}</b>
+            <b className="MainPageHeader__name">{currHost}</b>
           </Link>
           {!props.withMap && (
             <Link to="/map?type=restaurant&showPanel=true" className="MainPageHeader__link">
