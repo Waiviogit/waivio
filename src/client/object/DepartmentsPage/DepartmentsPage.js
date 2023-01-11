9811e65d9edcedaa48f3d41ebec8a5992c0ea886
katerynapadafa <padafa2000@gmail.com>
2023-01-11 17:31:16 +0200

    removed skip

--- src/client/object/DepartmentsPage/DepartmentsPage.js
+++ src/client/object/DepartmentsPage/DepartmentsPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { isEmpty } from 'lodash';
-// import PropTypes from "prop-types";
 import { FormattedMessage } from 'react-intl';
 import InfiniteScroll from 'react-infinite-scroller';
 import Loading from '../../components/Icon/Loading';
@@ -9,30 +8,24 @@ import ObjectCardView from '../../objectCard/ObjectCardView';
 import { getObjectsByDepartment } from '../../../waivioApi/ApiClient';
 import { getActiveDepartment } from '../../../store/objectDepartmentsStore/objectDepartmentsSelectors';
 
+const limit = 10;
+
 const DepartmentsPage = () => {
   const [hasMore, setHasMore] = useState(false);
   const [filteredObjects, setFilteredObjects] = useState([]);
-  let skip = 0;
-  const limit = 10;
   const activeDepartment = useSelector(getActiveDepartment);
 
   useEffect(() => {
     setFilteredObjects([]);
     if (!isEmpty(activeDepartment))
-      getObjectsByDepartment([activeDepartment.name], skip, limit).then(r => {
+      getObjectsByDepartment([activeDepartment.name], 0, limit).then(r => {
         setHasMore(r.hasMore);
         setFilteredObjects(r.wobjects);
       });
   }, [activeDepartment]);
 
-  useEffect(() => {
-    if (filteredObjects.length >= limit) {
-      skip += filteredObjects.length;
-    }
-  }, [filteredObjects]);
-
   const loadMoreRelatedObjects = () => {
-    getObjectsByDepartment([activeDepartment.name], skip, limit).then(r => {
+    getObjectsByDepartment([activeDepartment.name], filteredObjects.length, limit).then(r => {
       setHasMore(r.hasMore);
       setFilteredObjects([...filteredObjects, ...r.wobjects]);
     });
@@ -58,10 +51,6 @@ const DepartmentsPage = () => {
       </InfiniteScroll>
     </>
   );
-};
-
-DepartmentsPage.propTypes = {
-  // wobject: PropTypes.shape().isRequired
 };
 
 export default DepartmentsPage;
