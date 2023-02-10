aea31570660c9613d2ae0566e590a499be6d9ca8
lucykolosova <avavom61>
2023-02-10 15:33:37 +0200

    fix issues

--- src/client/newRewards/DetailsModal/DetailsBody.js
+++ src/client/newRewards/DetailsModal/DetailsBody.js
@@ -17,7 +17,7 @@ const DetailsModalBody = ({ proposition, requirements, agreementObjects, intl })
   const getClassForCurrCreteria = creteria => classNames({ 'criteria-row__required': !creteria });
   const rate = useSelector(getRate);
   const rewardFund = useSelector(getRewardFund);
-  const requiredObject = proposition.requiredObject || proposition.object;
+  const requiredObject = proposition.requiredObject;
   const minExpertise = getMinExpertise({
     campaignMinExpertise: proposition?.userRequirements?.minExpertise,
     rewardFundRecentClaims: rewardFund.recent_claims,
