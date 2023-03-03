81c41f80fac59a439c8d59a867536ce4e462bdaf
lucykolosova <avavom61>
2023-03-03 10:29:42 +0200

    change user departments

--- src/client/Shop/ShopDepartments/ShopDepartmentsList.js
+++ src/client/Shop/ShopDepartments/ShopDepartmentsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useRouteMatch } from 'react-router';
 import { Modal } from 'antd';
@@ -18,10 +18,8 @@ const ShopDepartmentsList = ({ visible, onClose, getShopDepartments, path }) => 
     });
   }, []);
 
-  const excludedMain = useMemo(
-    () => departments.map(d => d.name).filter(d => d === match.params.department),
-    [match.params.department, departments],
-  );
+  const excludedMain = departments.map(d => d.name);
+
   const renderDep = match.params.department
     ? departments.filter(d => d.name === match.params.department)
     : departments;
