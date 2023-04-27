0d06168ec18c3099245a87032f67f99a1efcb261
katerynapadafa <padafa2000@gmail.com>
2023-04-27 09:32:56 +0300

    added  pictures lightbox for description page

--- src/client/object/Description/DescriptionPage.js
+++ src/client/object/Description/DescriptionPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import Lightbox from 'react-image-lightbox';
 import { get, isEmpty } from 'lodash';
 import { getObject } from '../../../waivioApi/ApiClient';
 import './DescriptionPage.less';
@@ -7,6 +8,8 @@ import { isMobile } from '../../../common/helpers/apiHelpers';
 
 const DescriptionPage = ({ match }) => {
   const [wobject, setWobject] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
   const wobjName = match.params.name;
   const description = wobject?.description;
   const pics = [...get(wobject, 'preview_gallery', [])];
@@ -25,17 +28,32 @@ const DescriptionPage = ({ match }) => {
     if (isEmpty(wobject)) getObject(wobjName).then(res => setWobject(res));
   }, [wobject.author_permlink]);
 
+  const onPicClick = (e, pic) => {
+    setIsOpen(true);
+    setPhotoIndex(pics.indexOf(pic));
+  };
+
   return (
     <div className="DescriptionPage Body">
       <div className="DescriptionPage__body">{description}</div>
       <div>
         {!isEmpty(pictures) &&
           pictures?.map(p => (
-            <div key={p.id}>
+            <div key={p.id} onClick={e => onPicClick(e, p)}>
               <img className="DescriptionPage__image" src={p.body} alt=" " />
             </div>
           ))}
       </div>
+      {isOpen && (
+        <Lightbox
+          mainSrc={pictures[photoIndex]?.body}
+          nextSrc={pictures[(photoIndex + 1) % pictures.length]?.body}
+          prevSrc={pictures[(photoIndex - 1) % pictures.length]?.body}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() => setPhotoIndex((photoIndex - 1) % pictures.length)}
+          onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % pictures.length)}
+        />
+      )}
     </div>
   );
 };
