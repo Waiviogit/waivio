9ebdf8f611e8b43233796103eb4d5e73afaf2518
lucykolosova <avavom61>
2023-05-31 20:29:13 +0300

    add to switch new websites

--- src/client/websites/WebsiteTools/Configuration/SwitchConfigPage.js
+++ src/client/websites/WebsiteTools/Configuration/SwitchConfigPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useRouteMatch } from 'react-router';
 import ShopWebsiteConfigurations from './ConfigPages/ShopWebsiteConfigurations';
 import WebsitesConfigurations from './ConfigPages/WebsitesConfigurations';
+import { socialDomens } from '../../../social-gifts/listOfSocialWebsites';
 
 const SwitchConfigPage = () => {
   const match = useRouteMatch();
 
-  if (match.params.site.includes('.socialgifts.')) return <ShopWebsiteConfigurations />;
+  if (socialDomens.some(item => match.params.site.includes(item)))
+    return <ShopWebsiteConfigurations />;
 
   return <WebsitesConfigurations />;
 };
--- src/routes/switchRoutes.js
+++ src/routes/switchRoutes.js
@@ -1,9 +1,10 @@
 import websitesRoutes from './configs/websitesRoutes';
 import routes from './configs/routes';
 import socialRoutes from '../client/social-gifts/routes';
+import { socialDomens } from '../client/social-gifts/listOfSocialWebsites';
 
 const switchRoutes = (host, page) => {
-  if (host.includes('socialgifts')) return socialRoutes(host, page);
+  if (socialDomens.some(item => host.includes(item))) return socialRoutes(host, page);
   if (host.includes('dining')) return websitesRoutes(host, page);
 
   return routes;
