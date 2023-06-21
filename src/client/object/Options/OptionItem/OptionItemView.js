96e7c1dbdf88adf9a84f6d18d0d6c77eb2e69c96
katerynapadafa <padafa2000@gmail.com>
2023-06-21 13:17:32 +0300

    new galery

--- src/client/object/Options/OptionItem/OptionItemView.js
+++ src/client/object/Options/OptionItem/OptionItemView.js
@@ -12,6 +12,7 @@ import {
 import { getActiveOption } from '../../../../store/optionsStore/optionsSelectors';
 import LinkButton from '../../../components/LinkButton/LinkButton';
 import { isMobile } from '../../../../common/helpers/apiHelpers';
+import { getProxyImageURL } from '../../../../common/helpers/image';
 
 const OptionItemView = ({
   option,
@@ -139,7 +140,7 @@ const OptionItemView = ({
                 onMouseOut={onMouseOut}
                 onClick={e => onOptionButtonClick(e, el)}
                 className={getOptionsPicturesClassName(el)}
-                src={el.body.image}
+                src={getProxyImageURL(el.body.image)}
                 alt="option"
                 key={el.permlink}
               />
