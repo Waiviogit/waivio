21cff121d1090d15ec3a0095367c3eb638f24f5b
fesmofet <borees2@gmail.com>
2023-08-02 15:33:35 +0300

    cache for seo

---
+++ src/common/constants/ssrData.js
@@ -0,0 +1,5 @@
+export const USER_AGENT = 'User-Agent';
+export const SSR_CACHE_KEY = 'ssr_cache:';
+export const USER_AGENTS_KEY = 'user_agent_bots:';
+export const  SSR_CACHE_TTL = 86400;
+export const  REDIS_DB_NUM = 10;
---
+++ src/server/handlers/cachePageHandler.js
@@ -0,0 +1,28 @@
+import redis from '../redis/operations';
+import { SSR_CACHE_KEY, SSR_CACHE_TTL, USER_AGENT, USER_AGENTS_KEY } from '../../common/constants/ssrData';
+import USER_AGENTS from '../../common/constants/googleUserAgents';
+
+export const isSearchBot = async (req) => {
+  const userAgent = req.get(USER_AGENT);
+  if (!userAgent) return false;
+  const { result } = await redis.sismember({ key: USER_AGENTS_KEY, member: userAgent });
+  return !!result;
+}
+const getUrlForRedis = (req) => `${SSR_CACHE_KEY}${req.hostname}${req.url}`;
+
+export const getCachedPage = async (req) => {
+  const {result} = await redis.get({ key: getUrlForRedis(req) });
+  return result;
+}
+
+export const setCachedPage = async ({ page, req }) => {
+  const key = getUrlForRedis(req);
+
+  await redis.set({ key, data: page });
+  await redis.expire({ key, time: SSR_CACHE_TTL });
+}
+
+
+export const loadUserAgents = async () => {
+  await redis.sadd({ key: USER_AGENTS_KEY, member: USER_AGENTS });
+}
