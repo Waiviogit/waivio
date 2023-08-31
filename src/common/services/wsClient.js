c82e906951678af2db1855931d121818fb309767
fesmofet <borees2@gmail.com>
2023-08-31 16:37:24 +0300

    ws socket cache

---
+++ src/common/services/wsClient.js
@@ -0,0 +1,95 @@
+import WebSocket from 'ws';
+import events from 'events';
+
+const emitter = new events.EventEmitter();
+
+const REQUESTS_TO_DISABLE = 15;
+const REQUESTS_TO_RENEW = 3000;
+
+const HIVE_SOCKET_ERR = {
+  ERROR: 'error socket closed',
+  DISABLED: 'socket disabled',
+  CLOSED: 'connection close',
+  TIMEOUT: 'Timeout exceed',
+};
+
+class SocketClient {
+  constructor(url) {
+    this.url = url;
+    this.timeoutCount = 0;
+  }
+
+  async init() {
+    return new Promise(resolve => {
+      this.ws = new WebSocket(this.url);
+
+      this.ws.on('error', () => {
+        console.error('error socket closed');
+        this.ws.close();
+        resolve({ error: new Error(HIVE_SOCKET_ERR.ERROR) });
+      });
+
+      this.ws.on('message', message => {
+        try {
+          const data = JSON.parse(message.toString());
+
+          emitter.emit(data.id, { data, error: data.error });
+          // eslint-disable-next-line no-empty
+        } catch (error) {}
+      });
+
+      this.ws.on('open', () => {
+        setTimeout(() => {
+          resolve(this.ws);
+        }, 100);
+      });
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getUniqId() {
+    return `${Date.now().toString()}#${Math.random()
+      .toString(36)
+      .substr(2, 9)}`;
+  }
+
+  async sendMessage(message = {}) {
+    if (this.timeoutCount >= REQUESTS_TO_DISABLE) {
+      this.timeoutCount++;
+      if (this.timeoutCount > REQUESTS_TO_RENEW) {
+        this.timeoutCount = 0;
+      }
+
+      return { error: new Error(HIVE_SOCKET_ERR.TIMEOUT) };
+    }
+    if (this?.ws?.readyState !== 1) {
+      await this.init();
+    }
+
+    return new Promise(resolve => {
+      if (this.ws.readyState !== 1) {
+        resolve({ error: new Error(HIVE_SOCKET_ERR.CLOSED) });
+      }
+
+      const id = this.getUniqId();
+
+      // eslint-disable-next-line no-param-reassign
+      message.id = id;
+      this.ws.send(JSON.stringify(message));
+      emitter.once(id, ({ data, error }) => {
+        if (error) resolve({ error });
+        resolve(data);
+      });
+
+      setTimeout(() => {
+        if (emitter.eventNames().includes(id)) {
+          this.timeoutCount++;
+          emitter.off(id, () => {});
+          resolve({ error: new Error(HIVE_SOCKET_ERR.TIMEOUT) });
+        }
+      }, 2 * 1000);
+    });
+  }
+}
+
+export default SocketClient;
---
+++ src/server/seo-service/seoServiceApi.js
@@ -0,0 +1,64 @@
+import SocketClient from '../../common/services/wsClient';
+import { CONNECTION_STRING_SEO } from '../../common/constants/ssrData';
+
+const socketClient = new SocketClient(CONNECTION_STRING_SEO);
+const getPageByUrl = async ({ url }) => {
+  const result = await socketClient.sendMessage({
+    name: 'webPage',
+    method: 'getPageByUrl',
+    args: [{ url }],
+  });
+  if (result?.error) {
+    return '';
+  }
+  return result.data;
+};
+
+const createPage = async ({ url, page }) => {
+  const result = await socketClient.sendMessage({
+    name: 'webPage',
+    method: 'createPage',
+    args: [{ url, page }],
+  });
+  if (result?.error) {
+    return { error: result.error };
+  }
+  return result.data;
+};
+
+const userAgentExists = async ({ userAgent }) => {
+  const result = await socketClient.sendMessage({
+    name: 'botAgent',
+    method: 'userAgentExists',
+    args: [{ userAgent }],
+  });
+  if (result?.error) {
+    return false;
+  }
+  return result.data;
+};
+
+const addVisit = async ({ userAgent }) => {
+  const result = await socketClient.sendMessage({
+    name: 'botStatistics',
+    method: 'addVisit',
+    args: [{ userAgent }],
+  });
+  if (result?.error) {
+    return { error: result.error };
+  }
+  return result.data;
+};
+
+export const webPage = {
+  getPageByUrl,
+  createPage,
+};
+
+export const botStatistics = {
+  addVisit,
+};
+
+export const botAgent = {
+  userAgentExists,
+};
