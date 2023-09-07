4a3553685db85902b66a06c0e62418db268fa9b6
katerynapadafa <padafa2000@gmail.com>
2023-09-07 09:36:28 +0300

    refactored wobjectContainer

--- src/client/object/Wobj/WobjectContainer.js
+++ src/client/object/Wobj/WobjectContainer.js
@@ -1,45 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+import PropTypes from 'prop-types';
 import { get, isEmpty, isNil } from 'lodash';
-import OBJECT_TYPE from '../const/objectTypes';
-import {
-  clearObjectFromStore,
-  getNearbyObjects as getNearbyObjectsAction,
-  getObject,
-  getObjectFollowers as getObjectFollowersAction,
-} from '../../../store/wObjectStore/wobjectsActions';
-import {
-  getAlbums,
-  resetGallery,
-  addAlbumToStore,
-  clearRelatedPhoto,
-} from '../../../store/galleryStore/galleryActions';
-import { objectFields } from '../../../common/constants/listOfFields';
-import {
-  getObjectName,
-  getUpdateFieldName,
-  prepareAlbumData,
-  prepareAlbumToStore,
-  showDescriptionPage,
-} from '../../../common/helpers/wObjectHelper';
-import {
-  setNestedWobject,
-  setCatalogBreadCrumbs,
-  getWobjectExpertise as getWobjectExpertiseAction,
-  getRelatedWobjects,
-} from '../../../store/wObjectStore/wobjActions';
-import { appendObject, getUpdates } from '../../../store/appendStore/appendActions';
+import { withRouter } from 'react-router-dom';
 import Wobj from './Wobj';
-import NotFound from '../../statics/NotFound';
-import {
-  getHelmetIcon,
-  getIsWaivio,
-  getScreenSize,
-  getSiteName,
-  getWeightValue,
-} from '../../../store/appStore/appSelectors';
+import { getAppendList } from '../../../store/appendStore/appendSelectors';
 import {
   getAuthenticatedUser,
   getAuthenticatedUserName,
@@ -53,271 +18,239 @@ import {
   getWobjectNested,
 } from '../../../store/wObjectStore/wObjectSelectors';
 import { getLocale } from '../../../store/settingsStore/settingsSelectors';
+import {
+  getHelmetIcon,
+  getIsWaivio,
+  getScreenSize,
+  getSiteName,
+  getWeightValue,
+} from '../../../store/appStore/appSelectors';
+
 import { getConfiguration } from '../../../store/websiteStore/websiteSelectors';
-import { getRate, getRewardFund } from '../../../store/appStore/appActions';
+import {
+  clearObjectFromStore,
+  getNearbyObjects as getNearbyObjectsAction,
+  getObject,
+  getObjectFollowers as getObjectFollowersAction,
+} from '../../../store/wObjectStore/wobjectsActions';
+import {
+  getRelatedWobjects,
+  getWobjectExpertise as getWobjectExpertiseAction,
+  setCatalogBreadCrumbs,
+  setNestedWobject,
+} from '../../../store/wObjectStore/wobjActions';
+import {
+  addAlbumToStore,
+  clearRelatedPhoto,
+  getAlbums,
+  resetGallery,
+} from '../../../store/galleryStore/galleryActions';
+import { appendObject, getUpdates } from '../../../store/appendStore/appendActions';
 import { setStoreActiveOption } from '../../../store/optionsStore/optionsActions';
-import { login } from '../../../store/authStore/authActions';
 import { resetBreadCrumb } from '../../../store/shopStore/shopActions';
-import { getAppendList } from '../../../store/appendStore/appendSelectors';
+import {
+  getObjectName,
+  getUpdateFieldName,
+  prepareAlbumData,
+  prepareAlbumToStore,
+  showDescriptionPage,
+} from '../../../common/helpers/wObjectHelper';
+import NotFound from '../../statics/NotFound';
+import OBJECT_TYPE from '../const/objectTypes';
+import { objectFields } from '../../../common/constants/listOfFields';
+import { login } from '../../../store/authStore/authActions';
+import { getRate, getRewardFund } from '../../../store/appStore/appActions';
 
-@withRouter
-@connect(
-  state => ({
-    updates: getAppendList(state),
-    authenticated: getIsAuthenticated(state),
-    authenticatedUser: getAuthenticatedUser(state),
-    authenticatedUserName: getAuthenticatedUserName(state),
-    loaded: getWobjectIsFatching(state),
-    failed: getWobjectIsFailed(state),
-    locale: getLocale(state),
-    wobject: getObjectState(state),
-    nestedWobject: getWobjectNested(state),
-    isFetching: getObjectFetchingState(state),
-    screenSize: getScreenSize(state),
-    helmetIcon: getHelmetIcon(state),
-    isWaivio: getIsWaivio(state),
-    supportedObjectTypes: get(getConfiguration(state), 'supported_object_types'),
-    weightValue: getWeightValue(state, getObjectState(state).weight),
-    siteName: getSiteName(state),
-  }),
-  {
-    clearObjectFromStore,
-    setCatalogBreadCrumbs,
-    setNestedWobject,
-    getObject,
-    resetGallery,
-    getAlbums,
-    appendObject,
-    getUpdates,
-    addAlbumToStore,
-    clearRelatedPhoto,
-    getNearbyObjects: getNearbyObjectsAction,
-    getWobjectExpertise: getWobjectExpertiseAction,
-    getObjectFollowers: getObjectFollowersAction,
-    getRelatedWobjects,
-    setStoreActiveOption,
-    resetBreadCrumb,
-  },
-)
-class WobjectContainer extends React.Component {
-  static propTypes = {
-    route: PropTypes.shape().isRequired,
-    authenticatedUserName: PropTypes.string.isRequired,
-    authenticated: PropTypes.bool.isRequired,
-    match: PropTypes.shape().isRequired,
-    history: PropTypes.shape().isRequired,
-    location: PropTypes.shape({
-      hash: PropTypes.string,
-    }).isRequired,
-    failed: PropTypes.bool,
-    isFetching: PropTypes.bool,
-    getObject: PropTypes.func.isRequired,
-    resetBreadCrumb: PropTypes.func.isRequired,
-    weightValue: PropTypes.number.isRequired,
-    supportedObjectTypes: PropTypes.arrayOf(PropTypes.string),
-    isWaivio: PropTypes.bool.isRequired,
-    resetGallery: PropTypes.func.isRequired,
-    wobject: PropTypes.shape(),
-    nestedWobject: PropTypes.shape(),
-    updates: PropTypes.arrayOf(),
-    clearObjectFromStore: PropTypes.func,
-    setNestedWobject: PropTypes.func,
-    setCatalogBreadCrumbs: PropTypes.func,
-    locale: PropTypes.string,
-    helmetIcon: PropTypes.string.isRequired,
-    siteName: PropTypes.string.isRequired,
-    getAlbums: PropTypes.func,
-    appendObject: PropTypes.func,
-    getUpdates: PropTypes.func,
-    addAlbumToStore: PropTypes.func,
-    clearRelatedPhoto: PropTypes.func,
-    getNearbyObjects: PropTypes.func.isRequired,
-    getWobjectExpertise: PropTypes.func.isRequired,
-    getObjectFollowers: PropTypes.func.isRequired,
-    getRelatedWobjects: PropTypes.func.isRequired,
-    setStoreActiveOption: PropTypes.func.isRequired,
-  };
+const WobjectContainer = props => {
+  const [isEditMode, setIsEditMode] = useState(
+    props.wobject.type === OBJECT_TYPE.PAGE &&
+      props.authenticated &&
+      !props.wobject[objectFields.pageContent],
+  );
+  const objectName = getObjectName(props.wobject);
+  const newsFilter =
+    props.match.params[1] === 'newsFilter' ? { newsFilter: props.match.params.itemId } : {};
 
-  static defaultProps = {
-    getAlbums: () => {},
-    authenticatedUserName: '',
-    locale: '',
-    loaded: false,
-    failed: false,
-    isFetching: false,
-    supportedObjectTypes: [],
-    wobject: {},
-    clearObjectFromStore: () => {},
-    setCatalogBreadCrumbs: () => {},
-    setNestedWobject: () => {},
-    appendObject: () => {},
-    addAlbumToStore: () => {},
-    getRelatedAlbum: () => {},
-    clearRelatedPhoto: () => {},
-  };
+  const toggleViewEditMode = () => setIsEditMode(!isEditMode);
 
-  static async fetchData({ store, match }) {
-    const res = await store.dispatch(login());
-
-    return Promise.all([
-      store.dispatch(getObject(match.params.name, res?.value?.name)),
-      store.dispatch(getObjectFollowersAction({ object: match.params.name, skip: 0, limit: 5 })),
-      store.dispatch(getRate()),
-      store.dispatch(getRewardFund()),
-      store.dispatch(getNearbyObjectsAction(match.params.name)),
-      store.dispatch(
-        getWobjectExpertiseAction(
-          match.params[1] === 'newsFilter' ? { newsFilter: match.params.itemId } : {},
-          match.params.name,
-        ),
-      ),
-    ]);
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEditMode:
-        props.wobject.type === OBJECT_TYPE.PAGE &&
-        props.authenticated &&
-        !props.wobject[objectFields.pageContent],
-    };
-  }
-
-  componentDidMount() {
-    const { match, authenticatedUserName, history, locale, isWaivio } = this.props;
-    const newsFilter = match.params[1] === 'newsFilter' ? { newsFilter: match.params.itemId } : {};
-
-    this.props.getObject(match.params.name, authenticatedUserName).then(async res => {
-      if ((await showDescriptionPage(res.value, locale)) && !match.params[0] && isWaivio) {
-        history.push(`/object/${res.value.author_permlink}/description`);
-      }
-      this.props.getAlbums(match.params.name);
-      this.props.getNearbyObjects(match.params.name);
-      this.props.getWobjectExpertise(newsFilter, match.params.name);
-      this.props.getObjectFollowers({
-        object: match.params.name,
-        skip: 0,
-        limit: 5,
-        userName: authenticatedUserName,
-      });
-      this.props.getRelatedWobjects(match.params.name);
-      if (isEmpty(this.props.updates) || isNil(this.props.updates) || isNil(match.params[1])) {
-        const field = getUpdateFieldName(match.params[1]);
-
-        this.props.getUpdates(match.params.name, field, 'createdAt');
-      }
-    });
-  }
-
-  componentDidUpdate(prevProps) {
-    const { authenticatedUserName, match, locale, history, isWaivio } = this.props;
-    const newsFilter = match.params[1] === 'newsFilter' ? { newsFilter: match.params.itemId } : {};
-
-    if (
-      prevProps.match.params.name !== match.params.name ||
-      prevProps.locale !== locale ||
-      prevProps.authenticatedUserName !== authenticatedUserName
-    ) {
-      this.props.getAlbums(match.params.name);
-      this.props.getObject(match.params.name, authenticatedUserName).then(async res => {
-        if ((await showDescriptionPage(res.value, locale)) && !match.params[0] && isWaivio) {
-          history.push(`/object/${res.value.author_permlink}/description`);
-        }
-      });
-      this.props.resetGallery();
-      this.props.clearObjectFromStore();
-      this.props.setCatalogBreadCrumbs([]);
-      this.props.setNestedWobject({});
-      this.props.getRelatedWobjects(match.params.name);
-      this.props.getWobjectExpertise(newsFilter, match.params.name);
-      this.props.resetBreadCrumb();
-      this.props.getObjectFollowers({
-        object: match.params.name,
-        skip: 0,
-        limit: 5,
-        userName: authenticatedUserName,
-      });
-    }
-  }
-
-  componentWillUnmount() {
-    this.props.clearObjectFromStore();
-    this.props.setCatalogBreadCrumbs([]);
-    this.props.setNestedWobject({});
-    this.props.clearRelatedPhoto();
-    this.props.setStoreActiveOption({});
-    this.props.resetBreadCrumb();
-    this.props.resetGallery();
-  }
-
-  toggleViewEditMode = () => this.setState(prevState => ({ isEditMode: !prevState.isEditMode }));
-
-  appendAlbum = () => {
+  const appendAlbum = () => {
     const formData = {
       galleryAlbum: 'Photos',
     };
 
-    const { authenticatedUserName, wobject } = this.props;
-    const data = prepareAlbumData(formData, authenticatedUserName, wobject);
+    const data = prepareAlbumData(formData, props.authenticatedUserName, props.wobject);
     const album = prepareAlbumToStore(data);
 
-    const { author } = this.props.appendObject(data);
+    const { author } = props.appendObject(data);
 
-    this.props.addAlbumToStore({ ...album, author });
+    props.addAlbumToStore({ ...album, author });
   };
 
-  render() {
-    const {
-      route,
-      authenticated,
-      failed,
-      authenticatedUserName,
-      match,
-      wobject,
-      nestedWobject,
-      isFetching,
-      history,
-      location,
-    } = this.props;
-    const { isEditMode } = this.state;
-    const objectName = getObjectName(wobject);
+  useEffect(() => {
+    props.getObject(props.match.params.name, props.authenticatedUserName).then(async res => {
+      if (
+        (await showDescriptionPage(res.value, props.locale)) &&
+        !props.match.params[0] &&
+        props.isWaivio
+      ) {
+        props.history.push(`/object/${res.value.author_permlink}/description`);
+      }
+      props.getAlbums(props.match.params.name);
+      props.getNearbyObjects(props.match.params.name);
+      props.getWobjectExpertise(newsFilter, props.match.params.name);
+      props.getObjectFollowers({
+        object: props.match.params.name,
+        skip: 0,
+        limit: 5,
+        userName: props.authenticatedUserName,
+      });
+      props.getRelatedWobjects(props.match.params.name);
+      if (isEmpty(props.updates) || isNil(props.updates) || isNil(props.match.params[1])) {
+        const field = getUpdateFieldName(props.match.params[1]);
 
-    if (failed)
-      return (
-        <div className="main-panel">
-          <NotFound
-            item={match.params.name}
-            title={'there_are_not_object_with_name'}
-            titleDefault={'Sorry! There are no object with name {item} on Waivio'}
-          />
-        </div>
-      );
+        props.getUpdates(props.match.params.name, field, 'createdAt');
+      }
+    });
 
+    return () => {
+      props.clearObjectFromStore();
+      props.setCatalogBreadCrumbs([]);
+      props.setNestedWobject({});
+      props.clearRelatedPhoto();
+      props.setStoreActiveOption({});
+      props.resetBreadCrumb();
+      props.resetGallery();
+    };
+  }, [props.match.params.name, props.locale, props.authenticatedUserName]);
+
+  if (props.failed)
     return (
-      <Wobj
-        route={route}
-        isSocial={route.isSocial}
-        authenticated={authenticated}
-        failed={failed}
-        authenticatedUserName={authenticatedUserName}
-        location={location}
-        wobject={wobject}
-        nestedWobject={nestedWobject}
-        isFetching={isFetching}
-        history={history}
-        isEditMode={isEditMode}
-        toggleViewEditMode={this.toggleViewEditMode}
-        objectName={objectName}
-        appendAlbum={this.appendAlbum}
-        helmetIcon={this.props.helmetIcon}
-        siteName={this.props.siteName}
-        isWaivio={this.props.isWaivio}
-        supportedObjectTypes={this.props.supportedObjectTypes}
-        weightValue={this.props.weightValue}
-      />
+      <div className="main-panel">
+        <NotFound
+          item={props.match.params.name}
+          title={'there_are_not_object_with_name'}
+          titleDefault={'Sorry! There are no object with name {item} on Waivio'}
+        />
+      </div>
     );
-  }
-}
 
-export default WobjectContainer;
+  return (
+    <Wobj
+      route={props.route}
+      isSocial={props.route.isSocial}
+      authenticated={props.authenticated}
+      failed={props.failed}
+      authenticatedUserName={props.authenticatedUserName}
+      location={props.location}
+      wobject={props.wobject}
+      nestedWobject={props.nestedWobject}
+      isFetching={props.isFetching}
+      history={props.history}
+      isEditMode={isEditMode}
+      toggleViewEditMode={toggleViewEditMode}
+      objectName={objectName}
+      appendAlbum={appendAlbum}
+      helmetIcon={props.helmetIcon}
+      siteName={props.siteName}
+      isWaivio={props.isWaivio}
+      supportedObjectTypes={props.supportedObjectTypes}
+      weightValue={props.weightValue}
+    />
+  );
+};
+
+WobjectContainer.propTypes = {
+  route: PropTypes.shape().isRequired,
+  authenticatedUserName: PropTypes.string.isRequired,
+  authenticated: PropTypes.bool.isRequired,
+  match: PropTypes.shape().isRequired,
+  history: PropTypes.shape().isRequired,
+  location: PropTypes.shape({
+    hash: PropTypes.string,
+  }).isRequired,
+  failed: PropTypes.bool,
+  isFetching: PropTypes.bool,
+  getObject: PropTypes.func.isRequired,
+  resetBreadCrumb: PropTypes.func.isRequired,
+  weightValue: PropTypes.number.isRequired,
+  supportedObjectTypes: PropTypes.arrayOf(PropTypes.string),
+  isWaivio: PropTypes.bool.isRequired,
+  resetGallery: PropTypes.func.isRequired,
+  wobject: PropTypes.shape(),
+  nestedWobject: PropTypes.shape(),
+  updates: PropTypes.arrayOf(),
+  clearObjectFromStore: PropTypes.func,
+  setNestedWobject: PropTypes.func,
+  setCatalogBreadCrumbs: PropTypes.func,
+  locale: PropTypes.string,
+  helmetIcon: PropTypes.string.isRequired,
+  siteName: PropTypes.string.isRequired,
+  getAlbums: PropTypes.func,
+  appendObject: PropTypes.func,
+  getUpdates: PropTypes.func,
+  addAlbumToStore: PropTypes.func,
+  clearRelatedPhoto: PropTypes.func,
+  getNearbyObjects: PropTypes.func.isRequired,
+  getWobjectExpertise: PropTypes.func.isRequired,
+  getObjectFollowers: PropTypes.func.isRequired,
+  getRelatedWobjects: PropTypes.func.isRequired,
+  setStoreActiveOption: PropTypes.func.isRequired,
+};
+
+WobjectContainer.fetchData = async ({ store, match }) => {
+  const res = await store.dispatch(login());
+
+  return Promise.all([
+    store.dispatch(getObject(match.params.name, res?.value?.name)),
+    store.dispatch(getObjectFollowersAction({ object: match.params.name, skip: 0, limit: 5 })),
+    store.dispatch(getRate()),
+    store.dispatch(getRewardFund()),
+    store.dispatch(getNearbyObjectsAction(match.params.name)),
+    store.dispatch(
+      getWobjectExpertiseAction(
+        match.params[1] === 'newsFilter' ? { newsFilter: match.params.itemId } : {},
+        match.params.name,
+      ),
+    ),
+  ]);
+};
+
+const mapStateToProps = state => ({
+  updates: getAppendList(state),
+  authenticated: getIsAuthenticated(state),
+  authenticatedUser: getAuthenticatedUser(state),
+  authenticatedUserName: getAuthenticatedUserName(state),
+  loaded: getWobjectIsFatching(state),
+  failed: getWobjectIsFailed(state),
+  locale: getLocale(state),
+  wobject: getObjectState(state),
+  nestedWobject: getWobjectNested(state),
+  isFetching: getObjectFetchingState(state),
+  screenSize: getScreenSize(state),
+  helmetIcon: getHelmetIcon(state),
+  isWaivio: getIsWaivio(state),
+  supportedObjectTypes: get(getConfiguration(state), 'supported_object_types'),
+  weightValue: getWeightValue(state, getObjectState(state).weight),
+  siteName: getSiteName(state),
+});
+
+const mapDispatchToProps = {
+  clearObjectFromStore,
+  setCatalogBreadCrumbs,
+  setNestedWobject,
+  getObject,
+  resetGallery,
+  getAlbums,
+  appendObject,
+  getUpdates,
+  addAlbumToStore,
+  clearRelatedPhoto,
+  getNearbyObjects: getNearbyObjectsAction,
+  getWobjectExpertise: getWobjectExpertiseAction,
+  getObjectFollowers: getObjectFollowersAction,
+  getRelatedWobjects,
+  setStoreActiveOption,
+  resetBreadCrumb,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(WobjectContainer));
