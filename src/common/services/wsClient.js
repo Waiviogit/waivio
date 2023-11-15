456a0227e506e54d59a70461bb06e3264244a32a
katerynapadafa <padafa2000@gmail.com>
2023-11-15 10:09:27 +0200

    sitemap test

--- src/common/services/wsClient.js
+++ src/common/services/wsClient.js
@@ -25,21 +25,25 @@ class SocketClient {
       this.ws = new WebSocket(this.url);
 
       this.ws.on('error', error => {
-        console.error('error socket closed:', error);
+        console.error('testSitemap line 28', error);
         this.ws.close();
         resolve({ error: new Error(HIVE_SOCKET_ERR.ERROR) });
       });
 
       this.ws.on('message', message => {
         try {
+          console.error('testSitemap line 35', message);
           const data = JSON.parse(message.toString());
 
           emitter.emit(data.id, { data, error: data.error });
           // eslint-disable-next-line no-empty
-        } catch (error) {}
+        } catch (error) {
+          console.error('testSitemap line 41', message);
+        }
       });
 
       this.ws.on('open', () => {
+        console.error('testSitemap line 46');
         setTimeout(() => {
           resolve(this.ws);
         }, 100);
@@ -64,11 +68,13 @@ class SocketClient {
       return { error: new Error(HIVE_SOCKET_ERR.TIMEOUT) };
     }
     if (this?.ws?.readyState !== 1) {
+      console.error('testSitemap line 71', this?.ws?.readyState);
       await this.init();
     }
 
     return new Promise(resolve => {
       if (this.ws.readyState !== 1) {
+        console.error('testSitemap line 77', this?.ws?.readyState);
         resolve({ error: new Error(HIVE_SOCKET_ERR.CLOSED) });
       }
 
@@ -78,12 +84,14 @@ class SocketClient {
       message.id = id;
       this.ws.send(JSON.stringify(message));
       emitter.once(id, ({ data, error }) => {
+        console.error('testSitemap line 87', data, error);
         if (error) resolve({ error });
         resolve(data);
       });
 
       setTimeout(() => {
         if (emitter.eventNames().includes(id)) {
+          console.error('testSitemap line 97');
           this.timeoutCount++;
           emitter.off(id, () => {});
           resolve({ error: new Error(HIVE_SOCKET_ERR.TIMEOUT) });
