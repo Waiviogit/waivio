531379761991e3368c30bad48d6a8902db65eecc
katerynapadafa <padafa2000@gmail.com>
2023-11-17 10:19:59 +0200

    added video plugin for webpage with controls

---
+++ src/client/object/ObjectOfTypeWebpage/videoPlugin.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import video from '@react-page/plugins-video';
+import ReactPlayer from 'react-player';
+
+const customVideoPlugin = {
+  ...video,
+  Renderer: props => (
+    <div
+      style={{
+        alignContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        height: '0px',
+        paddingBottom: '65.25%',
+      }}
+    >
+      {props.readOnly ? null : (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 10,
+          }}
+        />
+      )}
+      <ReactPlayer
+        url={props.data ? props.data.src : undefined}
+        height="100%"
+        width="100%"
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+        blur={props.data ? props.data.blur : undefined}
+        controls
+        muted={props.data ? props.data.muted : undefined}
+        playing={false}
+        loop={props.data ? props.data.loop : undefined}
+      />
+    </div>
+  ),
+};
+
+customVideoPlugin.Renderer.propTypes = {
+  data: PropTypes.shape({
+    src: PropTypes.string,
+    blur: PropTypes.bool,
+    muted: PropTypes.bool,
+    loop: PropTypes.bool,
+  }),
+  readOnly: PropTypes.bool,
+};
+
+export default customVideoPlugin;
