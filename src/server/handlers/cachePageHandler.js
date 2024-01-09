6264642b35f327e9dcfcc0b20534b6e537745097
fesmofet <borees2@gmail.com>
2024-01-09 10:56:44 +0200

    cache_from_cdn

--- src/server/handlers/cachePageHandler.js
+++ src/server/handlers/cachePageHandler.js
@@ -1,6 +1,6 @@
 import { USER_AGENT } from '../../common/constants/ssrData';
-
 import { webPage, botStatistics, botAgent, sitemap } from '../seo-service/seoServiceApi';
+import axios from 'axios';
 
 export const isSearchBot = async req => {
   const userAgent = req.get(USER_AGENT);
@@ -10,7 +10,17 @@ export const isSearchBot = async req => {
 const getUrl = req => `${req.hostname}${req.url}`;
 
 export const getCachedPage = async req => {
-  return webPage.getPageByUrl({ url: getUrl(req) });
+  try {
+    const bucketPath = process.env.CACHE_PAGE_BUCKET;
+    if (!bucketPath) return '';
+
+    const urlToFetch = `${bucketPath}${getUrl(req)}`;
+    const response = await axios.get(urlToFetch);
+
+    return response?.data ?? '';
+  } catch (error) {
+    return '';
+  }
 };
 
 export const setCachedPage = async ({ page, req }) => {
