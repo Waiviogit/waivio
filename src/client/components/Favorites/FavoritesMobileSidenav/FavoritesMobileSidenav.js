2400041aff604dd7794f36f123a8a3b7c565ec54
katerynapadafa <padafa2000@gmail.com>
2024-01-10 16:20:49 +0200

    favorites mobile fixes

--- src/client/components/Favorites/FavoritesMobileSidenav/FavoritesMobileSidenav.js
+++ src/client/components/Favorites/FavoritesMobileSidenav/FavoritesMobileSidenav.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router';
-import { Modal } from 'antd';
+import { Icon, Modal } from 'antd';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { injectIntl } from 'react-intl';
 import './FavoritesMobileSidenav.less';
 
 const FavoritesMobileSidenav = ({ objectTypes, setVisible, visible, intl }) => {
-  const { name, objectType } = useParams();
+  const { name, objectType = objectTypes?.[0] } = useParams();
 
   const modalBody = () => (
-    <div>
+    <div className={'FavoritesMobileSidenav'}>
       <b className="FavoritesMobileSidenav__maindepName">Objects</b>
       {objectTypes?.map(type => (
         <div
           key={type}
-          className="FavoritesMobileSidenav__depName ttc"
+          className={`FavoritesMobileSidenav__depName${objectType === type ? '--open' : ''} ttc`}
           onClick={() => setVisible(false)}
         >
           <NavLink
             to={`/@${name}/favorites/${type}`}
             isActive={() => objectType === type}
             className="FavoritesMobileSidenav__depName"
-            activeClassName="FavoritesMobileSidenav__depName--open"
+            activeClassName="FavoritesMobileSidenav__depName--active"
           >
             {intl.formatMessage({
               id: `object_type_${type}`,
@@ -38,6 +38,11 @@ const FavoritesMobileSidenav = ({ objectTypes, setVisible, visible, intl }) => {
     <React.Fragment>
       <div className="FavoritesMobileSidenav__mobileCrumbs" onClick={() => setVisible(true)}>
         <b>Objects</b> (<span className="FavoritesMobileSidenav__select">Select</span>)
+        {objectType && (
+          <span>
+            <Icon type="right" /> <span className={'ttc'}>{objectType}</span>
+          </span>
+        )}
       </div>
       <Modal onCancel={() => setVisible(false)} visible={visible} footer={null}>
         {modalBody()}
