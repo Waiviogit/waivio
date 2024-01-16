686e6b42738f58df479d426ac3aa7b5938ea0044
katerynapadafa <padafa2000@gmail.com>
2024-01-16 09:34:54 +0200

    added edit button for lists

--- src/client/social-gifts/Breadcrumbs/Breadcrumbs.js
+++ src/client/social-gifts/Breadcrumbs/Breadcrumbs.js
@@ -1,27 +1,47 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation, useRouteMatch } from 'react-router';
+import { useHistory, useLocation, useRouteMatch } from 'react-router';
 import { isEmpty, isNil, takeRight } from 'lodash';
 import { Link } from 'react-router-dom';
-import { Icon } from 'antd';
+import { injectIntl } from 'react-intl';
+import { Button, Icon } from 'antd';
 import PropTypes from 'prop-types';
-import { createNewHash, getObjectName } from '../../../common/helpers/wObjectHelper';
-import { getShopBreadCrumbs } from '../../../store/wObjectStore/wObjectSelectors';
-import { getUsedLocale } from '../../../store/appStore/appSelectors';
+import {
+  accessTypesArr,
+  createNewHash,
+  getObjectName,
+  haveAccess,
+} from '../../../common/helpers/wObjectHelper';
+import { getObject, getShopBreadCrumbs } from '../../../store/wObjectStore/wObjectSelectors';
+import { getUsedLocale, getUserAdministrator } from '../../../store/appStore/appSelectors';
 import { getObjectInfo } from '../../../waivioApi/ApiClient';
-import { setAllBreadcrumbsForChecklist } from '../../../store/wObjectStore/wobjActions';
+import {
+  setAllBreadcrumbsForChecklist,
+  setEditMode,
+} from '../../../store/wObjectStore/wobjActions';
 import useQuery from '../../../hooks/useQuery';
+import {
+  getAuthenticatedUserName,
+  getIsAuthenticated,
+} from '../../../store/authStore/authSelectors';
 
 import './Breadcrumbs.less';
 
-const Breadcrumbs = ({ inProduct }) => {
+const Breadcrumbs = ({ inProduct, intl }) => {
   const breadcrumbs = useSelector(getShopBreadCrumbs);
   const locale = useSelector(getUsedLocale);
+  const authenticated = useSelector(getIsAuthenticated);
+  const wobject = useSelector(getObject);
+  const username = useSelector(getAuthenticatedUserName);
+  const isAdministrator = getUserAdministrator;
   const match = useRouteMatch();
+  const history = useHistory();
   const location = useLocation();
   const dispatch = useDispatch();
   const query = useQuery();
+  const accessExtend = haveAccess(wobject, username, accessTypesArr[0]);
   const permlinks = location.hash.replace('#', '').split('/');
+  const listObjType = wobject?.object_type === 'list';
   let linkList = location.hash ? [match.params.name, ...permlinks] : [match.params.name];
 
   if (inProduct) {
@@ -39,6 +59,13 @@ const Breadcrumbs = ({ inProduct }) => {
           .split(' ')
           .slice(0, 3)
           .join(' ')}...`;
+
+  const editListClick = () => {
+    const lastItemPermlink = breadcrumbs[breadcrumbs?.length - 1]?.author_permlink;
+
+    dispatch(setEditMode(true));
+    history.push(`/object/${lastItemPermlink}`);
+  };
 
   useEffect(() => {
     if (linkList && !(isNil(linkList[0]) && linkList.length === 1))
@@ -73,52 +100,66 @@ const Breadcrumbs = ({ inProduct }) => {
   if (!linkList) return null;
 
   return (
-    <div className="Breadcrumbs">
-      {breadcrumbs?.map((crumb, index) => (
-        <React.Fragment key={crumb.author_permlink}>
-          {inProduct && match.params.name === crumb.author_permlink ? (
-            <span> {getTruncatedTitle(getObjectName(crumb))}</span>
-          ) : (
-            <Link
-              to={{
-                hash:
-                  match.params.name === crumb.author_permlink ||
-                  (inProduct && breadcrumbs[0].author_permlink === crumb.author_permlink)
-                    ? ''
-                    : createNewHash(
-                        crumb.author_permlink,
-                        inProduct
-                          ? takeRight(linkList, linkList.length - 1).join('/')
-                          : location.hash,
-                      ),
-                pathname: inProduct
-                  ? `/checklist/${breadcrumbs[0].author_permlink}`
-                  : location.pathname,
-                ...(inProduct
-                  ? {}
-                  : {
-                      search:
-                        match.params.name === crumb.author_permlink
-                          ? ''
-                          : `currObj=${crumb.author_permlink}`,
-                    }),
-              }}
-            >
-              {getTruncatedTitle(getObjectName(crumb))}
-            </Link>
-          )}
-          {breadcrumbs.length > 1 && index !== breadcrumbs.length - 1 ? <Icon type="right" /> : ''}
-        </React.Fragment>
-      ))}
+    <div className={'flex '}>
+      <div className="Breadcrumbs">
+        {breadcrumbs?.map((crumb, index) => (
+          <React.Fragment key={crumb.author_permlink}>
+            {inProduct && match.params.name === crumb.author_permlink ? (
+              <span> {getTruncatedTitle(getObjectName(crumb))}</span>
+            ) : (
+              <Link
+                to={{
+                  hash:
+                    match.params.name === crumb.author_permlink ||
+                    (inProduct && breadcrumbs[0].author_permlink === crumb.author_permlink)
+                      ? ''
+                      : createNewHash(
+                          crumb.author_permlink,
+                          inProduct
+                            ? takeRight(linkList, linkList.length - 1).join('/')
+                            : location.hash,
+                        ),
+                  pathname: inProduct
+                    ? `/checklist/${breadcrumbs[0].author_permlink}`
+                    : location.pathname,
+                  ...(inProduct
+                    ? {}
+                    : {
+                        search:
+                          match.params.name === crumb.author_permlink
+                            ? ''
+                            : `currObj=${crumb.author_permlink}`,
+                      }),
+                }}
+              >
+                {getTruncatedTitle(getObjectName(crumb))}
+              </Link>
+            )}
+            {breadcrumbs.length > 1 && index !== breadcrumbs.length - 1 ? (
+              <Icon type="right" />
+            ) : (
+              ''
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+      {accessExtend && authenticated && isAdministrator && listObjType && (
+        <div className="Breadcrumbs__edit-container">
+          <Button onClick={editListClick}>
+            {intl.formatMessage({ id: 'edit', defaultMessage: 'Edit' })}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
 
 Breadcrumbs.propTypes = {
   inProduct: PropTypes.bool,
+  intl: PropTypes.shape(),
 };
 Breadcrumbs.defaultProps = {
   inProduct: false,
 };
 
-export default Breadcrumbs;
+export default injectIntl(Breadcrumbs);
