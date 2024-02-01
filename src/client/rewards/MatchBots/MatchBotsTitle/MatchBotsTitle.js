2477805e30e7f6b1aa65234cf1e2d3b614116105
lucykolosova <avavom61>
2024-02-01 12:35:47 +0200

    fix issues

--- src/client/rewards/MatchBots/MatchBotsTitle/MatchBotsTitle.js
+++ src/client/rewards/MatchBots/MatchBotsTitle/MatchBotsTitle.js
@@ -1,13 +1,23 @@
+// import Cookie from 'js-cookie';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Tooltip } from 'antd';
+// import { useDispatch } from 'react-redux';
 
 import { redirectAuthHiveSigner } from '../../../../common/helpers/matchBotsHelpers';
+// import { toggleBots } from '../../../../store/authStore/authActions';
 
 import './MatchBotsTitle.less';
 
 const MatchBotsTitle = ({ isAuthBot, botTitle, turnOffTitle, turnOnTitle, botType }) => {
-  const handleRedirect = () => redirectAuthHiveSigner(isAuthBot, botType);
+  // const dispatch = useDispatch();
+  const handleRedirect = () => {
+    // Cookie.get('auth')
+    //   ? dispatch(toggleBots(botType, isAuthBot))
+    //   : redirectAuthHiveSigner(isAuthBot, botType);
+
+    redirectAuthHiveSigner(isAuthBot, botType);
+  };
 
   return (
     <div className="MatchBots__title-wrap">
