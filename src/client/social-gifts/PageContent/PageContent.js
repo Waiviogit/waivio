f5cd041d44973937d3e4b2113442399a5b6de7d1
katerynapadafa <padafa2000@gmail.com>
2024-02-09 10:03:30 +0200

    added border for social page pictures

--- src/client/social-gifts/PageContent/PageContent.js
+++ src/client/social-gifts/PageContent/PageContent.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import BodyContainer from '../../containers/Story/BodyContainer';
 import { getObject } from '../../../waivioApi/ApiClient';
 
 import './PageContent.less';
 import { getLastPermlinksFromHash, getObjectName } from '../../../common/helpers/wObjectHelper';
+import { getIsSocial } from '../../../store/appStore/appSelectors';
 
 const PageContent = ({ wobj }) => {
   const [content, setContent] = useState(wobj.pageContent || '');
   const { name } = useParams();
   const location = useLocation();
   const objName = location.hash ? getLastPermlinksFromHash(location.hash) : name;
+  const isSocial = useSelector(getIsSocial);
 
   useEffect(() => {
     if (wobj) {
@@ -28,7 +31,7 @@ const PageContent = ({ wobj }) => {
   }, [objName]);
 
   return (
-    <div className={'PageContent'}>
+    <div className={`PageContent ${isSocial ? 'social' : ''}`}>
       <BodyContainer isPage full body={content} />
     </div>
   );
