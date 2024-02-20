5b73b93c917051a7c7e0d934c8dc5301dec6bc4b
katerynapadafa <padafa2000@gmail.com>
2024-02-20 11:38:55 +0200

    webpage preview fix

--- src/client/object/ObjectOfTypeWebpage/ObjectOfTypeWebpage.js
+++ src/client/object/ObjectOfTypeWebpage/ObjectOfTypeWebpage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'antd';
 import { injectIntl } from 'react-intl';
+import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import { useHistory, useParams } from 'react-router';
 import { has, isNil } from 'lodash';
@@ -15,16 +16,27 @@ import { getObject } from '../../../waivioApi/ApiClient';
 import { colorPickerPlugin } from './colorPickerPlugin';
 import { getIsEditMode } from '../../../store/wObjectStore/wObjectSelectors';
 import { objectFields } from '../../../common/constants/listOfFields';
-import { getLastPermlinksFromHash, getObjectName } from '../../../common/helpers/wObjectHelper';
+import {
+  getLastPermlinksFromHash,
+  getObjectAvatar,
+  getObjectName,
+} from '../../../common/helpers/wObjectHelper';
 import { setNestedWobject } from '../../../store/wObjectStore/wobjActions';
 import AppendWebpageModal from './AppendWebpageModal';
 import { getAuthenticatedUserName } from '../../../store/authStore/authSelectors';
-import { getIsSocial, getUsedLocale } from '../../../store/appStore/appSelectors';
+import {
+  getHelmetIcon,
+  getIsSocial,
+  getSiteName,
+  getUsedLocale,
+} from '../../../store/appStore/appSelectors';
 import customVideoPlugin from './videoPlugin';
 
 import './ObjectOfTypeWebpage.less';
 import CatalogBreadcrumb from '../Catalog/CatalogBreadcrumb/CatalogBreadcrumb';
 import Loading from '../../components/Icon/Loading';
+import { useSeoInfoWithAppUrl } from '../../../hooks/useSeoInfo';
+import DEFAULTS from '../const/defaultValues';
 
 const customSlate = slate(config => ({
   ...config,
@@ -55,6 +67,12 @@ const ObjectOfTypeWebpage = ({ intl }) => {
   const jsonVal = currentValue ? JSON.stringify(currentValue) : null;
   const siteLink = location && `${location?.origin}/`;
   const isSocial = useSelector(getIsSocial);
+  const title = `${wobject.name}`;
+  const description = `${wobject.description || ''} ${wobject.name}}`;
+  const { canonicalUrl } = useSeoInfoWithAppUrl(wobject.canonical);
+  const siteImage = getObjectAvatar(wobject) || DEFAULTS.AVATAR;
+  const siteName = useSelector(getSiteName);
+  const helmetIcon = useSelector(getHelmetIcon);
 
   useEffect(() => {
     setLoading(true);
@@ -88,42 +106,67 @@ const ObjectOfTypeWebpage = ({ intl }) => {
   }
 
   return (
-    <div className="ObjectOfTypeWebpage">
-      {!isEditMode && history?.location.hash && <CatalogBreadcrumb wobject={wobject} intl={intl} />}
-      {loading ? (
-        <Loading />
-      ) : (
-        <Editor
-          readOnly={!isEditMode || showModal}
-          cellPlugins={plugins}
-          value={currentValue}
-          onChange={newValue => setCurrentValue(newValue)}
-        />
-      )}
-      {isEditMode && (
-        <div className="object-of-type-page__row align-center">
-          <Button
-            htmlType="button"
-            // disabled={isNil(currentValue)}
-            onClick={() => setShowModal(true)}
-            size="large"
-            className={'ready-to-publish-btn'}
-          >
-            {intl.formatMessage({ id: 'ready_to_publish', defaultMessage: 'Ready to publish' })}
-          </Button>
-        </div>
-      )}
-      {showModal && (
-        <AppendWebpageModal
-          objName={getObjectName(wobject)}
-          wObject={wobject}
-          showModal={showModal}
-          hideModal={() => setShowModal(false)}
-          webpageBody={jsonVal}
-          field={objectFields.webpage}
-        />
-      )}
-    </div>
+    <>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="article" />
+        <link rel="canonical" href={canonicalUrl} />
+        <meta property="og:url" content={canonicalUrl} />
+        <meta property="og:image" content={siteImage} />
+        <meta property="og:image:url" content={siteImage} />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:card" content={siteImage ? 'summary_large_image' : 'summary'} />
+        <meta name="twitter:site" content={`@${siteName}`} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" property="twitter:image" content={siteImage} />
+        <meta property="og:site_name" content={siteName} />
+        <link rel="image_src" href={siteImage} />
+        <link id="favicon" rel="icon" href={helmetIcon} type="image/x-icon" />
+      </Helmet>
+      <div className="ObjectOfTypeWebpage">
+        {!isEditMode && history?.location.hash && (
+          <CatalogBreadcrumb wobject={wobject} intl={intl} />
+        )}
+        {loading ? (
+          <Loading />
+        ) : (
+          <Editor
+            readOnly={!isEditMode || showModal}
+            cellPlugins={plugins}
+            value={currentValue}
+            onChange={newValue => setCurrentValue(newValue)}
+          />
+        )}
+        {isEditMode && (
+          <div className="object-of-type-page__row align-center">
+            <Button
+              htmlType="button"
+              // disabled={isNil(currentValue)}
+              onClick={() => setShowModal(true)}
+              size="large"
+              className={'ready-to-publish-btn'}
+            >
+              {intl.formatMessage({ id: 'ready_to_publish', defaultMessage: 'Ready to publish' })}
+            </Button>
+          </div>
+        )}
+        {showModal && (
+          <AppendWebpageModal
+            objName={getObjectName(wobject)}
+            wObject={wobject}
+            showModal={showModal}
+            hideModal={() => setShowModal(false)}
+            webpageBody={jsonVal}
+            field={objectFields.webpage}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
