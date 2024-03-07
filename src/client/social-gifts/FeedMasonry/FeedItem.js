3e91c5eb27ff428d04cc7782dc31206cc704b023
lucykolosova <avavom61>
2024-03-07 15:44:31 +0200

    fix 3speak preview

--- src/client/social-gifts/FeedMasonry/FeedItem.js
+++ src/client/social-gifts/FeedMasonry/FeedItem.js
@@ -83,8 +83,8 @@ const FeedItem = ({ post, photoQuantity, preview }) => {
 
   if (is3speak) {
     const jsonMetadata = parseJSON(post.json_metadata);
-    console.log(imagePath);
-    embed = { ...embeds[0], thumbnail: jsonMetadata?.video?.info?.sourceMap?.[1]?.url };
+
+    embed = { ...embeds[0], thumbnail: imagePath[0] || jsonMetadata?.video?.info?.sourceMap?.[1]?.url };
   }
 
   return (
