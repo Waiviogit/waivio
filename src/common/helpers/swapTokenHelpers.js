7ca6af8788d41093bd3791d02653622e251e0739
katerynapadafa <padafa2000@gmail.com>
2024-03-07 16:47:56 +0200

    tken amount for swap fix

--- src/common/helpers/swapTokenHelpers.js
+++ src/common/helpers/swapTokenHelpers.js
@@ -122,7 +122,9 @@ export const getSwapOutput = ({ symbol, amountIn, pool, slippage, from, params, 
     liquidityOut = pool.baseQuantity;
   }
 
-  const tokenAmount = from ? BigNumber(amountIn).toFixed() : amountOut;
+  const tokenAmount = from
+    ? BigNumber(amountIn).toFixed(precision, BigNumber.ROUND_DOWN)
+    : amountOut;
 
   const slippageAmount = from ? amountOut.times(slippage) : BigNumber(amountIn).times(slippage);
 
@@ -166,7 +168,7 @@ export const getSwapOutput = ({ symbol, amountIn, pool, slippage, from, params, 
     minAmountOut: minAmountOutToFixed,
     tokenPair,
     tokenSymbol,
-    tokenAmount: tokenAmount.toFixed(precision, BigNumber.ROUND_DOWN),
+    tokenAmount,
   });
 
   return {
