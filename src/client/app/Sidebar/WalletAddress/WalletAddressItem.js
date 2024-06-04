f4889a89ec0fe8a9adbe78f65c589fd84af16b0a
katerynapadafa <padafa2000@gmail.com>
2024-06-04 14:14:33 +0300

    added hive qr code

--- src/client/app/Sidebar/WalletAddress/WalletAddressItem.js
+++ src/client/app/Sidebar/WalletAddress/WalletAddressItem.js
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
-import { Button, Modal } from 'antd';
+import { Button, Input, Modal } from 'antd';
+import { isNil, isEmpty } from 'lodash';
+import { Link } from 'react-router-dom';
 import QRCode from 'qrcode.react';
+import { encodeOp } from 'hive-uri';
+import { useSelector } from 'react-redux';
 import { cryptocurrenciesList } from '../../../../common/constants/listOfFields';
 import { parseWobjectField } from '../../../../common/helpers/wObjectHelper';
 import CopyButton from '../../../widgets/CopyButton/CopyButton';
+import { getAuthenticatedUserName } from '../../../../store/authStore/authSelectors';
 
 const WalletAddressItem = ({ address }) => {
   const addressBody = parseWobjectField(address, 'body');
+  const [qrCodeLink, setQRCodeLink] = useState(null);
+  const [amount, setAmount] = useState(null);
+  const username = addressBody.address;
   const [openModal, setOpenModal] = useState(false);
-  const history = useHistory();
+  const authUserName = useSelector(getAuthenticatedUserName);
+  const disabledGenerate = isNil(amount) || isEmpty(amount) || amount === '0';
+
   const onWalletAddressClick = () => {
-    if (addressBody.symbol === 'HIVE') {
-      history.push(`/@${addressBody.address}`);
-    } else {
-      setOpenModal(true);
-    }
+    setOpenModal(true);
   };
 
   const cryptocurrency = cryptocurrenciesList.find(crypto => crypto.name === addressBody.symbol);
+
+  const closeModal = () => {
+    setOpenModal(false);
+    setQRCodeLink(null);
+    setAmount(null);
+  };
+
+  const generateQRCodeData = () => {
+    const url = encodeOp([
+      'transfer',
+      { from: authUserName, to: username, amount: `${amount} HIVE`, memo: '' },
+    ]);
+
+    setQRCodeLink(url);
+  };
 
   return (
     <div className={'WalletAddressItem__container'}>
@@ -36,22 +56,48 @@ const WalletAddressItem = ({ address }) => {
       </span>
       <Modal
         title={addressBody.symbol}
-        onCancel={() => setOpenModal(false)}
+        onCancel={closeModal}
         visible={openModal}
         footer={[
-          <Button key="ok" type="primary" onClick={() => setOpenModal(false)}>
+          <Button key="ok" type="primary" onClick={closeModal}>
             <FormattedMessage id="ok" defaultMessage="Ok" />
           </Button>,
         ]}
       >
-        <div className="WalletAddressItem__title">Address:</div>
-
-        <div className={'Deposit__section'}>
-          <CopyButton className="WalletAddressItem__input" text={addressBody.address} />
-          <div className="WalletAddressItem__qr-code-container">
-            <QRCode className="Deposit__qr-code" value={addressBody.address} />
+        {addressBody.symbol === 'HIVE' ? (
+          <div>
+            <div className="WalletAddressItem__title">
+              Select the amount of HIVE you want to send to{' '}
+              <Link to={`/@${username}`}>{username}</Link>
+            </div>
+            <Input
+              value={amount}
+              placeholder={'Enter amount'}
+              type={'number'}
+              onInput={e => setAmount(e.target.value)}
+            />
+            <div className="WalletAddressItem__generate-container">
+              <Button type={'primary'} onClick={generateQRCodeData} disabled={disabledGenerate}>
+                Generate QR code
+              </Button>
+            </div>
+            {!isNil(amount) && !isNil(qrCodeLink) && (
+              <div className="WalletAddressItem__qr-code-container">
+                <QRCode className="Deposit__qr-code" value={qrCodeLink} />
+              </div>
+            )}
           </div>
-        </div>
+        ) : (
+          <div>
+            <div className="WalletAddressItem__title">Address:</div>
+            <div className={'Deposit__section'}>
+              <CopyButton className="WalletAddressItem__input" text={addressBody.address} />
+              <div className="WalletAddressItem__qr-code-container">
+                <QRCode className="Deposit__qr-code" value={addressBody.address} />
+              </div>
+            </div>
+          </div>
+        )}
       </Modal>
     </div>
   );
