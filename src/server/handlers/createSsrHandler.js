eee2859565bc0c67133e129ecfb9f9317dfb3b0f
Lucy <lucy@MacBook-Pro-Lucy.local>
2024-06-07 11:38:09 +0300

    add page path

--- src/server/handlers/createSsrHandler.js
+++ src/server/handlers/createSsrHandler.js
@@ -62,7 +62,6 @@ export default function createSsrHandler(template) {
         await updateBotCount(req);
         const cachedPage = await getCachedPage(req);
         if (cachedPage) {
-          console.log('SEND CACHED PAGE');
           return res.send(cachedPage);
         }
       }
@@ -160,7 +159,10 @@ export default function createSsrHandler(template) {
 
       return res.send(page);
     } catch (err) {
-      console.error('SSR error occured, falling back to bundled application instead', err);
+      console.error(
+        `SSR error occured, falling back to bundled application instead ${req.url}`,
+        err,
+      );
       let settings = {};
       let adsenseSettings = {};
       const isWaivio = req.hostname.includes('waivio');
