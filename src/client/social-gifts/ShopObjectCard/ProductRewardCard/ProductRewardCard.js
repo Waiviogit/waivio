2f669fd958bba203eddc530f5ca308d8ed51c124
katerynapadafa <padafa2000@gmail.com>
2024-06-14 13:10:09 +0300

    added main reward cars for sites

--- src/client/social-gifts/ShopObjectCard/ProductRewardCard/ProductRewardCard.js
+++ src/client/social-gifts/ShopObjectCard/ProductRewardCard/ProductRewardCard.js
@@ -3,9 +3,18 @@ import { isEmpty } from 'lodash';
 import PropTypes from 'prop-types';
 import { getPropositionsKey } from '../../../../common/helpers/newRewardsHelper';
 import Proposition from '../../../newRewards/reuseble/Proposition/Proposition';
+import SocialCampaignCard from './SocialCampaignCard';
 
 const ProductRewardCard = ({ reward, isSocialProduct }) => (
   <div className="ProductRewardCard">
+    {!isEmpty(reward?.main) && (
+      <SocialCampaignCard
+        isSocialProduct={isSocialProduct}
+        sponsor={reward?.main?.guideName}
+        isCampaign
+        proposition={reward?.main}
+      />
+    )}
     {!isEmpty(reward?.secondary) &&
       reward?.secondary?.map((proposition, i) => (
         <Proposition
