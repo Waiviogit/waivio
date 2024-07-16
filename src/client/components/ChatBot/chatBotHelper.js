ec2d5067ab36db7c223e11dabeeb2f8691a53871
katerynapadafa <padafa2000@gmail.com>
2024-07-16 17:57:10 +0300

    test host fix, chatbot fix

--- src/client/components/ChatBot/chatBotHelper.js
+++ src/client/components/ChatBot/chatBotHelper.js
@@ -1,6 +1,7 @@
 export const quickMessages = [
   { text: 'Proofread', label: 'Proofread' },
   { text: 'Translate to English', label: 'Translate' },
+  { text: 'Question about Waivio', label: 'About Waivio' },
 ];
 
 export default null;
