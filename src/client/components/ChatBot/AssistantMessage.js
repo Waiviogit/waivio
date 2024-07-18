92b217bcfa7cd508e2145cf7d2b6877a3cf83466
katerynapadafa <padafa2000@gmail.com>
2024-07-18 15:13:09 +0300

    fixed ai links

--- src/client/components/ChatBot/AssistantMessage.js
+++ src/client/components/ChatBot/AssistantMessage.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import TypingText from './TypingText';
 import { getHtml } from '../Story/Body';
+import { getAppUrl } from '../../../store/appStore/appSelectors';
 
-const AssistantMessage = ({ text, loading }) => (
-  <div className="flex">
-    {' '}
-    <img className="chat-logo-small" src="/images/icons/cryptocurrencies/waiv.png" alt={'Waivio'} />
-    {!loading && <div className="message from-assistant">{getHtml(text)}</div>}
-    {loading && <TypingText />}
-  </div>
-);
+const AssistantMessage = ({ text, loading }) => {
+  const appUrl = useSelector(getAppUrl);
+
+  return (
+    <div className="flex">
+      {' '}
+      <img
+        className="chat-logo-small"
+        src="/images/icons/cryptocurrencies/waiv.png"
+        alt={'Waivio'}
+      />
+      {!loading && (
+        <div className="message from-assistant">{getHtml(text, {}, 'Object', { appUrl })}</div>
+      )}
+      {loading && <TypingText />}
+    </div>
+  );
+};
 
 AssistantMessage.propTypes = {
   text: PropTypes.string,
