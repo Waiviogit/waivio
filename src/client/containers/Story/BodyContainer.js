f2a59f7d2ca7aeac5ac9f9208ff2bb5a28ec7325
katerynapadafa <padafa2000@gmail.com>
2024-08-05 21:18:00 +0300

    page fix

--- src/client/containers/Story/BodyContainer.js
+++ src/client/containers/Story/BodyContainer.js
@@ -1,13 +1,13 @@
 import { connect } from 'react-redux';
 import Body from '../../components/Story/Body';
-import { getAppUrl } from '../../../store/appStore/appSelectors';
+import { getAppHost } from '../../../store/appStore/appSelectors';
 import {
   getExitPageSetting,
   getRewriteLinks,
 } from '../../../store/settingsStore/settingsSelectors';
 
 export default connect(state => ({
-  appUrl: getAppUrl(state),
+  appUrl: getAppHost(state),
   rewriteLinks: getRewriteLinks(state),
   exitPageSetting: getExitPageSetting(state),
 }))(Body);
