a4ffce4e84041f5e89e502f41d4a3ae4c2df64e3
katerynapadafa <padafa2000@gmail.com>
2024-09-11 15:58:36 +0300

    group fields fixes

--- src/client/object/AppendModal/FormComponents/GroupForms/AddUserForm.js
+++ src/client/object/AppendModal/FormComponents/GroupForms/AddUserForm.js
@@ -2,51 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import { isEmpty } from 'lodash';
-import SelectUserForAutocomplete from '../../../../widgets/SelectUserForAutocomplete';
 import SearchUsersAutocomplete from '../../../../components/EditorUser/SearchUsersAutocomplete';
+import './GroupForms.less';
+import Avatar from '../../../../components/Avatar';
 
-const AddUserForm = props => (
-  <div className="NewsFiltersRule__line">
-    <div className="AppendForm__appendTitles">
-      {props.intl.formatMessage({
-        id: 'add_a_user',
-        defaultMessage: 'Add a user',
-      })}
-    </div>
-    {!isEmpty(props.selectedUsers) && (
-      <div className="NewsFiltersRule__card-wrap">
-        {props.selectedUsers.map(userName => (
-          <>
-            <div>
-              <SelectUserForAutocomplete
-                isNewsFeed
-                account={userName}
-                resetUser={() => props.deleteUser(userName)}
-              />
-            </div>
-          </>
-        ))}
+const AddUserForm = props => {
+  const account = props.selectedUsers[0];
+
+  return (
+    <div className="NewsFiltersRule__line">
+      <div className="AppendForm__appendTitles NewsFiltersRule-title">
+        {props.intl.formatMessage({
+          id: `${props.isAdd ? 'add_a_user' : 'exclude_a_user'}`,
+          defaultMessage: `${props.isAdd ? 'Add a user' : 'Exclude a user'}`,
+        })}
       </div>
-    )}
-    <div className="NewsFiltersRule__line-search">
-      {props.selectedUsers.length < 1 && (
-        <SearchUsersAutocomplete
-          placeholder={props.intl.formatMessage({
-            id: 'find_a_user',
-            defaultMessage: 'Find a user',
-          })}
-          handleSelect={props.handleSelectUsersBlog}
-        />
+      {!isEmpty(props.selectedUsers) && (
+        <div className="SelectUserForAutocomplete">
+          <div className="SelectUserForAutocomplete__user-info">
+            <Avatar username={account} size={40} />
+            <span>{account}</span>
+          </div>
+          <span
+            role="presentation"
+            onClick={() => props.deleteUser(account)}
+            className="iconfont icon-delete"
+          />
+        </div>
       )}
+      <div className="NewsFiltersRule__line-search mt1">
+        {props.selectedUsers.length < 1 && (
+          <SearchUsersAutocomplete
+            placeholder={props.intl.formatMessage({
+              id: 'find_a_user',
+              defaultMessage: 'Find a user',
+            })}
+            handleSelect={props.handleSelectUsersBlog}
+          />
+        )}
+      </div>
+      <p>
+        {props.isAdd
+          ? 'Add a specific user to the group.'
+          : 'Exclude a specific user from the group.'}
+      </p>
     </div>
-    <p>Choose users whose followers should be in the group.</p>
-  </div>
-);
+  );
+};
 
 AddUserForm.propTypes = {
   intl: PropTypes.shape().isRequired,
   selectedUsers: PropTypes.arrayOf().isRequired,
   handleSelectUsersBlog: PropTypes.func.isRequired,
   deleteUser: PropTypes.func.isRequired,
+  isAdd: PropTypes.bool.isRequired,
 };
 export default injectIntl(AddUserForm);
