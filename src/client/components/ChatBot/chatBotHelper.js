7d602046cc9aaf04e36245c7b2ae2936ef89039f
katerynapadafa <padafa2000@gmail.com>
2024-09-25 17:11:06 +0300

    ai assistant header fixes

--- src/client/components/ChatBot/chatBotHelper.js
+++ src/client/components/ChatBot/chatBotHelper.js
@@ -1,7 +1,16 @@
-export const quickMessages = siteName => [
-  { text: `Question about ${siteName}`, label: `About ${siteName === 'Waivio' ? siteName : ''}` },
-  { text: 'Proofread the following post', label: 'Proofread' },
-  { text: 'Translate to English', label: 'Translate' },
-];
+import { isEmpty, isNil } from 'lodash';
+
+export const quickMessages = (siteName, currHost, name) => {
+  const host = !isNil(name) && !isEmpty(name) ? `(${currHost})` : '';
+
+  return [
+    {
+      text: `Question about ${siteName === 'Waivio' ? siteName : `${siteName} ${host}`}`,
+      label: `About ${siteName === 'Waivio' ? siteName : ''}`,
+    },
+    { text: 'Proofread the following post', label: 'Proofread' },
+    { text: 'Translate to English', label: 'Translate' },
+  ];
+};
 
 export default null;
