25f2867171e841dff6944e3804f2ad0e161d0e3d
katerynapadafa <padafa2000@gmail.com>
2024-10-10 10:08:03 +0300

    cookies on logout fix

--- src/store/authStore/authActions.js
+++ src/store/authStore/authActions.js
@@ -96,7 +96,7 @@ export const logout = () => (dispatch, getState, { busyAPI, steemConnectAPI }) =
   const hiveAuth = Cookie.get('auth');
   const language = getWebsiteLanguage(state);
 
-  Cookie.remove('shownMaps');
+  // Cookie.remove('shownMaps');
   if (state.auth.isGuestUser) {
     accessToken = getGuestAccessToken();
     clearGuestAuthData();
