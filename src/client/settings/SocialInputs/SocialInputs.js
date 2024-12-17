720becfd76ba8886e60f55046544d45bcf8b42be
katerynapadafa <padafa2000@gmail.com>
2024-12-17 17:39:18 +0200

    edit profile reset fix

--- src/client/settings/SocialInputs/SocialInputs.js
+++ src/client/settings/SocialInputs/SocialInputs.js
@@ -2,11 +2,12 @@ import React from 'react';
 import socialProfiles, { socialWallets } from '../../../common/helpers/socialProfiles';
 import SocialInputItem from './SocialInputItem';
 
-const SocialInputs = ({ getFieldDecorator, getFieldValue, setErrors, intl, errors }) => {
+const SocialInputs = ({ getFieldDecorator, getFieldValue, setErrors, intl, errors, metadata }) => {
   const allProfiles = [...socialProfiles, ...socialWallets];
 
   return allProfiles.map(profile => (
     <SocialInputItem
+      metadata={metadata}
       key={profile.id}
       profile={profile}
       getFieldDecorator={getFieldDecorator}
