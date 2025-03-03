5494a99dd1397ac23f86417c63630fd572429ed5
katerynapadafa <padafa2000@gmail.com>
2025-03-03 13:18:39 +0200

    waiv delegations block fix

--- src/client/wallet/WAIVwallet/WAIVWalletSummaryInfo.js
+++ src/client/wallet/WAIVwallet/WAIVWalletSummaryInfo.js
@@ -77,6 +77,7 @@ const WAIVWalletSummaryInfo = props => {
   const delegationsIn = Number(get(props.currencyInfo, 'delegationsIn', null));
   const delegationsOut = Number(get(props.currencyInfo, 'delegationsOut', null));
   const pendingUndelegations = Number(get(props.currencyInfo, 'pendingUndelegations', null));
+
   const delegation = delegationsIn - delegationsOut;
   const showPowerDown = unstake !== 0 && !isNil(unstake) && !isNaN(unstake);
   const estAccValue =
@@ -200,7 +201,7 @@ const WAIVWalletSummaryInfo = props => {
               </div>
             )}
 
-            {!isNil(delegation) && !isNaN(delegation) && (
+            {!isNil(delegation) && !isNaN(delegation) && pendingUndelegations !== 0 && (
               <div className="WalletSummaryInfo__itemWrap--no-border delegation-block">
                 <div className="WalletSummaryInfo__item">
                   <div className="WalletSummaryInfo__label power-down">WAIV delegations</div>
