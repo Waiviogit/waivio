12987f52dabc97dd442bd7aff6dafe6f6d8eac43
katerynapadafa <padafa2000@gmail.com>
2025-03-19 14:26:22 +0200

    paypal info

--- src/client/websites/WebsiteTools/PayPal/PayPalSubscriptionDetails.js
+++ src/client/websites/WebsiteTools/PayPal/PayPalSubscriptionDetails.js
@@ -4,8 +4,17 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import Loading from '../../../components/Icon/Loading';
 
-const PayPalSubscriptionDetails = ({ info, loading }) =>
-  loading ? (
+const PayPalSubscriptionDetails = ({ info, loading }) => {
+  const add30Days = createDate => {
+    const newDate = new Date(createDate);
+
+    newDate.setUTCDate(newDate.getUTCDate() + 30);
+
+    return newDate.toISOString();
+  };
+  const nextDate = info?.billing_info?.next_billing_time || add30Days(info?.create_time);
+
+  return loading ? (
     <Loading />
   ) : (
     <>
@@ -13,14 +22,17 @@ const PayPalSubscriptionDetails = ({ info, loading }) =>
         <div>
           <div>
             {' '}
+            <b>Status:</b> {info?.status}
+          </div>
+          <div>
+            {' '}
             <b>Amount:</b>{' '}
             <FormattedNumber value={info?.billing_info?.last_payment?.amount?.value} />{' '}
             {info?.billing_info?.last_payment?.amount?.currency_code}
           </div>
           <div>
-            <b>Next payment date:</b>{' '}
-            <FormattedDate value={info?.billing_info?.next_billing_time} />{' '}
-            <FormattedTime value={info?.billing_info?.next_billing_time} />
+            <b>Next payment date:</b> <FormattedDate value={nextDate} />{' '}
+            <FormattedTime value={nextDate} />
           </div>
           <div>
             {' '}
@@ -30,6 +42,7 @@ const PayPalSubscriptionDetails = ({ info, loading }) =>
       )}
     </>
   );
+};
 
 PayPalSubscriptionDetails.propTypes = {
   info: PropTypes.shape(),
