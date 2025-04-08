da0f75da3b068ea0d92450451d1c95bfb165dc51
katerynapadafa <padafa2000@gmail.com>
2025-04-08 18:04:06 +0300

    website actions instacart fix

--- src/client/widgets/InstacartWidget.js
+++ src/client/widgets/InstacartWidget.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { websiteStatisticsAction } from '../../waivioApi/ApiClient';
 
-const InstacartWidget = ({ instacartAff, className, isProduct = false }) => (
-  <div
-    className={className}
-    id={'shop-with-instacart-v1'}
-    data-affiliate_id={instacartAff?.affiliateCode}
-    data-source_origin="affiliate_hub"
-    data-affiliate_platform="recipe_widget"
-    style={
-      isProduct
-        ? {
-            marginBottom: '15px',
-          }
-        : {
-            display: 'flex',
-            justifyContent: 'center',
-            marginBottom: '10px',
-          }
-    }
-  />
-);
+const InstacartWidget = ({ instacartAff, className, isProduct = false }) => {
+  useEffect(() => {
+    const onBlur = () => {
+      websiteStatisticsAction();
+      window && window.gtag('event', 'buy_now', { debug_mode: true });
+    };
+
+    window && window.addEventListener('blur', onBlur);
+
+    return () => window && window.removeEventListener('blur', onBlur);
+  }, []);
+
+  return (
+    <div
+      className={className}
+      id="shop-with-instacart-v1"
+      data-affiliate_id={instacartAff?.affiliateCode}
+      data-source_origin="affiliate_hub"
+      data-affiliate_platform="recipe_widget"
+      style={
+        isProduct
+          ? { marginBottom: '15px' }
+          : { display: 'flex', justifyContent: 'center', marginBottom: '10px' }
+      }
+    />
+  );
+};
 
 InstacartWidget.propTypes = {
-  instacartAff: PropTypes.shape(),
+  instacartAff: PropTypes.shape({
+    affiliateCode: PropTypes.string,
+  }),
   className: PropTypes.string,
   isProduct: PropTypes.bool,
 };
