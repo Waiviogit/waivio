ff66d76da65d1dcffd51396985e44fba9caa6a9c
katerynapadafa <padafa2000@gmail.com>
2025-07-31 17:24:24 +0300

    slider fix

--- src/client/social-gifts/SocialProduct/ObjectsSlider/ObjectsSlider.js
+++ src/client/social-gifts/SocialProduct/ObjectsSlider/ObjectsSlider.js
@@ -2,41 +2,37 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { isEmpty } from 'lodash';
 import { Carousel, Icon } from 'antd';
-import { useSelector } from 'react-redux';
 import ShopObjectCard from '../../ShopObjectCard/ShopObjectCard';
 import { isTabletOrMobile } from '../socialProductHelper';
-import useAdLevelData from '../../../../hooks/useAdsense';
-import { getSettingsAds } from '../../../../store/websiteStore/websiteSelectors';
+// import useAdLevelData from '../../../../hooks/useAdsense';
+// import { getSettingsAds } from '../../../../store/websiteStore/websiteSelectors';
 
 const ObjectsSlider = ({ title, objects, name }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideWidth = 270;
   const slidesToShow = Math.floor(typeof window !== 'undefined' && window.innerWidth / slideWidth);
-  const { frequency = 5 } = useAdLevelData();
+  // const { frequency = 5 } = useAdLevelData();
+  // const unitCode = useSelector(getSettingsAds)?.displayUnitCode || '';
+  // const isAdInjectable = name === 'similar' || name === 'related';
 
-  const unitCode = useSelector(getSettingsAds)?.displayUnitCode || '';
-
-  const isAdInjectable = name === 'similar' || name === 'related';
-
-  const mixedObjects =
-    isAdInjectable && frequency && unitCode
-      ? objects.reduce((acc, obj, idx) => {
-          if (idx > 0 && idx % frequency === 0) {
-            acc.push({ isAd: true, key: `ad-${idx}` });
-          }
-          acc.push(obj);
-
-          return acc;
-        }, [])
-      : objects;
+  // const mixedObjects =
+  //   isAdInjectable && frequency && unitCode
+  //     ? objects.reduce((acc, obj, idx) => {
+  //         if (idx > 0 && idx % frequency === 0) {
+  //           acc.push({ isAd: true, key: `ad-${idx}` });
+  //         }
+  //         acc.push(obj);
+  //
+  //         return acc;
+  //       }, [])
+  //     : objects;
 
   const carouselSettings = {
     dots: false,
     arrows: !isTabletOrMobile,
     lazyLoad: false,
     rows: 1,
-    nextArrow:
-      currentSlide >= mixedObjects.length - slidesToShow ? null : <Icon type="caret-right" />,
+    nextArrow: currentSlide >= objects.length - slidesToShow ? null : <Icon type="caret-right" />,
     prevArrow: currentSlide === 0 ? null : <Icon type="caret-left" />,
     slidesToScroll: !isTabletOrMobile ? slidesToShow : 1,
     swipeToSlide: isTabletOrMobile,
@@ -49,12 +45,12 @@ const ObjectsSlider = ({ title, objects, name }) => {
   };
 
   return (
-    !isEmpty(mixedObjects) && (
+    !isEmpty(objects) && (
       <div className="SocialProduct__addOn-section">
         <div className="SocialProduct__heading">{title}</div>
         <div className={`Slider__wrapper-${name}`}>
           <Carousel {...carouselSettings} beforeChange={onSlideChange}>
-            {mixedObjects.map(
+            {objects.map(
               (item, idx) => (
                 // item?.isAd ? (
                 //   <GoogleAds inShop key={item.key || `ad-${idx}`} />
