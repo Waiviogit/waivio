ad0e412ef90c1975137981c15fd6c55af39a9ab5
katerynapadafa <padafa2000@gmail.com>
2025-08-04 11:11:55 +0300

    test ads

--- src/client/adsenseAds/GoogleAds.js
+++ src/client/adsenseAds/GoogleAds.js
@@ -58,12 +58,19 @@ const GoogleAds = ({
     const hideEmptyAds = () => {
       document.querySelectorAll('.google-ads').forEach(ad => {
         const ins = ad.querySelector('ins');
-        const iframe = ins?.querySelector('iframe');
-        const isInsEmpty = !ins || ins.childNodes.length === 0 || ins.innerHTML.trim() === '';
 
-        if (isInsEmpty || !iframe) {
-          ad.classList.add('hidden-ad');
-        }
+        // const iframe = ins?.querySelector('iframe');
+        // eslint-disable-next-line no-console
+        console.log(ins.childNodes, 'childNodes');
+        // eslint-disable-next-line no-console
+        console.log(ins.innerHTML, 'innerHTML');
+        // eslint-disable-next-line no-console
+        console.log(ins.innerHTML, 'innerHTML');
+        // const isInsEmpty = !ins || ins.childNodes.length === 0 || ins.innerHTML.trim() === '';
+
+        //  if (isInsEmpty || !iframe) {
+        // ad.classList.add('hidden-ad');
+        //  }
       });
 
       // document.querySelectorAll('.slick-slide').forEach(slide => {
