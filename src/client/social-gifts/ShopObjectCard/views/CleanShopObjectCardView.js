d5ca21ce5cedb7d18d7a21738086bc64b66d0215
lucykolosova <ludmila@MacBook-Pro-Ludmila-308.local>
2025-12-08 15:31:47 +0200

    fix new issues about template

--- src/client/social-gifts/ShopObjectCard/views/CleanShopObjectCardView.js
+++ src/client/social-gifts/ShopObjectCard/views/CleanShopObjectCardView.js
@@ -13,6 +13,7 @@ import {
   isNewInstacartProgram,
   isOldInstacartProgram,
 } from '../../../../common/helpers/wObjectHelper';
+import { averageRate, formatAverageRate } from '../../../components/Sidebar/Rate/rateHelper';
 
 import '../ShopObjectCard.clean.less';
 
@@ -33,103 +34,109 @@ const CleanShopObjectCardView = ({
   proposition,
   daysLeft,
   onClick,
-}) => (
-  <div className="ShopObjectCardClean" onClick={onClick}>
-    <div className="ShopObjectCardClean__imageWrap">
-      <a href={objLink} onClick={e => e.preventDefault()}>
-        <img className="ShopObjectCardClean__image" src={url} alt={altText} />
-        {withRewards && (
-          <div className="ShopObjectCardClean__rewardText">
-            {isSpecialCampaign ? (
-              <>
-                <USDDisplay value={specialAmount} currencyDisplay="symbol" />
-                {isGiveawayCampaign
-                  ? ` Giveaway${daysLeft !== null ? ` - ${daysLeft} Days Left!` : ''}`
-                  : ` Contest${daysLeft !== null ? ` - Win in ${daysLeft} Days!` : ''}`}
-              </>
-            ) : (
-              <>
-                <FormattedMessage
-                  id={`share_photo${
-                    proposition?.requirements?.minPhotos === 1 ? '' : 's'
-                  }_and_earn`}
-                  defaultMessage={`Share {minPhotos} photo${
-                    proposition?.requirements?.minPhotos === 1 ? '' : 's'
-                  } & earn`}
-                  values={{ minPhotos: proposition?.requirements?.minPhotos }}
-                />{' '}
-                <USDDisplay value={rewardAmount} currencyDisplay={'symbol'} />
-              </>
-            )}
-          </div>
-        )}
-      </a>
-      <div
-        className={classNames('ShopObjectCardClean__heart', {
-          'ShopObjectCardClean__heart--no-border': !isAuthenticated,
-        })}
-      >
-        <HeartButton wobject={wObject} size={'20px'} />
-      </div>
-    </div>
-    <div className="ShopObjectCardClean__content">
-      <a
-        href={objLink}
-        onClick={e => e.preventDefault()}
-        title={wObject?.description ? truncate(wObject?.description, { length: 200 }) : wobjName}
-        className="ShopObjectCardClean__name"
-      >
-        {truncate(wobjName, {
-          length: isSocialProduct && isMobile() ? 35 : 110,
-          separator: '...',
-        })}
-      </a>
+}) => {
+  const ratingValue = !isEmpty(rating)
+    ? formatAverageRate(rating.average_rating_weight) || averageRate(rating) || 0
+    : 0;
 
-      <div className="ShopObjectCardClean__priceRow">
-        <div>
-          {!isEmpty(rating) && (
-            <div className="ShopObjectCardClean__rating">
-              <Icon type="star" theme="filled" />
-              <span>{rating.average?.toFixed(1) || rating.rating_value}</span>
+  return (
+    <div className="ShopObjectCardClean" onClick={onClick}>
+      <div className="ShopObjectCardClean__imageWrap">
+        <a href={objLink} onClick={e => e.preventDefault()}>
+          <img className="ShopObjectCardClean__image" src={url} alt={altText} />
+          {withRewards && (
+            <div className="ShopObjectCardClean__rewardText">
+              {isSpecialCampaign ? (
+                <>
+                  <USDDisplay value={specialAmount} currencyDisplay="symbol" />
+                  {isGiveawayCampaign
+                    ? ` Giveaway${daysLeft !== null ? ` - ${daysLeft} Days Left!` : ''}`
+                    : ` Contest${daysLeft !== null ? ` - Win in ${daysLeft} Days!` : ''}`}
+                </>
+              ) : (
+                <>
+                  <FormattedMessage
+                    id={`share_photo${
+                      proposition?.requirements?.minPhotos === 1 ? '' : 's'
+                    }_and_earn`}
+                    defaultMessage={`Share {minPhotos} photo${
+                      proposition?.requirements?.minPhotos === 1 ? '' : 's'
+                    } & earn`}
+                    values={{ minPhotos: proposition?.requirements?.minPhotos }}
+                  />{' '}
+                  <USDDisplay value={rewardAmount} currencyDisplay={'symbol'} />
+                </>
+              )}
             </div>
           )}
-          {wObject.price && (
-            <span className="ShopObjectCardClean__price" title={wObject.price}>
-              {wObject.price}
-            </span>
+        </a>
+        <div
+          className={classNames('ShopObjectCardClean__heart', {
+            'ShopObjectCardClean__heart--no-border': !isAuthenticated,
+          })}
+        >
+          <HeartButton wobject={wObject} size={'20px'} />
+        </div>
+      </div>
+      <div className="ShopObjectCardClean__content">
+        <a
+          href={objLink}
+          onClick={e => e.preventDefault()}
+          title={wObject?.description ? truncate(wObject?.description, { length: 200 }) : wobjName}
+          className="ShopObjectCardClean__name"
+        >
+          {truncate(wobjName, {
+            length: isSocialProduct && isMobile() ? 35 : 110,
+            separator: '...',
+          })}
+        </a>
+
+        <div className="ShopObjectCardClean__priceRow">
+          <div>
+            {ratingValue > 0 && (
+              <div className="ShopObjectCardClean__rating">
+                <Icon type="star" theme="filled" />
+                <span>{ratingValue.toFixed(1)}</span>
+              </div>
+            )}
+            {wObject.price && (
+              <span className="ShopObjectCardClean__price" title={wObject.price}>
+                {wObject.price}
+              </span>
+            )}
+          </div>
+          {!isEmpty(wObject.affiliateLinks) && (
+            <div className="ShopObjectCardClean__affiliatLinks">
+              {wObject.affiliateLinks
+                .sort((a, b) => a?.type?.charCodeAt(0) - b?.type?.charCodeAt(0))
+                .map(affLink => {
+                  if (
+                    affLink.type.toLocaleLowerCase() === 'instacart' &&
+                    isNewInstacartProgram(affLink)
+                  )
+                    return (
+                      <InstacartWidget
+                        inCard
+                        key={affLink.link}
+                        instacartAff={affLink}
+                        wobjPerm={wObject?.author_permlink}
+                      />
+                    );
+                  if (
+                    affLink.type.toLocaleLowerCase() === 'instacart' &&
+                    isOldInstacartProgram(affLink)
+                  )
+                    return null;
+
+                  return <AffiliatLink key={affLink.link} link={affLink} />;
+                })}
+            </div>
           )}
         </div>
-        {!isEmpty(wObject.affiliateLinks) && (
-          <div className="ShopObjectCardClean__affiliatLinks">
-            {wObject.affiliateLinks
-              .sort((a, b) => a?.type?.charCodeAt(0) - b?.type?.charCodeAt(0))
-              .map(affLink => {
-                if (
-                  affLink.type.toLocaleLowerCase() === 'instacart' &&
-                  isNewInstacartProgram(affLink)
-                )
-                  return (
-                    <InstacartWidget
-                      inCard
-                      key={affLink.link}
-                      instacartAff={affLink}
-                      wobjPerm={wObject?.author_permlink}
-                    />
-                  );
-                if (
-                  affLink.type.toLocaleLowerCase() === 'instacart' &&
-                  isOldInstacartProgram(affLink)
-                )
-                  return null;
-
-                return <AffiliatLink key={affLink.link} link={affLink} />;
-              })}
-          </div>
-        )}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 CleanShopObjectCardView.propTypes = {
   wObject: PropTypes.shape({
