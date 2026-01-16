49a2a274145161040598fb4ecda9f9d380c1da7e
lucykolosova <ludmila@MacBook-Pro-Ludmila-1434.local>
2026-01-16 11:37:56 +0200

    show all description and delete console in api

--- src/designTemplates/clean/components/ListHero.js
+++ src/designTemplates/clean/components/ListHero.js
@@ -1,15 +1,12 @@
 import PropTypes from 'prop-types';
-import React, { memo, useMemo, useState } from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, { memo, useMemo } from 'react';
 import classNames from 'classnames';
 import { getObjectName } from '../../../common/helpers/wObjectHelper';
-import { removeEmptyLines, shortenDescription } from '../../../client/object/wObjectHelper';
+import { removeEmptyLines } from '../../../client/object/wObjectHelper';
 
 import './ListHero.less';
 
 const CleanListHero = ({ wobject }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
-
   const name = useMemo(() => getObjectName(wobject) || wobject?.name, [wobject, wobject?.name]);
 
   const heroTitle = useMemo(() => wobject?.title || getObjectName(wobject), [wobject]);
@@ -24,22 +21,7 @@ const CleanListHero = ({ wobject }) => {
     return wobject?.description.split('\n\n').filter(p => p.trim().length > 0);
   }, [fullDescription]);
 
-  const shortDescription = useMemo(() => {
-    if (!fullDescription) return '';
-
-    const { firstDescrPart } = shortenDescription(fullDescription, 360);
-
-    return firstDescrPart;
-  }, [fullDescription]);
-
-  const shortDescriptionParagraphs = useMemo(() => {
-    if (!shortDescription) return [];
-
-    return shortDescription.split('\n\n').filter(p => p.trim().length > 0);
-  }, [shortDescription]);
-
-  const hasMoreText = fullDescription.length > 360;
-  const displayParagraphs = isExpanded ? descriptionParagraphs : shortDescriptionParagraphs;
+  const displayParagraphs = descriptionParagraphs;
 
   const banner = wobject?.avatar;
 
@@ -54,55 +36,19 @@ const CleanListHero = ({ wobject }) => {
     <section
       className={classNames('CleanListHero', {
         'CleanListHero--no-banner': !banner,
-        'CleanListHero--expanded': isExpanded,
       })}
     >
       <div className="CleanListHero__content">
         {name && <h2 className="CleanListHero__title">{name}</h2>}
         {displayParagraphs.length > 0 && (
           <div className="CleanListHero__subtitle">
-            {displayParagraphs.map((paragraph, index) => {
-              if (isExpanded && index === 0) {
-                return (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <p key={index} className="CleanListHero__paragraph--first">
-                    {paragraph}
-                  </p>
-                );
-              }
-              if (isExpanded && index > 0) {
-                return null;
-              }
-
-              return (
-                // eslint-disable-next-line react/no-array-index-key
-                <p key={index}>{paragraph}</p>
-              );
-            })}
+            {displayParagraphs.map((paragraph, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <p key={index}>{paragraph}</p>
+            ))}
           </div>
         )}
-        {hasMoreText && (
-          <button
-            type="button"
-            className="CleanListHero__readMore"
-            onClick={() => setIsExpanded(!isExpanded)}
-          >
-            {isExpanded ? (
-              <FormattedMessage id="show_less" defaultMessage="show less" />
-            ) : (
-              <FormattedMessage id="read_more" defaultMessage="read more" />
-            )}
-          </button>
-        )}
       </div>
-      {isExpanded && displayParagraphs.length > 1 && (
-        <div className="CleanListHero__restParagraphs">
-          {displayParagraphs.slice(1).map((paragraph, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <p key={index + 1}>{paragraph}</p>
-          ))}
-        </div>
-      )}
       {banner && (
         <div className="CleanListHero__banner">
           <img src={banner} alt={heroTitle || 'Hero visual'} />
